b824875a87dc60a1cce8afbdc95b386071b510be
Patrick Barker <barkerp@vmware.com>
2020-10-06 16:24:23 -0600

    clean

--- pkg/v1/cli/repository_test.go
+++ pkg/v1/cli/repository_test.go
@@ -1,17 +1,23 @@
 package cli
 
-// func TestGCPRepository(t *testing.T) {
-// 	gcpReg := CommunityGCPBucketRepository
-// 	list, err := gcpReg.List()
-// 	require.NoError(t, err)
+import (
+	"testing"
 
-// 	require.GreaterOrEqual(t, len(list), 2)
+	"github.com/stretchr/testify/require"
+)
 
-// 	_, err = gcpReg.Describe("cluster")
-// 	require.NoError(t, err)
+func TestGCPRepository(t *testing.T) {
+	gcpReg := CommunityGCPBucketRepository
+	list, err := gcpReg.List()
+	require.NoError(t, err)
 
-// 	bin, err := gcpReg.Fetch("cluster", VersionLatest, LinuxAMD64)
-// 	require.NoError(t, err)
+	require.GreaterOrEqual(t, len(list), 2)
 
-// 	require.GreaterOrEqual(t, len(bin), 10)
-// }
+	_, err = gcpReg.Describe("cluster")
+	require.NoError(t, err)
+
+	bin, err := gcpReg.Fetch("cluster", VersionLatest, LinuxAMD64)
+	require.NoError(t, err)
+
+	require.GreaterOrEqual(t, len(bin), 10)
+}
