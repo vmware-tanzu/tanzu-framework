7433660ebbb0539ce135334018f058513a32c4a2
Patrick Barker <barkerp@vmware.com>
2020-10-08 11:27:18 -0600

    cli framework only

--- pkg/v1/client/metadata.go
+++
@@ -1,9 +0,0 @@
-package client
-
-const (
-	// NameHeader used by every client making an API call
-	NameHeader = "x-client-name"
-
-	// VersionHeader is the client version header.
-	VersionHeader = "x-client-version"
-)
