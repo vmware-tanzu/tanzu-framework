0f26d9e59d91ade67d0c9cdd63792f90fd3dabd7
Rwang721 <riawang0721@gmail.com>
2021-01-13 16:35:31 -0800

    Implement registry client to pull and unpack image

---
+++ pkg/v1/tkr/pkg/registry/client.go
@@ -0,0 +1,112 @@
+package registry
+
+import (
+	"archive/tar"
+	"fmt"
+	"io"
+	"io/ioutil"
+
+	regname "github.com/google/go-containerregistry/pkg/name"
+	regv1 "github.com/google/go-containerregistry/pkg/v1"
+	ctlimg "github.com/k14s/imgpkg/pkg/imgpkg/image"
+	"github.com/pkg/errors"
+)
+
+type registry struct {
+	registry ctlimg.Registry
+}
+
+func New(opts ctlimg.RegistryOpts) Registry {
+
+	reg := ctlimg.NewRegistry(opts)
+
+	return &registry{
+		registry: reg,
+	}
+}
+
+// ListImageTags lists all tags of the given image.
+func (r *registry) ListImageTags(imageName string) ([]string, error) {
+	ref, err := regname.ParseReference(imageName, regname.WeakValidation)
+	if err != nil {
+		return []string{}, err
+	}
+
+	return r.registry.ListTags(ref.Context())
+}
+
+// GetFile gets the file content bundled in the given image:tag.
+// If filename is empty, it will get the first file.
+func (r *registry) GetFile(image string, tag string, filename string) ([]byte, error) {
+	ref, err := regname.ParseReference(fmt.Sprintf("%s:%s", image, tag), regname.WeakValidation)
+	if err != nil {
+		return nil, err
+	}
+	imgs, err := ctlimg.NewImages(ref, r.registry).Images()
+	if err != nil {
+		return nil, errors.Wrap(err, "Collecting images: %s")
+	}
+	if len(imgs) == 0 {
+		return nil, errors.New("Expected to find at least one image, but found none")
+	}
+
+	if len(imgs) > 1 {
+		fmt.Println("Found multiple images, extracting first")
+	}
+
+	return getFileContentFromImage(imgs[0], filename)
+}
+
+func getFileContentFromImage(image regv1.Image, filename string) ([]byte, error) {
+	layers, err := image.Layers()
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, imgLayer := range layers {
+		layerStream, err := imgLayer.Uncompressed()
+		if err != nil {
+			return nil, err
+		}
+
+		defer layerStream.Close()
+
+		files, err := getFileFromLayer(layerStream)
+		if err != nil {
+			return nil, err
+		}
+
+		for k, v := range files {
+			if filename == "" || k == filename {
+				return v, nil
+			}
+		}
+	}
+	return nil, errors.New("cannot find file from the image")
+}
+
+func getFileFromLayer(stream io.Reader) (files map[string][]byte, err error) {
+	files = make(map[string][]byte)
+	tarReader := tar.NewReader(stream)
+
+	for {
+		hdr, err := tarReader.Next()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return files, err
+		}
+
+		if hdr.Typeflag == tar.TypeReg || hdr.Typeflag == tar.TypeRegA {
+			buf, err := ioutil.ReadAll(tarReader)
+			if err != nil {
+				return files, err
+			}
+			files[hdr.Name] = buf
+		}
+
+	}
+	return files, err
+}
---
+++ pkg/v1/tkr/pkg/registry/client_integ_test.go
@@ -0,0 +1,42 @@
+package registry
+
+import (
+	"os"
+	"testing"
+
+	ctlimg "github.com/k14s/imgpkg/pkg/imgpkg/image"
+	"github.com/vmware-tanzu-private/core/pkg/v1/tkr/pkg/types"
+)
+
+func TestRegistryPullBOM(t *testing.T) {
+	if integTest := os.Getenv("TKR_INTEG_TEST"); integTest != "true" {
+		return
+	}
+	bomRegistry := os.Getenv("TEST_BOM_IMAGE_PATH")
+	if bomRegistry == "" {
+		t.Fatal("TEST_BOM_IMAGE_PATH must be set")
+	}
+
+	ro := ctlimg.RegistryOpts{}
+
+	reg := New(ro)
+
+	tags, err := reg.ListImageTags(bomRegistry)
+	if err != nil {
+		t.Fatalf("error listing tags should not occurs %s", err.Error())
+	}
+
+	t.Log(tags[0])
+
+	content, err := reg.GetFile(bomRegistry, tags[0], "")
+	if err != nil {
+		t.Fatalf("error getting image content should not occurs %s", err.Error())
+	}
+
+	bom, err := types.NewBom(content)
+	if err != nil {
+		t.Fatalf("error parsing bom content should not occurs %s", err.Error())
+	}
+
+	t.Log(bom.GetComponent("kubernetes"))
+}
