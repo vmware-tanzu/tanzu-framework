0b038e60a8d3700dd2f911e4c22fe589a11a152c
Sean McGinnis <sean.mcginnis@gmail.com>
2021-02-03 13:41:51 -0600

    Clean up minor linting issues
    This makes a few updates to address linting concerns.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- apis/run/v1alpha1/tanzukubernetesrelease_types.go
+++ apis/run/v1alpha1/tanzukubernetesrelease_types.go
@@ -8,6 +8,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha3"
 )
+
 const (
 	ConditionCompatible       = "Compatible"
 	ConditionUpgradeAvailable = "UpgradeAvailable"
