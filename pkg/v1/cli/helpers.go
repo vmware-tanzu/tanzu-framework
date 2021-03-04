0556801d67c9d9ff116c9f76bd6cd30686e24224
Sudarshan Aji <57639960+saji-pivotal@users.noreply.github.com>
2021-03-05 03:30:39 +0530

    TKG-4246: Remove cluster entry from the config file (#353)
    * TKG-4108: Implemented remove server from the configs
    
    * TKG-4108: Reverting changes from branch TKG-4086
    
    * TKG-4108: Adding config server delete
    
    * TKG-4246: Added a prompt with a warning message that deleting the server entry will cause loss of access to thhe cluster
    
    * TKG-4246: Adding helpers, and ran linter
    
    * TKG-4246: Making list.go in line with main
    
    * TKG-4246: Reformated code according to the linter suggestion
    
    * TKG-4246: Added a comment as required by the GoLinter
    
    * TKG-4246: Addressed code review comments
    
    * TKG-4246: Removed duplicated function
    
    * TKG-4246: Addressed code review comments
    
    * TKG-4246: Fixing linters
    
    * TKG-4246: Trying to fix linter errors

---
+++ pkg/v1/cli/helpers.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+var (
+	okResponsesMap = map[string]struct{}{
+		"y": {},
+		"Y": {},
+	}
+)
+
+// AskForConfirmation is used to prompt the user to confirm or deny a choice
+func AskForConfirmation(message string) error {
+	var response string
+	msg := message + " [y/N]: "
+	fmt.Print(msg)
+	_, err := fmt.Scanln(&response)
+	if err != nil {
+		return errors.New("aborted")
+	}
+	if _, exit := okResponsesMap[response]; !exit {
+		return errors.New("aborted")
+	}
+	return nil
+}
