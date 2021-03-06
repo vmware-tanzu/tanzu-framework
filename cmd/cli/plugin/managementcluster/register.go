28061015fc8f75a6ee2423b8d31401a2298f3d87
Sean McGinnis <sean.mcginnis@gmail.com>
2021-03-06 07:04:26 -0600

    Migrate management-cluster plugin
    This migrates the management cluster plugin from the
    tanzu-cli-tkg-plugins repo to here. This is a core plugin that is needed
    for normal operations, and needs to be included when we make this repo
    open source.
    
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>
    (cherry picked from commit 0923c310c3f747a8ac2be54c17f9572c31953e2a)

---
+++ cmd/cli/plugin/managementcluster/register.go
@@ -0,0 +1,43 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/vmware-tanzu-private/core/apis/client/v1alpha1"
+	"github.com/vmware-tanzu-private/tkg-cli/pkg/tkgctl"
+)
+
+var registerCmd = &cobra.Command{
+	Use:   "register",
+	Short: "Register the management cluster to Tanzu Mission Control",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return runForCurrentMC(registerWithTmc)
+	},
+}
+
+type registerOptions struct {
+	tmcRegistrationURL string
+}
+
+var ro = &registerOptions{}
+
+func init() {
+	registerCmd.Flags().StringVarP(&ro.tmcRegistrationURL, "tmc-registration-url", "", "", "URL to download the yml which has configuration related to various kubernetes objects to be deployed on the management cluster for it to register to Tanzu Mission Control")
+	_ = registerCmd.MarkFlagRequired("tmc-registration-url")
+}
+
+func registerWithTmc(server *v1alpha1.Server) error {
+	tkgClient, err := newTKGCtlClient()
+	if err != nil {
+		return err
+	}
+
+	options := tkgctl.RegisterOptions{
+		ClusterName:        server.Name,
+		TMCRegistrationURL: ro.tmcRegistrationURL,
+	}
+	return tkgClient.RegisterWithTmc(options)
+}
