358dddb7251451a81a65a84149948663ecb308ab
Vui Lam <vui@vmware.com>
2021-05-19 14:18:53 -0700

    Initial drop of provider templates and overlays
    This is the set of provider yamls along with their cluster templates and
    ytt overlays used for customiziung cluster creation
    
    - add providers/client
    - update import paths and generated bindata
    
    Signed-off-by: Vui Lam <vui@vmware.com>

---
+++ pkg/v1/providers/client/client.go
@@ -0,0 +1,22 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package client provides access to the providers template data
+package client
+
+import (
+	"github.com/vmware-tanzu-private/core/pkg/v1/providers/client/manifest"
+	"github.com/vmware-tanzu-private/core/pkg/v1/tkg/providerinterface"
+)
+
+type provider struct {
+}
+
+// New returns provider client which implements provider interface
+func New() providerinterface.ProviderInterface {
+	return &provider{}
+}
+
+func (p *provider) GetProviderBundle() ([]byte, error) {
+	return manifest.Asset("provider-bundle/providers.zip")
+}
