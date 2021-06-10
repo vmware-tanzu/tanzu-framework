7ca379a6ea49d3ae3978de26a2664541afeead28
Anuj Chaudhari <anujc@vmware.com>
2021-06-09 18:21:05 -0700

    Allow tags with builder plugin, embed provider bundle based on build tags

---
+++ pkg/v1/providers/client/manifest/embed_without_local_providers.go
@@ -0,0 +1,9 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// +build !embedproviders
+
+// Package manifest...
+package manifest
+
+var ProviderZipBundle []byte
