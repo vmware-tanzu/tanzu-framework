d63d890b763d620052f3c612a18c364f6c28539b
Rajath Agasthya <ragasthya@vmware.com>
2021-07-08 10:31:45 -0700

    Address build and lint errors
    Also ignores `envtest` for capability controller by adding a build tag
    because envtest fails and there are no tests yet.

--- cmd/controller-managers/capabilities/main.go
+++ cmd/controller-managers/capabilities/main.go
@@ -1,18 +1,5 @@
-/*
-Copyright 2021.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 package main
 
@@ -28,9 +15,9 @@ import (
 
 	// +kubebuilder:scaffold:imports
 
-	runv1alpha1 "github.com/vmware-tanzu-private/core/apis/run/v1alpha1"
-	controllers "github.com/vmware-tanzu-private/core/controllers/run"
-	"github.com/vmware-tanzu-private/core/pkg/v1/discovery"
+	runv1alpha1 "github.com/vmware-tanzu/tanzu-framework/apis/run/v1alpha1"
+	controllers "github.com/vmware-tanzu/tanzu-framework/controllers/run"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/discovery"
 )
 
 var (
