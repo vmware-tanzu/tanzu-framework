e347705661cdbf7d9b7b19ee15b53f2568c65686
Rajath Agasthya <ragasthya@vmware.com>
2021-07-08 10:31:45 -0700

    Address build and lint errors
    Also ignores `envtest` for capability controller by adding a build tag
    because envtest fails and there are no tests yet.

--- pkg/v1/discovery/generate.go
+++ pkg/v1/discovery/generate.go
@@ -1,10 +1,13 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package discovery
 
 import (
 	"fmt"
 	"math/rand"
 
-	runv1alpha1 "github.com/vmware-tanzu-private/core/apis/run/v1alpha1"
+	runv1alpha1 "github.com/vmware-tanzu/tanzu-framework/apis/run/v1alpha1"
 )
 
 // QueryTargetsToCapabilityResource is a helper function to generate a Capability v1alpha1 resource from a slice of QueryTarget.
@@ -19,7 +22,7 @@ func QueryTargetsToCapabilityResource(queryTargets []QueryTarget) (*runv1alpha1.
 		switch query := qt.(type) {
 		case *QueryGVR:
 			q := runv1alpha1.QueryGVR{
-				Name:     fmt.Sprintf("gvr-%d", rand.Int31()),
+				Name:     fmt.Sprintf("gvr-%d", rand.Int31()), //nolint:gosec
 				Group:    query.group,
 				Versions: query.versions,
 				Resource: query.resource,
@@ -27,7 +30,7 @@ func QueryTargetsToCapabilityResource(queryTargets []QueryTarget) (*runv1alpha1.
 			gvrQueries = append(gvrQueries, q)
 		case *QueryObject:
 			q := runv1alpha1.QueryObject{
-				Name:               fmt.Sprintf("object-%d", rand.Int31()),
+				Name:               fmt.Sprintf("object-%d", rand.Int31()), //nolint:gosec
 				ObjectReference:    *query.object,
 				WithAnnotations:    query.annotationsMap(true),
 				WithoutAnnotations: query.annotationsMap(false),
@@ -35,7 +38,7 @@ func QueryTargetsToCapabilityResource(queryTargets []QueryTarget) (*runv1alpha1.
 			objectQueries = append(objectQueries, q)
 		case *QueryPartialSchema:
 			q := runv1alpha1.QueryPartialSchema{
-				Name:          fmt.Sprintf("partialSchema-%d", rand.Int31()),
+				Name:          fmt.Sprintf("partialSchema-%d", rand.Int31()), //nolint:gosec
 				PartialSchema: query.schema,
 			}
 			partialSchemaQueries = append(partialSchemaQueries, q)
