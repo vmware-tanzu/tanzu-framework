40a95e3fdf5a55c5cd54ff083bf3f754e7d06b3d
Ian Coffey <icoffey@vmware.com>
2021-07-09 20:40:46 -0400

    Resolve gitignored data

---
+++ addons/pinniped/post-deploy/hack/scripts/common.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+################################################################################
+##                               FUNCTIONS                                    ##
+################################################################################
+function sha256 {
+  local cmd
+
+  if command -v sha256sum &> /dev/null; then
+    cmd=(sha256sum)
+  elif command -v shasum &> /dev/null; then
+    cmd=(shasum -a 256)
+  else
+    echo "ERROR: could not find shasum or sha256sum."
+    return 1
+  fi
+
+  "${cmd[@]}" "$@"
+}
+
+function sha1 {
+  local cmd
+
+  if command -v sha1sum &> /dev/null; then
+    cmd=(sha1sum)
+  elif command -v shasum &> /dev/null; then
+    cmd=(shasum -a 1)
+  else
+    echo "ERROR: could not find shasum or sha1sum."
+    return 1
+  fi
+
+  "${cmd[@]}" "$@"
+}
