4ef006db3e720bb1e5a8c06be949bf6a545ba728
Guangyuan Wang <wguangyuan@vmware.com>
2021-07-13 23:34:46 +0000

    always log cluster condition on changes

--- pkg/v1/tkg/clusterclient/clusterclient.go
+++ pkg/v1/tkg/clusterclient/clusterclient.go
@@ -447,6 +447,7 @@ func (c *client) WaitForClusterInitialized(clusterName, namespace string) error 
 	var err error
 	var currentClusterInfo ClusterStatusInfo
 	var lastClusterInfo ClusterStatusInfo
+	var lastReason string
 	unchangedCounter := 0
 	interval := 15 * time.Second
 
@@ -486,6 +487,11 @@ func (c *client) WaitForClusterInitialized(clusterName, namespace string) error 
 		} else {
 			unchangedCounter++
 			log.V(7).Infof("cluster state is unchanged %v", unchangedCounter)
+		}
+
+		if lastReason != err.Error() {
+			log.Info(err.Error())
+			lastReason = err.Error()
 		}
 
 		lastClusterInfo = currentClusterInfo
