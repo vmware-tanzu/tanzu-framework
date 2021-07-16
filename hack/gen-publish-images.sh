7f4a2395d079c775a4479b6b5eeacadb6abd9604
Ashish Amarnath <ashisham@vmware.com>
2021-07-16 13:44:48 -0700

    Remove dependency of airgap image copy script on local BOM files (#215)
    * remove the dependency of airgap image copy script on local BOM files
    
    * copy tanzu core packages from tkr-bom as carvel bundle
    
    Signed-off-by: Ashish Amarnath <ashisham@vmware.com>

--- hack/gen-publish-images.sh
+++ hack/gen-publish-images.sh
@@ -6,6 +6,9 @@ set -euo pipefail
 
 TANZU_BOM_DIR=${HOME}/.config/tanzu/tkg/bom
 INSTALL_INSTRUCTIONS='See https://github.com/mikefarah/yq#install for installation instructions'
+TKG_CUSTOM_IMAGE_REPOSITORY=${TKG_CUSTOM_IMAGE_REPOSITORY:-''}
+TKG_IMAGE_REPO=${TKG_IMAGE_REPO:-''}
+
 
 echodual() {
   echo "$@" 1>&2
@@ -13,14 +16,12 @@ echodual() {
 }
 
 if [ -z "$TKG_CUSTOM_IMAGE_REPOSITORY" ]; then
-  echo "TKG_CUSTOM_IMAGE_REPOSITORY variable is not defined" >&2
+  echo "TKG_CUSTOM_IMAGE_REPOSITORY variable is required but is not defined" >&2
   exit 1
 fi
 
-if [[ -d "$TANZU_BOM_DIR" ]]; then
-  BOM_DIR="${TANZU_BOM_DIR}"
-else
-  echo "Tanzu Kubernetes Grid directories not found. Run CLI once to initialise." >&2
+if [ -z "$TKG_IMAGE_REPO" ]; then
+  echo "TKG_IMAGE_REPO variable is required but is not defined" >&2
   exit 2
 fi
 
@@ -46,19 +47,13 @@ function imgpkg_copy() {
 echo "set -euo pipefail"
 echodual "Note that yq must be version above or equal to version 4.5 and below version 5."
 
-actualImageRepository=""
-# Iterate through BoM file to read actual image repository
-for TKG_BOM_FILE in "$BOM_DIR"/*.yaml; do
-  # Get actual image repository from BoM file
-  actualImageRepository=$(yq e '.imageConfig.imageRepository' "$TKG_BOM_FILE")
-  break
-done
+actualImageRepository="$TKG_IMAGE_REPO"
 
 # Iterate through TKG BoM file to create the complete Image name
 # and then pull, retag and push image to custom registry.
 list=$(imgpkg  tag  list -i "${actualImageRepository}"/tkg-bom)
 for imageTag in ${list}; do
-  if [[ ${imageTag} == v* ]]; then 
+  if [[ ${imageTag} == v* ]]; then
     TKG_BOM_FILE="tkg-bom-${imageTag//_/+}.yaml"
     imgpkg pull --image "${actualImageRepository}/tkg-bom:${imageTag}" --output "tmp" > /dev/null 2>&1
     echodual "Processing TKG BOM file ${TKG_BOM_FILE}"
@@ -66,7 +61,6 @@ for imageTag in ${list}; do
     actualTKGImage=${actualImageRepository}/tkg-bom:${imageTag}
     customTKGImage=${TKG_CUSTOM_IMAGE_REPOSITORY}/tkg-bom:${imageTag}
     imgpkg_copy "-i" $actualTKGImage $customTKGImage
-    
     
     # Get components in the tkg-bom.
     # Remove the leading '[' and trailing ']' in the output of yq.
@@ -78,7 +72,7 @@ for imageTag in ${list}; do
     get_comp_images="yq e '.components[\"${comp}\"][]  | select(has(\"images\"))|.images[] | .imagePath + \":\" + .tag' "\"tmp/\"$TKG_BOM_FILE""
 
     flags="-i"
-    if [ $comp = "tkg-standard-packages" ] || [ $comp = "tkg-core-packages" ]; then
+    if [ $comp = "tkg-standard-packages" ]; then
       flags="-b"
     fi
     eval $get_comp_images | while read -r image; do
@@ -98,7 +92,7 @@ done
 # and then pull, retag and push image to custom registry.
 list=$(imgpkg  tag  list -i ${actualImageRepository}/tkr-bom)
 for imageTag in ${list}; do
-  if [[ ${imageTag} == v* ]]; then 
+  if [[ ${imageTag} == v* ]]; then
     TKR_BOM_FILE="tkr-bom-${imageTag//_/+}.yaml"
     echodual "Processing TKR BOM file ${TKR_BOM_FILE}"
 
@@ -106,13 +100,27 @@ for imageTag in ${list}; do
     customTKRImage=${TKG_CUSTOM_IMAGE_REPOSITORY}/tkr-bom:${imageTag}
     imgpkg_copy "-i" $actualTKRImage $customTKRImage
     imgpkg pull --image ${actualImageRepository}/tkr-bom:${imageTag} --output "tmp" > /dev/null 2>&1
-    yq e '.. | select(has("images"))|.images[] | .imagePath + ":" + .tag ' "tmp/$TKR_BOM_FILE" |
-    while read -r image; do
-      actualImage=${actualImageRepository}/${image}
-      customImage=$TKG_CUSTOM_IMAGE_REPOSITORY/${image}
-      imgpkg_copy "-i" $actualImage $customImage
-      echo ""
+
+    # Get components in the tkr-bom.
+    # Remove the leading '[' and trailing ']' in the output of yq.
+    components=(`yq e '.components | keys | .. style="flow"' "tmp/$TKR_BOM_FILE" | sed 's/^.//;s/.$//'`)
+    for comp in "${components[@]}"
+    do
+    # remove: leading and trailing whitespace, and trailing comma
+    comp=`echo $comp | sed -e 's/^[[:space:]]*//' | sed 's/,*$//g'`
+    get_comp_images="yq e '.components[\"${comp}\"][]  | select(has(\"images\"))|.images[] | .imagePath + \":\" + .tag' "\"tmp/\"$TKR_BOM_FILE""
+
+    flags="-i"
+    if [ $comp = "tkg-core-packages" ]; then
+      flags="-b"
+    fi
+    eval $get_comp_images | while read -r image; do
+        actualImage=${actualImageRepository}/${image}
+        customImage=$TKG_CUSTOM_IMAGE_REPOSITORY/${image}
+        imgpkg_copy $flags $actualImage $customImage
+      done
     done
+
     rm -rf tmp
     echodual "Finished processing TKR BOM file ${TKR_BOM_FILE}"
     echo ""
