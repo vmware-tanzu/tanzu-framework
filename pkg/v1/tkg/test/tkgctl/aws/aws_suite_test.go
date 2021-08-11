c0adea87c5eeb0f924d3dddfee589e7e8fc360a1
Daniel Guo <danniel1205@gmail.com>
2021-08-11 11:05:29 -0700

    Update e2e tests to pass in Edition as InitRegionOptions
    Signed-off-by: Daniel Guo <danniel1205@gmail.com>

--- pkg/v1/tkg/test/tkgctl/aws/aws_suite_test.go
+++ pkg/v1/tkg/test/tkgctl/aws/aws_suite_test.go
@@ -105,6 +105,7 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 			DeployTKGonVsphere7:         e2eConfig.ManagementClusterOptions.DeployTKGonVsphere7,
 			EnableTKGSOnVsphere7:        e2eConfig.ManagementClusterOptions.EnableTKGSOnVsphere7,
 			VsphereControlPlaneEndpoint: e2eConfig.ManagementClusterOptions.Endpoint,
+			Edition:                     "tkg",
 		})
 
 		Expect(err).To(BeNil())
