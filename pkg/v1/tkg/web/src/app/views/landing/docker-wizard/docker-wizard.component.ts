6d4c27283d19f81ea0aa11d258115a56279d94f5
raymond zhang <zraymond@vmware.com>
2021-08-19 10:18:22 -0700

    added  cluster name to docker

--- pkg/v1/tkg/web/src/app/views/landing/docker-wizard/docker-wizard.component.ts
+++ pkg/v1/tkg/web/src/app/views/landing/docker-wizard/docker-wizard.component.ts
@@ -44,6 +44,8 @@ export class DockerWizardComponent extends WizardBaseDirective implements OnInit
             dockerDaemonForm: this.formBuilder.group({
             }),
             networkForm: this.formBuilder.group({
+            }),
+            dockerNodeSettingForm: this.formBuilder.group({
             })
             // identityForm: this.formBuilder.group({
             // })
@@ -57,6 +59,8 @@ export class DockerWizardComponent extends WizardBaseDirective implements OnInit
             } else {
                 return 'Specify the cluster Pod CIDR';
             }
+        } else if (stepName === 'nodeSetting') {
+            return 'Optional: Specify the management cluster name'
         }
 
         // else if (stepName === 'identity') {
@@ -84,6 +88,8 @@ export class DockerWizardComponent extends WizardBaseDirective implements OnInit
             clusterPodCIDR: this.getFieldValue('networkForm', 'clusterPodCidr'),
             cniType: this.getFieldValue('networkForm', 'cniType')
         };
+
+        payload.clusterName = this.getFieldValue('dockerNodeSettingForm', 'clusterName');
 
         if (this.getFieldValue('networkForm', 'proxySettings')) {
             let proxySettingsMap = null;
