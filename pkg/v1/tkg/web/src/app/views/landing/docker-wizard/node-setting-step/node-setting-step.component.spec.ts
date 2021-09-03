b85dc03cd67067b25789a4f3a28cf1a22113defb
raymondz1 <70610112+raymondz1@users.noreply.github.com>
2021-09-02 17:32:10 -0700

    added  cluster name to docker (#468)

---
+++ pkg/v1/tkg/web/src/app/views/landing/docker-wizard/node-setting-step/node-setting-step.component.spec.ts
@@ -0,0 +1,45 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ValidationService } from '../../wizard/shared/validation/validation.service';
+
+import { NodeSettingStepComponent } from './node-setting-step.component';
+
+describe('NodeSettingStepComponent', () => {
+    let component: NodeSettingStepComponent;
+    let fixture: ComponentFixture<NodeSettingStepComponent>;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ NodeSettingStepComponent ],
+            imports: [
+                ReactiveFormsModule,
+                SharedModule
+            ],
+            providers: [
+                ValidationService,
+                FormBuilder
+            ]
+        })
+        .compileComponents();
+    }));
+
+    beforeEach(() => {
+        const fb = new FormBuilder();
+
+        fixture = TestBed.createComponent(NodeSettingStepComponent);
+        component = fixture.componentInstance;
+        component.formGroup = fb.group({
+            clusterName: 'testClusterName'
+        });
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should have a cluster name', () => {
+        expect(component.formGroup.get('clusterName').value).toBe('testClusterName');
+    });
+});
