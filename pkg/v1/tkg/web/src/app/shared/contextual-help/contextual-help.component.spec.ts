e93d0a62f7cea71fb0b4976513ea1de563eb7915
Justin Miclette <miclettej@vmware.com>
2021-09-13 12:47:52 -0700

    TCE Standalone UI - remove VNET worker node CIDR fields
    Updates UI for TCE standalone edition to remove unused worker node CIDR fields
    
    Fixes some unit tests related to contextual help component
    
    Signed-off-by: Justin Miclette <miclettej@vmware.com>

--- pkg/v1/tkg/web/src/app/shared/contextual-help/contextual-help.component.spec.ts
+++ pkg/v1/tkg/web/src/app/shared/contextual-help/contextual-help.component.spec.ts
@@ -28,14 +28,15 @@ describe('ContextualHelpComponent', () => {
     });
 
     it('should show contextual help component', () => {
-        component.show(['step1']);
-        expect(component.visible).toBeTrue();
+        component.getHTMLContent(['step1']);
+        component.open();
+        expect(component.isVisible).toBeTrue();
         expect(component.htmlContentIndexArray.length).toEqual(1);
     });
 
     it('should hide contextual help component', () => {
         component.hide();
-        expect(component.visible).toBeFalse();
+        expect(component.isVisible).toBeFalse();
     });
 
     it('should show detail for a topic', () => {
@@ -53,11 +54,4 @@ describe('ContextualHelpComponent', () => {
         component.navigateBack();
         expect(component.isTopicView).toBeTrue();
     });
-
-    it('should toggle pin', () => {
-        component.togglePin();
-        expect(component.isPinned).toBeTrue();
-        component.togglePin();
-        expect(component.isPinned).toBeFalse();
-    })
 });
