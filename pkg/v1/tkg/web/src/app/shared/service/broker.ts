a1736f0db30bd160d0a2fbe9b0a255c4d286b07d
Shimon Walner <swalner@vmware.com>
2021-10-26 09:19:40 -0700

    Nit: a -> an in comment

--- pkg/v1/tkg/web/src/app/shared/service/broker.ts
+++ pkg/v1/tkg/web/src/app/shared/service/broker.ts
@@ -5,7 +5,7 @@ import { AppDataService } from "./app-data.service";
  *@Class Broker - exports classes as static members to avoid injecting instances of these classes through a constructor
 
  Usage - Broker.messenger.methodName() - access methods from static class without need for a Messenger instance
- Usage - Broker.appDataService.methodName() - access methods from static class without need for a AppDataService instance
+ Usage - Broker.appDataService.methodName() - access methods from static class without need for an AppDataService instance
  */
 export default class Broker {
     static messenger = new Messenger();
