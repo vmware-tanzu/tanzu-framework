bce17db4b2602051d94d922259a4d74af45d85d7
Shimon Walner <swalner@vmware.com>
2021-10-26 08:43:27 -0700

    Adds class documentation per PR review

--- pkg/v1/tkg/web/src/app/shared/service/broker.ts
+++ pkg/v1/tkg/web/src/app/shared/service/broker.ts
@@ -1,6 +1,12 @@
 import { Messenger } from './Messenger';
 import { AppDataService } from "./app-data.service";
 
+/**
+ *@Class Broker - exports classes as static members to avoid injecting instances of these classes through a constructor
+
+ Usage - Broker.messenger.methodName() - access methods from static class without need for a Messenger instance
+ Usage - Broker.appDataService.methodName() - access methods from static class without need for a AppDataService instance
+ */
 export default class Broker {
     static messenger = new Messenger();
     static appDataService = new AppDataService();
