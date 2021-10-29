26e1b9280dd178694a3ed303c8571fb14cb5bc06
Harish Yayi <yharish991@gmail.com>
2021-10-29 10:33:27 -0400

    add CRS for management package repository
    Signed-off-by: Harish Yayi <yharish991@gmail.com>

--- pkg/v1/tkg/client/upgrade_addon.go
+++ pkg/v1/tkg/client/upgrade_addon.go
@@ -205,6 +205,11 @@ func (c *TkgClient) DoUpgradeAddon(regionalClusterClient clusterclient.Client, /
 			}
 			crsDisabledAddon = true
 			ccOptions.TargetNamespace = constants.TkrNamespace
+		case "packages/management-package-repo":
+			if !options.IsRegionalCluster {
+				return errors.Errorf("upgrade of '%s' component is only supported on management cluster", addonName)
+			}
+			crsDisabledAddon = true
 		default:
 			return errors.Errorf("upgrade of '%s' component is not supported", addonName)
 		}
