f1c34aec10a1831a721da2d9618db197122c1f05
Daniel Guo <danniel1205@gmail.com>
2021-11-01 11:23:22 -0700

    Display the latest package version as part of "tanzu package available list" command (#948)
    Signed-off-by: Daniel Guo <danniel1205@gmail.com>

--- cmd/cli/plugin/package/package_available_list.go
+++ cmd/cli/plugin/package/package_available_list.go
@@ -6,10 +6,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/k14s/semver/v4"
 	"github.com/spf13/cobra"
 
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli/component"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/kappclient"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/log"
+
+	"go.uber.org/multierr"
 )
 
 var packageAvailableListCmd = &cobra.Command{
@@ -51,19 +55,31 @@ func packageAvailableList(cmd *cobra.Command, args []string) error {
 			return err
 		}
 		if packageAvailableOp.AllNamespaces {
-			t.SetKeys("NAME", "DISPLAY-NAME", "SHORT-DESCRIPTION", "NAMESPACE")
+			t.SetKeys("NAME", "DISPLAY-NAME", "SHORT-DESCRIPTION", "LATEST-VERSION", "NAMESPACE")
 		} else {
-			t.SetKeys("NAME", "DISPLAY-NAME", "SHORT-DESCRIPTION")
+			t.SetKeys("NAME", "DISPLAY-NAME", "SHORT-DESCRIPTION", "LATEST-VERSION")
 		}
+		var multiGetVerErr error
 		for i := range packageMetadataList.Items {
 			pkg := packageMetadataList.Items[i]
+			latestVersion, getVerErr := getPackageLatestVersion(pkg.Name, pkg.Namespace, kc)
+			// It is safe to use multierr.Append() even when getVerErr is nil. The API handles that case.
+			multiGetVerErr = multierr.Append(multiGetVerErr, getVerErr)
 			if packageAvailableOp.AllNamespaces {
-				t.AddRow(pkg.Name, pkg.Spec.DisplayName, pkg.Spec.ShortDescription, pkg.Namespace)
+				t.AddRow(pkg.Name, pkg.Spec.DisplayName, pkg.Spec.ShortDescription, latestVersion, pkg.Namespace)
 			} else {
-				t.AddRow(pkg.Name, pkg.Spec.DisplayName, pkg.Spec.ShortDescription)
+				t.AddRow(pkg.Name, pkg.Spec.DisplayName, pkg.Spec.ShortDescription, latestVersion)
 			}
 		}
 		t.RenderWithSpinner()
+
+		// If there are any errors when getting the latest package version, we do not want to error out the command and
+		// block user from moving forward. When an error occurs, the latest version will be empty. What we want is to
+		// show a warning message to explain what the empty latest version means.
+		if multiGetVerErr != nil {
+			log.Warning("\nUnable to get the latest version for all packages, some packages' latest version fields" +
+				" are empty. Please try again or use `tanzu package available list <package-name>` to get the version.")
+		}
 		return nil
 	}
 	t, err := component.NewOutputWriterWithSpinner(cmd.OutOrStdout(), outputFormat,
@@ -91,4 +107,34 @@ func packageAvailableList(cmd *cobra.Command, args []string) error {
 	}
 	t.RenderWithSpinner()
 	return nil
+}
+
+// getPackageLatestVersion returns the latest version of a particular package under a namespace
+func getPackageLatestVersion(packageName, namespace string, kc kappclient.Client) (string, error) {
+	pkgs, listErr := kc.ListPackages(packageName, namespace)
+	if listErr != nil {
+		return "", listErr
+	}
+	var latest *semver.Version
+	var parseErr error
+	for i := range pkgs.Items {
+		pkg := pkgs.Items[i]
+		current, err := semver.Make(pkg.Spec.Version)
+		if err != nil {
+			// If we are not able to parse the version of current package, record the error and continue
+			parseErr = multierr.Append(parseErr, err)
+			continue
+		}
+		if latest == nil {
+			latest = &current
+		} else if latest.LT(current) {
+			latest = &current
+		}
+	}
+
+	// If we are not able to get and compare versions from all packages, latest is nil
+	if latest == nil {
+		return "", parseErr
+	}
+	return latest.String(), nil
 }
