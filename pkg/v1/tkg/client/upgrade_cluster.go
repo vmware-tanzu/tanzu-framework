d70073f15a1f55d4f5821dbcca836dd7c1d72173
Harish Yayi <yharish991@gmail.com>
2021-11-08 15:26:39 -0500

    update capabilities ytt templates to upgrade Capabilities CRD version to v1
    Signed-off-by: Harish Yayi <yharish991@gmail.com>

--- pkg/v1/tkg/client/upgrade_cluster.go
+++ pkg/v1/tkg/client/upgrade_cluster.go
@@ -346,7 +346,10 @@ func (c *TkgClient) upgradeAddonPreNodeUpgrade(regionalClusterClient clusterclie
 	// tanzu-addons-manager and tkr-controller only runs in management cluster
 	if isRegionalCluster {
 		addonsToBeUpgraded = append(addonsToBeUpgraded,
-			"addons-management/tanzu-addons-manager", "tkr/tkr-controller", "addons-management/core-package-repo")
+			"addons-management/tanzu-addons-manager",
+			"tkr/tkr-controller",
+			"addons-management/core-package-repo",
+			"capabilities/capabilities-controller")
 	}
 	upgradeClusterMetadataOptions := &UpgradeAddonOptions{
 		AddonNames:        addonsToBeUpgraded,
