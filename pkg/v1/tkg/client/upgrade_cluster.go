bcbbaa0c37323e7cf0d03e669bd42b71d3c9f6e1
Harish Yayi <yharish991@gmail.com>
2021-11-09 12:37:42 -0500

    add default FeatureGate resource
    Signed-off-by: Harish Yayi <yharish991@gmail.com>

--- pkg/v1/tkg/client/upgrade_cluster.go
+++ pkg/v1/tkg/client/upgrade_cluster.go
@@ -349,7 +349,7 @@ func (c *TkgClient) upgradeAddonPreNodeUpgrade(regionalClusterClient clusterclie
 			"addons-management/tanzu-addons-manager",
 			"tkr/tkr-controller",
 			"addons-management/core-package-repo",
-			"packages/management-package-repo," +
+			"packages/management-package-repo",
 			"packages/management-package")
 	}
 	upgradeClusterMetadataOptions := &UpgradeAddonOptions{
