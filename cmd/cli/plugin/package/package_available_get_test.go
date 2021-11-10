916e861677b60db07cb8fc00e1e9c33fc0a85e3f
Daniel Guo <danniel1205@gmail.com>
2021-11-09 16:23:54 -0800

    fix a bug when getting values schema in nested schema structure
    Signed-off-by: Daniel Guo <danniel1205@gmail.com>

--- cmd/cli/plugin/package/package_available_get_test.go
+++ cmd/cli/plugin/package/package_available_get_test.go
@@ -20,65 +20,116 @@ import (
 
 // This is a dummy package schema copied from external-dns
 var rawValidDummyPackageSchema = `
-title: external-dns.community.tanzu.vmware.com.0.8.0 values schema
-examples:
-  - namespace: tanzu-system-service-discovery
-    deployment:
-      args:
-        - --source=service
-        - --txt-owner-id=k8s
-        - --domain-filter=k8s.example.org
-        - --namespace=tanzu-system-service-discovery
-        - --provider=rfc2136
-        - --rfc2136-host=100.69.97.77
-        - --rfc2136-port=53
-        - --rfc2136-zone=k8s.example.org
-        - --rfc2136-tsig-secret=MTlQs3NNU=
-        - --rfc2136-tsig-secret-alg=hmac-sha256
-        - --rfc2136-tsig-keyname=externaldns-key
-        - --rfc2136-tsig-axfr
-      env: []
-      securityContext: {}
-      volumeMounts: []
-      volumes: []
+additionalProperties: false
 properties:
-  namespace:
-    type: string
-    description: The namespace in which to deploy ExternalDNS.
-    default: external-dns
-    examples:
-      - external-dns
-  deployment:
+  accelerator:
+    additionalProperties: true
+    description: Accelerator configuration
+    properties: {}
     type: object
-    description: Deployment related configuration
+  api_portal:
+    additionalProperties: true
+    description: API Portal configuration
+    properties: {}
+    type: object
+  appliveview:
+    additionalProperties: true
+    description: App Live View configuration
+    properties: {}
+    type: object
+  buildservice:
+    description: Build Service configuration
     properties:
-      args:
-        type: array
-        description: |
-          List of arguments passed via command-line to external-dns.  For
-          more guidance on configuration options for your desired DNS
-          provider, consult the ExternalDNS docs at
-          https://github.com/kubernetes-sigs/external-dns#running-externaldns
-        items:
-          type: string
-      env:
-        type: array
-        description: "List of environment variables to set in the external-dns container."
-        items:
-          $ref: "#/definitions/io.k8s.api.core.v1.EnvVar"
-      securityContext:
-        description: "SecurityContext defines the security options the external-dns container should be run with. More info: https://kubernetes.io/docs/tasks/configure-pod-container/security-context/"
-        $ref: "#/definitions/io.k8s.api.core.v1.SecurityContext"
-      volumeMounts:
-        type: array
-        description: "Pod volumes to mount into the external-dns container's filesystem."
-        items:
-          $ref: "#/definitions/io.k8s.api.core.v1.VolumeMount"
-      volumes:
-        type: array
-        description: "List of volumes that can be mounted by containers belonging to the external-dns pod. More info: https://kubernetes.io/docs/concepts/storage/volumes"
-        items:
-          $ref: "#/definitions/io.k8s.api.core.v1.Volume"
+      ca_cert_data:
+        description: tbs registry ca certificate (used for self signed registry)
+        type: string
+      kp_default_repository:
+        description: docker repository (required)
+        examples:
+        - registry.io/build-service
+        type: string
+      kp_default_repository_password:
+        description: registry password (required)
+        examples:
+        - password
+        type: string
+      kp_default_repository_username:
+        description: registry username (required)
+        examples:
+        - janedoe@vmware.com
+        type: string
+      tanzunet_password:
+        description: tanzunet registry password (required for dependency updater
+          feature)
+        examples:
+        - password
+        type: string
+      tanzunet_username:
+        description: tanzunet registry username (required for dependency updater
+          feature)
+        examples:
+        - janedoe@vmware.com
+        type: string
+    type: object
+  cnrs:
+    additionalProperties: true
+    description: Cloud Native Runtimes configuration
+    properties: {}
+    type: object
+  image_policy_webhook:
+    additionalProperties: true
+    description: Image Policy Webhook configuration
+    properties: {}
+    type: object
+  install_cert_manager:
+    default: true
+    description: Install cert-manager
+    type: boolean
+  install_flux:
+    default: true
+    description: Install FluxCD source-controller
+    type: boolean
+  install_tekton:
+    default: true
+    description: Install Tekton
+    type: boolean
+  learningcenter:
+    additionalProperties: true
+    description: Learning Center configuration
+    properties: {}
+    type: object
+  ootb_supply_chain_basic:
+    additionalProperties: true
+    description: OOTB Supply Chain Basic configuration
+    properties: {}
+    type: object
+  ootb_supply_chain_testing:
+    additionalProperties: true
+    description: OOTB Supply Chain Testing configuration
+    properties: {}
+    type: object
+  ootb_supply_chain_testing_scanning:
+    additionalProperties: true
+    description: OOTB Supply Chain Testing Scanning configuration
+    type: object
+    properties:
+      dummyProperty-1:
+        default: dummyDefault
+        description: this is for testing purposes
+        type: string
+      dummyProperty-2:
+        description: this is for testing purposes
+        type: object
+        properties: {}
+  profile:
+    default: full
+    description: 'Profile to install. Valid values: full, dev-light'
+    type: string
+  tap_gui:
+    additionalProperties: true
+    description: TAP GUI configuration
+    type: object
+type: object
 `
 
 var _ = Describe("PackageAvailableGet", func() {
@@ -109,7 +160,7 @@ var _ = Describe("PackageAvailableGet", func() {
 			Expect(err).To(HaveOccurred())
 		})
 
-		It("should return error if schema is empty", func() {
+		It("should not return error if schema is empty", func() {
 			kappClient.GetPackageReturns(&kapppkg.Package{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      pkgName,
@@ -128,10 +179,10 @@ var _ = Describe("PackageAvailableGet", func() {
 
 			var buf bytes.Buffer
 			err := getValuesSchemaForPackage(pkgNamespace, pkgName, pkgVersion, kappClient, &buf)
-			Expect(err).To(HaveOccurred())
+			Expect(err).To(BeNil())
 		})
 
-		It("should return error if schema is empty", func() {
+		It("should return the values schema without any errors", func() {
 			kappClient.GetPackageReturns(&kapppkg.Package{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      pkgName,
