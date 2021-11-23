23920e8aea2bb08a98ccc4777a2342781fb771c8
Shimon Walner <swalner@vmware.com>
2021-11-23 09:58:44 -0800

    WIP: refactor enums in NODESETTING step

--- pkg/v1/tkg/web/src/app/views/landing/vsphere-wizard/vsphere-wizard.constants.ts
+++ pkg/v1/tkg/web/src/app/views/landing/vsphere-wizard/vsphere-wizard.constants.ts
@@ -18,3 +18,14 @@ export const vSphereNodeTypes: Array<NodeType> = [
         name: 'extra-large (cpu: 8, ram: 32 GB, disk: 80 GB)'
     }
 ];
+
+export enum VsphereField {
+    NODESETTING_CONTROL_PLANE_SETTING = 'controlPlaneSetting',
+    NODESETTING_INSTANCE_TYPE_DEV = 'devInstanceType',
+    NODESETTING_INSTANCE_TYPE_PROD = 'prodInstanceType',
+    NODESETTING_MACHINE_HEALTH_CHECKS_ENABLED = 'machineHealthChecksEnabled',
+    NODESETTING_WORKER_NODE_INSTANCE_TYPE = 'workerNodeInstanceType',
+    NODESETTING_CLUSTER_NAME = 'clusterName',
+    NODESETTING_CONTROL_PLANE_ENDPOINT_IP = 'controlPlaneEndpointIP',
+    NODESETTING_CONTROL_PLANE_ENDPOINT_PROVIDER = 'controlPlaneEndpointProvider',
+}
