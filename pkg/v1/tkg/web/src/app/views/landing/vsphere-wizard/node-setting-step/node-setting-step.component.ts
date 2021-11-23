1afb6a02f66d0819100406fced639a0b6be86637
Shimon Walner <swalner@vmware.com>
2021-11-23 08:30:37 -0800

    WIP: refactor enums in vsphere wizard

--- pkg/v1/tkg/web/src/app/views/landing/vsphere-wizard/node-setting-step/node-setting-step.component.ts
+++ pkg/v1/tkg/web/src/app/views/landing/vsphere-wizard/node-setting-step/node-setting-step.component.ts
@@ -13,12 +13,13 @@ import { VSphereWizardFormService } from 'src/app/shared/service/vsphere-wizard-
  * App imports
  */
 import { IpFamilyEnum, PROVIDERS, Providers } from '../../../../shared/constants/app.constants';
-import { NodeType, vSphereNodeTypes } from '../../wizard/shared/constants/wizard.constants';
+import { NodeType} from '../../wizard/shared/constants/wizard.constants';
 import { StepFormDirective } from '../../wizard/shared/step-form/step-form';
 import { ValidationService } from '../../wizard/shared/validation/validation.service';
 import { KUBE_VIP, NSX_ADVANCED_LOAD_BALANCER } from '../../wizard/shared/components/steps/load-balancer/load-balancer-step.component';
 import Broker from 'src/app/shared/service/broker';
 import { AppEdition } from 'src/app/shared/constants/branding.constants';
+import { vSphereNodeTypes } from "../vsphere-wizard.constants";
 
 @Component({
     selector: 'app-node-setting-step',
---
+++ pkg/v1/tkg/web/src/app/views/landing/vsphere-wizard/vsphere-wizard.constants.ts
@@ -0,0 +1,20 @@
+import {NodeType} from "../wizard/shared/constants/wizard.constants";
+
+export const vSphereNodeTypes: Array<NodeType> = [
+    {
+        id: 'small',
+        name: 'small (cpu: 2, ram: 4 GB, disk: 20 GB)'
+    },
+    {
+        id: 'medium',
+        name: 'medium (cpu: 2, ram: 8 GB, disk: 40 GB)'
+    },
+    {
+        id: 'large',
+        name: 'large (cpu: 4, ram: 16 GB, disk: 40 GB)'
+    },
+    {
+        id: 'extra-large',
+        name: 'extra-large (cpu: 8, ram: 32 GB, disk: 80 GB)'
+    }
+];
