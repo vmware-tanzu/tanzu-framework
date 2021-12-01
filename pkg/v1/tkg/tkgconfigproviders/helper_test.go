be0836ecf915f2a04f1090d059d0629633c16701
Shimon Walner <swalner@vmware.com>
2021-12-01 09:48:47 -0800

    Cleans up tests

--- pkg/v1/tkg/tkgconfigproviders/helper_test.go
+++ pkg/v1/tkg/tkgconfigproviders/helper_test.go
@@ -33,62 +33,62 @@ func TestMapToConfigString(t *testing.T) {
 	shouldBeBlank(t, result, "sending empty map")
 }
 
-func shouldBeEmpty(t *testing.T, description string, result map[string]string) {
+func shouldBeEmpty(t *testing.T, target string, result map[string]string) {
 	if len(result) > 0 {
-		t.Fatalf("Test \"%s\" expected an empty map to result, but got \"%v\"", description, result)
+		t.Fatalf("Test sending string \"%s\" expected an empty map to result, but got \"%v\"", target, result)
 	}
 }
 
-func shouldHaveOneEntry(t *testing.T, description string, result map[string]string, key, value string) {
+func shouldHaveOneEntry(t *testing.T, target string, result map[string]string, key, value string) {
 	if len(result) != 1 {
-		t.Fatalf("Test \"%s\" expected a single-entry map to result, but got \"%v\"", description, result)
+		t.Fatalf("Test sending string \"%s\" expected a single-entry map to result, but got \"%v\"", target, result)
 	} else if result[key] != value {
-		t.Fatalf("Test \"%s\" expected a result of map[%s:%s], but got \"%v\"", description, key, value, result)
+		t.Fatalf("Test sending string \"%s\" expected a result of map[%s:%s], but got \"%v\"", target, key, value, result)
 	}
 }
 
-func shouldHaveTwoEntries(t *testing.T, description string, result map[string]string, key1 string, value1 string, key2 string, value2 string) {
+func shouldHaveTwoEntries(t *testing.T, target string, result map[string]string, key1 string, value1 string, key2 string, value2 string) {
 	if len(result) != 2 {
-		t.Fatalf("Test \"%s\" expected a double-entry map to result, but got \"%v\"", description, result)
+		t.Fatalf("Test sending string \"%s\" expected a double-entry map to result, but got \"%v\"", target, result)
 	} else if result[key1] != value1 || result[key2] != value2 {
-		t.Fatalf("Test \"%s\" expected a result of map[%s:%s %s:%s], but got \"%v\"", description, key1, value1, key2, value2, result)
+		t.Fatalf("Test sending string \"%s\" expected a result of map[%s:%s %s:%s], but got \"%v\"", target, key1, value1, key2, value2, result)
 	}
 }
 
 func TestConfigStringToMap(t *testing.T) {
 	target := ""
 	result := configStringToMap(target)
-	shouldBeEmpty(t, "empty string", result)
+	shouldBeEmpty(t, "", result)
 
 	target = "malformedKeyValue"
 	result = configStringToMap(target)
-	shouldBeEmpty(t, "string of "+target, result)
+	shouldBeEmpty(t, target, result)
 
 	target = "too:many:colons"
 	result = configStringToMap(target)
-	shouldBeEmpty(t, "string of "+target, result)
+	shouldBeEmpty(t, target, result)
 
 	target = "key1:value1"
 	result = configStringToMap(target)
-	shouldHaveOneEntry(t, "string of "+target, result, "key1", "value1")
+	shouldHaveOneEntry(t, target, result, "key1", "value1")
 
 	target = "key:value,"
 	result = configStringToMap(target)
-	shouldHaveOneEntry(t, "string of "+target, result, "key", "value")
+	shouldHaveOneEntry(t, target, result, "key", "value")
 
 	target = "key1:value1,malformedKeyValuePair"
 	result = configStringToMap(target)
-	shouldHaveOneEntry(t, "string of "+target, result, "key1", "value1")
+	shouldHaveOneEntry(t, target, result, "key1", "value1")
 
 	target = "malformedKeyValuePair,key1:value1,"
 	result = configStringToMap(target)
-	shouldHaveOneEntry(t, "string of "+target, result, "key1", "value1")
+	shouldHaveOneEntry(t, target, result, "key1", "value1")
 
 	target = "key1:value1,,,,"
 	result = configStringToMap(target)
-	shouldHaveOneEntry(t, "string of "+target, result, "key1", "value1")
+	shouldHaveOneEntry(t, target, result, "key1", "value1")
 
 	target = "key1:value1,key2:value2,"
 	result = configStringToMap(target)
-	shouldHaveTwoEntries(t, "string "+target, result, "key1", "value1", "key2", "value2")
+	shouldHaveTwoEntries(t, target, result, "key1", "value1", "key2", "value2")
 }
