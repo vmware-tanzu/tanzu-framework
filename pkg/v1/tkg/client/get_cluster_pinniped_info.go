7bb34102c7eab16a70dc16556fc3c5269a617102
Andrew Keesler <akeesler@vmware.com>
2021-12-14 10:39:06 -0500

    "cluster kubeconfig get" doesn't skip TLS verification
    Signed-off-by: Andrew Keesler <akeesler@vmware.com>

--- pkg/v1/tkg/client/get_cluster_pinniped_info.go
+++ pkg/v1/tkg/client/get_cluster_pinniped_info.go
@@ -5,18 +5,13 @@ package client
 
 import (
 	"fmt"
-	"net"
-	"strconv"
 	"time"
 
 	"github.com/pkg/errors"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
-	capi "sigs.k8s.io/cluster-api/api/v1beta1"
 
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/clusterclient"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/log"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/region"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/utils"
 )
@@ -71,22 +66,14 @@ func (c *TkgClient) GetClusterPinnipedInfo(options GetClusterPinnipedInfoOptions
 func (c *TkgClient) GetWCClusterPinnipedInfo(regionalClusterClient clusterclient.Client,
 	curRegion region.RegionContext, options GetClusterPinnipedInfoOptions) (*ClusterPinnipedInfo, error) {
 
-	clusterAPIServerURL, err := c.getClusterAPIServerURL(regionalClusterClient, options.ClusterName, options.Namespace)
+	wcClusterInfo, err := getClusterInfo(regionalClusterClient, options.ClusterName, options.Namespace)
 	if err != nil {
-		return nil, errors.Wrap(err, "failed to get cluster apiserver url from cluster objects")
-	}
-	clusterInfo, err := utils.GetClusterInfoFromCluster(clusterAPIServerURL)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get cluster-info from cluster")
+		return nil, errors.Wrap(err, "failed to get workload cluster information")
 	}
 	// for workload cluster pinniped-info should be available on management cluster
-	mcServerURL, err := utils.GetClusterServerFromKubeconfigAndContext(curRegion.SourceFilePath, curRegion.ContextName)
+	mcClusterInfo, err := getClusterInfo(regionalClusterClient, curRegion.ClusterName, TKGsystemNamespace)
 	if err != nil {
-		return nil, errors.Wrap(err, "unable to get cluster apiserver url")
-	}
-	mcClusterInfo, err := utils.GetClusterInfoFromCluster(mcServerURL)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get management cluster-info")
+		return nil, errors.Wrap(err, "failed to get management cluster information")
 	}
 	managementClusterPinnipedInfo, err := utils.GetPinnipedInfoFromCluster(mcClusterInfo)
 	if err != nil {
@@ -96,7 +83,7 @@ func (c *TkgClient) GetWCClusterPinnipedInfo(regionalClusterClient clusterclient
 		return nil, errors.New("failed to get pinniped-info from management cluster")
 	}
 
-	workloadClusterPinnipedInfo, err := utils.GetPinnipedInfoFromCluster(clusterInfo)
+	workloadClusterPinnipedInfo, err := utils.GetPinnipedInfoFromCluster(wcClusterInfo)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get pinniped-info from workload cluster")
 	}
@@ -112,7 +99,7 @@ func (c *TkgClient) GetWCClusterPinnipedInfo(regionalClusterClient clusterclient
 
 	return &ClusterPinnipedInfo{
 		ClusterName:  options.ClusterName,
-		ClusterInfo:  clusterInfo,
+		ClusterInfo:  wcClusterInfo,
 		PinnipedInfo: pinnipedInfo,
 	}, nil
 }
@@ -167,31 +154,4 @@ func getClusterInfo(
 	}
 
 	return cluster, nil
-}
-
-func (c *TkgClient) getClusterAPIServerURL(regionalClusterClient clusterclient.Client, clusterName, namespace string) (string, error) {
-	var apiServerHost string
-	var apiServerPort string
-	var cluster capi.Cluster
-	err := regionalClusterClient.GetResource(&cluster, clusterName, namespace, nil, nil)
-	if err != nil {
-		if apierrors.IsNotFound(err) {
-			errMsg := fmt.Sprintf("cluster '%s' is not present in namespace '%s' ", clusterName, namespace)
-			log.V(4).Info(errMsg)
-			return "", errors.New(errMsg)
-		}
-		return "", errors.Wrap(err, "failed to get 'cluster' resource")
-	}
-
-	if cluster.Spec.ControlPlaneEndpoint.Host == "" {
-		return "", errors.New("controlplane endpoint 'host' was not set in 'cluster' resource")
-	}
-
-	if cluster.Spec.ControlPlaneEndpoint.Port == 0 {
-		return "", errors.New("controlplane endpoint 'port' was not set in 'cluster' resource")
-	}
-
-	apiServerHost = cluster.Spec.ControlPlaneEndpoint.Host
-	apiServerPort = strconv.Itoa(int(cluster.Spec.ControlPlaneEndpoint.Port))
-	return net.JoinHostPort(apiServerHost, apiServerPort), nil
 }
