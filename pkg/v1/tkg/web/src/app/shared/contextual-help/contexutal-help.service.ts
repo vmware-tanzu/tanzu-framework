b40fdae477a12c2133cbaf389f8b927913eec516
Justin Miclette <miclettej@vmware.com>
2021-12-16 15:36:00 -0800

    Revert "Kickstart UI: fix naming of contextual help service (#1387)"
    This reverts commit 793500359a003e4510231a939d0d284d95f500bf.

--- pkg/v1/tkg/web/src/app/shared/contextual-help/contexutal-help.service.ts
+++ pkg/v1/tkg/web/src/app/shared/contextual-help/contexutal-help.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable({
     providedIn: 'root'
 })
-export class ContextualHelpService {
+export class ContexutalHelpService {
 
     private componentList: any = [];
     constructor() { }
