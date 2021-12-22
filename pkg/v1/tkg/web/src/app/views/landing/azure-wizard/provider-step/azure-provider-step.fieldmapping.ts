b5b3a5e3b6a32067c01820eea446106f7e296bb1
Shimon Walner <swalner@vmware.com>
2021-12-21 17:37:46 -0800

    Corrects AzureProviderStepMapping to use correct fields

--- pkg/v1/tkg/web/src/app/views/landing/azure-wizard/provider-step/azure-provider-step.fieldmapping.ts
+++ pkg/v1/tkg/web/src/app/views/landing/azure-wizard/provider-step/azure-provider-step.fieldmapping.ts
@@ -3,13 +3,16 @@ import { AzureField, ResourceGroupOption } from '../azure-wizard.constants';
 
 export const AzureProviderStepMapping: StepMapping = {
     fieldMappings: [
-        { name: AzureField.VNET_CONTROLPLANE_NEWSUBNET_CIDR, required: true },
-        { name: AzureField.VNET_CONTROLPLANE_NEWSUBNET_NAME, required: true },
-        { name: AzureField.VNET_CONTROLPLANE_SUBNET_NAME, required: true },
-        { name: AzureField.VNET_CUSTOM_NAME, required: true },
-        { name: AzureField.VNET_EXISTING_NAME, required: true },
-        { name: AzureField.VNET_EXISTING_OR_CUSTOM, required: true },
-        { name: AzureField.VNET_RESOURCE_GROUP, required: true },
+        { name: AzureField.PROVIDER_AZURECLOUD, required: true },
+        { name: AzureField.PROVIDER_CLIENT, required: true },
+        { name: AzureField.PROVIDER_CLIENTSECRET, required: true },
+        { name: AzureField.PROVIDER_REGION, required: true },
+        { name: AzureField.PROVIDER_RESOURCEGROUPEXISTING, required: true },
+        { name: AzureField.PROVIDER_RESOURCEGROUPCUSTOM },
+        { name: AzureField.PROVIDER_RESOURCEGROUPOPTION, required: true },
+        { name: AzureField.PROVIDER_SSHPUBLICKEY, required: true },
+        { name: AzureField.PROVIDER_SUBSCRIPTION, required: true },
+        { name: AzureField.PROVIDER_TENANT, required: true },
     ]
 }
 
