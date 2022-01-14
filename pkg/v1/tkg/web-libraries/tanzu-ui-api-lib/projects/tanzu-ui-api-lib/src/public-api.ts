14d4139ee07ccf81048b80979b1a8dc590270a9b
Justin Miclette <miclettej@vmware.com>
2022-01-14 12:13:18 -0800

    remove unused files, add build script

--- pkg/v1/tkg/web-libraries/tanzu-ui-api-lib/projects/tanzu-ui-api-lib/src/public-api.ts
+++ pkg/v1/tkg/web-libraries/tanzu-ui-api-lib/projects/tanzu-ui-api-lib/src/public-api.ts
@@ -1,9 +1,6 @@
 /*
  * Public API Surface of tanzu-ui-api-lib
+ * Exports swagger generated APIClient and modules, and swagger generated models
  */
-
-export * from './lib/tanzu-ui-api-lib.service';
-export * from './lib/tanzu-ui-api-lib.component';
-export * from './lib/tanzu-ui-api-lib.module';
 export * from './lib/swagger/index';
 export * from './lib/swagger/models/index';
