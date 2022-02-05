2b5bbb35866bf37c6559968c8f92ee10ecd16483
Giri <gelumalai@vmware.com>
2022-02-04 18:19:30 -0800

    CLI: Add pre-download verificatin for HTTP plugin artifacts
    Currently, this trusts any local path and the "tanzu-cli-advanced-plugins"
    GCP bucket where TMC plugins are stored. Other exceptions can be added
    as and when necessary.
    
    Also, switches the default behavior from allow to deny.
    
    This is needed to tighten the security hooks and disallow installation
    of plugins from untrusted locations in case a user logs into an
    untrusted cluster.
    
    Signed-off-by: Giri <gelumalai@vmware.com>

--- pkg/v1/cli/pluginmanager/manager.go
+++ pkg/v1/cli/pluginmanager/manager.go
@@ -23,6 +23,7 @@ import (
 	cliv1alpha1 "github.com/vmware-tanzu/tanzu-framework/apis/cli/v1alpha1"
 	"github.com/vmware-tanzu/tanzu-framework/apis/config/v1alpha1"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli/artifact"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli/catalog"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli/common"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/cli/discovery"
@@ -385,7 +386,7 @@ func installOrUpgradePlugin(serverName string, p *plugin.Discovered, version str
 	log.Infof("Installing plugin '%v:%v'", p.Name, version)
 
 	// verify plugin before installation
-	err := verifyPlugin(p)
+	err := verifyPluginPreDownload(p)
 	if err != nil {
 		return errors.Wrapf(err, "%q plugin verification failed", p.Name)
 	}
@@ -712,11 +713,9 @@ func getPluginDescriptorResource(pluginFilePath string) (*cliv1alpha1.PluginDesc
 	return &pd, nil
 }
 
-// verifyPlugin verifies the plugin source is allowed to be used
-// and returns error if the verification fails
-// Currently this function only focuses on the verification of the OCI registry
-// based plugin artifacts by verifying the registry source
-func verifyPlugin(p *plugin.Discovered) error {
+// verifyPluginPreDownload verifies that the plugin distribution repo is trusted
+// and returns error if the verification fails.
+func verifyPluginPreDownload(p *plugin.Discovered) error {
 	artifactInfo, err := p.Distribution.DescribeArtifact(p.RecommendedVersion, runtime.GOOS, runtime.GOARCH)
 	if err != nil {
 		return err
@@ -724,7 +723,10 @@ func verifyPlugin(p *plugin.Discovered) error {
 	if artifactInfo.Image != "" {
 		return verifyRegistry(artifactInfo.Image)
 	}
-	return nil
+	if artifactInfo.URI != "" {
+		return verifyArtifactLocation(artifactInfo.URI)
+	}
+	return errors.Errorf("no download information available for artifact \"%s:%s:%s:%s\"", p.Name, p.RecommendedVersion, runtime.GOOS, runtime.GOARCH)
 }
 
 // verifyRegistry verifies the authenticity of the registry from where cli is
@@ -738,4 +740,29 @@ func verifyRegistry(image string) error {
 		}
 	}
 	return errors.Errorf("untrusted registry detected with image %q. Allowed registries are %v", image, trustedRegistries)
+}
+
+// verifyArtifactLocation verifies the artifact location from where the cli is
+// trying to download the plugins by comparing it with the list of trusted locations
+func verifyArtifactLocation(uri string) error {
+	art, err := artifact.NewURIArtifact(uri)
+	if err != nil {
+		return err
+	}
+
+	switch art.(type) {
+	case *artifact.LocalArtifact:
+		// trust local artifacts implicitly
+		return nil
+
+	default:
+		trustedLocations := config.GetTrustedArtifactLocations()
+		for _, tl := range trustedLocations {
+			// Verify that the URI has a trusted location as the prefix
+			if tl != "" && strings.HasPrefix(uri, tl) {
+				return nil
+			}
+		}
+		return errors.Errorf("untrusted artifact location detected with URI %q. Allowed locations are %v", uri, trustedLocations)
+	}
 }
