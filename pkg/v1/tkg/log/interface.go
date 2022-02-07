ff66b0d801a52cbd1e71574a1326a117f16f6660
Anuj Chaudhari <anujc@vmware.com>
2022-02-07 16:05:58 +0530

    Fix build and dependency issues after CAPI v1.1.0 update

--- pkg/v1/tkg/log/interface.go
+++ pkg/v1/tkg/log/interface.go
@@ -7,13 +7,49 @@ package log
 import (
 	"fmt"
 	"os"
-
-	"github.com/go-logr/logr"
 )
 
 // LoggerImpl represents the ability to log messages, both errors and not.
 type LoggerImpl interface {
-	logr.Logger
+	// Enabled tests whether this Logger is enabled.  For example, commandline
+	// flags might be used to set the logging verbosity and disable some info
+	// logs.
+	Enabled() bool
+
+	// Info logs a non-error message with the given key/value pairs as context.
+	//
+	// The msg argument should be used to add some constant description to
+	// the log line.  The key/value pairs can then be used to add additional
+	// variable information.  The key/value pairs should alternate string
+	// keys and arbitrary values.
+	Info(msg string, keysAndValues ...interface{})
+
+	// Error logs an error, with the given message and key/value pairs as context.
+	// It functions similarly to calling Info with the "error" named value, but may
+	// have unique behavior, and should be preferred for logging errors (see the
+	// package documentations for more information).
+	//
+	// The msg field should be used to add context to any underlying error,
+	// while the err field should be used to attach the actual error that
+	// triggered this log line, if present.
+	Error(err error, msg string, keysAndValues ...interface{})
+
+	// V returns an Logger value for a specific verbosity level, relative to
+	// this Logger.  In other words, V values are additive.  V higher verbosity
+	// level means a log message is less important.  It's illegal to pass a log
+	// level less than zero.
+	V(level int) LoggerImpl
+
+	// WithValues adds some key-value pairs of context to a logger.
+	// See Info for documentation on how key/value pairs work.
+	WithValues(keysAndValues ...interface{}) LoggerImpl
+
+	// WithName adds a new element to the logger's name.
+	// Successive calls with WithName continue to append
+	// suffixes to the logger's name.  It's strongly recommended
+	// that name segments contain only letters, digits, and hyphens
+	// (see the package documentation for more information).
+	WithName(name string) LoggerImpl
 
 	// Infof logs a non-error messages with the given message format with format specifier and arguments.
 	Infof(format string, args ...interface{})
@@ -81,17 +117,17 @@ func V(level int) LoggerImpl {
 }
 
 // WithName adds a new element to the logger's name.
-func WithName(name string) logr.Logger {
+func WithName(name string) LoggerImpl {
 	return l.WithName(name)
 }
 
 // WithValues adds some key-value pairs of context to a logger.
-func WithValues(kvList ...interface{}) logr.Logger {
+func WithValues(kvList ...interface{}) LoggerImpl {
 	return l.WithValues(kvList...)
 }
 
 // GetLogr logs a warning messages with the given message format with format specifier and arguments.
-func GetLogr() logr.Logger {
+func GetLogr() LoggerImpl {
 	return l
 }
 
