e99da8b69a6985d75eeb1f0d1f41383581a2df7a
Sriraman S <ssriraman@vmware.com>
2022-02-17 11:50:12 +0530

    refactor: 💡 Resolve comments from PR 1705
    Resolve the comments in .gitignore, documentations and copyright related
    changes suggested in PR 1705
    
    BREAKING CHANGE: 🧨 N/A

--- hack/fetch-install-tf.sh
+++ hack/fetch-install-tf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2021 VMware, Inc. All Rights Reserved.
+# Copyright 2022 VMware, Inc. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 # Inspired by - https://github.com/vmware-tanzu/community-edition/blob/main/hack/install.sh
