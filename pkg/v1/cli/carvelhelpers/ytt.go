c831608f342282b82fbbc698d34864843030b219
PremKumar Kalle <pkalle@vmware.com>
2022-02-24 15:45:17 -0800

    Update ytt module to github.com/vmware-tanzu/carvel-ytt v0.40.0
    Signed-off-by: PremKumar Kalle <pkalle@vmware.com>

--- pkg/v1/cli/carvelhelpers/ytt.go
+++ pkg/v1/cli/carvelhelpers/ytt.go
@@ -6,10 +6,10 @@ package carvelhelpers
 import (
 	"io"
 
-	yttui "github.com/k14s/ytt/pkg/cmd/ui"
-	"github.com/k14s/ytt/pkg/files"
-	"github.com/k14s/ytt/pkg/schema"
-	"github.com/k14s/ytt/pkg/workspace"
+	yttui "github.com/vmware-tanzu/carvel-ytt/pkg/cmd/ui"
+	"github.com/vmware-tanzu/carvel-ytt/pkg/files"
+	"github.com/vmware-tanzu/carvel-ytt/pkg/workspace"
+	"github.com/vmware-tanzu/carvel-ytt/pkg/workspace/datavalues"
 )
 
 // ProcessYTTPackage processes configuration directory with ytt tool
@@ -25,11 +25,11 @@ func ProcessYTTPackage(configDir string) ([]byte, error) {
 	libExecFact := workspace.NewLibraryExecutionFactory(&NoopUI{}, workspace.TemplateLoaderOpts{})
 	loader := libExecFact.New(libCtx)
 
-	valuesDoc, libraryValueDoc, err := loader.Values([]*workspace.DataValues{}, schema.NewNullSchema())
+	valuesDoc, libraryValueDoc, err := loader.Values([]*datavalues.Envelope{}, datavalues.NewNullSchema())
 	if err != nil {
 		return nil, err
 	}
-	result, err := loader.Eval(valuesDoc, libraryValueDoc, []*schema.DocumentSchemaEnvelope{})
+	result, err := loader.Eval(valuesDoc, libraryValueDoc, []*datavalues.SchemaEnvelope{})
 	if err != nil {
 		return nil, err
 	}
