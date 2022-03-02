6061541d26879efbc053c07de9073caeb4232018
Shashank Girish <girishsh@sjc-ubu-eng-101.vmware.com>
2022-03-02 05:48:34 -0800

    Make v1alpha3 TKR API the hub type.
    Signed-off-by: Shashank Girish <girishsh@vmware.com>

---
+++ apis/run/v1alpha3/conversion.go
@@ -0,0 +1,15 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha3
+
+func (*ClusterBootstrap) Hub()             {}
+func (*ClusterBootstrapList) Hub()         {}
+func (*ClusterBootstrapTemplate) Hub()     {}
+func (*ClusterBootstrapTemplateList) Hub() {}
+func (*KappControllerConfig) Hub()         {}
+func (*KappControllerConfigList) Hub()     {}
+func (*OSImage) Hub()                      {}
+func (*OSImageList) Hub()                  {}
+func (*TanzuKubernetesRelease) Hub()       {}
+func (*TanzuKubernetesReleaseList) Hub()   {}
