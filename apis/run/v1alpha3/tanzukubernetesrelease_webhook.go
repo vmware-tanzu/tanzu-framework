438a0b61ffba64c0728668738f171e66513b32e5
Shashank Girish <girishsh@vmware.com>
2022-03-16 15:52:13 +0530

    Remove deadcode in v1alpha3 tkr webhook.
    Signed-off-by: Shashank Girish <girishsh@vmware.com>

--- apis/run/v1alpha3/tanzukubernetesrelease_webhook.go
+++ apis/run/v1alpha3/tanzukubernetesrelease_webhook.go
@@ -5,16 +5,9 @@ package v1alpha3
 
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
-// log is for logging in this package.
-var tanzukubernetesreleaselog = logf.Log.WithName("tanzukubernetesrelease-resource")
-
 func (r *TanzuKubernetesRelease) SetupWebhookWithManager(mgr ctrl.Manager) error {
-	// TODO: Remove logger call below, as it is only used to prevent deadcode lint errors.
-	tanzukubernetesreleaselog.Info("TODO: Remove this - this is only to pass lint")
-
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(r).
 		Complete()
