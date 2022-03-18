d2680d50ba976761e8f407516618d78253350859
ShashankGirish <shashankgirishr@gmail.com>
2022-03-18 16:32:39 +0530

    TKR api conversion webhooks. (#1828)
    Implement TKR conversion webhooks.
    
    * Introduce conversion-gen module k8s.io/code-generator/cmd/conversion-gen v0.23.4. This is currently configured to generate conversion code for the apis/run/v1alpha1 APIs. Generation can be triggered using the `generate-go-conversion`
    makefile recipe.
    * Make v1alpha3 TKR API the hub type.
    * Add lint exclusions for API conversion code.
    * Add conversions for `TanzuKubernetesReleaseSpec`.
    * Introduces util/conversion package which functions that help with the
    conversions, and also help with testing loss-less conversion.
    * Generate webhooks for TanzuKubernetesRelease types.
    * Generate the webhooks for both v1alpha1 and v1alpha3 TanzuKubernetesRelease types. As part of the API generation, it also updates the PROJECT file and generates config/rbac yamls and an incomplete sample yaml.
    * Add tkr-conversion webhook under pkg/v2/tkr.
    
    Signed-off-by: Shashank Girish <girishsh@vmware.com>

---
+++ apis/run/v1alpha3/conversion.go
@@ -0,0 +1,7 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha3
+
+func (*TanzuKubernetesRelease) Hub()     {}
+func (*TanzuKubernetesReleaseList) Hub() {}
