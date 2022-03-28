5b8a9c2f0f3c853bfdfd35cb140e83df3c065694
Sudarshan Aji <57639960+saji-pivotal@users.noreply.github.com>
2022-03-28 10:43:25 -0700

    Removed slash from the custom image repo being read in as hostname (#1922)

---
+++ pkg/v1/tkg/kind/utils.go
@@ -0,0 +1,18 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package kind provides kind cluster functionalities
+package kind
+
+import "strings"
+
+// ResolveHostname either gets the hostname of the custom image repo or the default image repo
+func (k *KindClusterProxy) ResolveHostname(repositoryPath string) string {
+	hostname := ""
+	if repositoryPath != "" {
+		hostname = strings.Split(repositoryPath, "/")[0]
+	} else {
+		hostname = strings.Split(k.options.DefaultImageRepo, "/")[0]
+	}
+	return hostname
+}
