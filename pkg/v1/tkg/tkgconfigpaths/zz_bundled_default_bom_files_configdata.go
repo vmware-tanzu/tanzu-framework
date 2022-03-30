f452be0d4d8f4408f148ec3e1d5078644fca649c
Sriraman S <ssriraman@vmware.com>
2022-03-29 23:30:12 -0700

    Updating version tag used for BOM retrieval to v0.20.0
    Signed-off-by: Sriraman S <ssriraman@vmware.com>

--- pkg/v1/tkg/tkgconfigpaths/zz_bundled_default_bom_files_configdata.go
+++ pkg/v1/tkg/tkgconfigpaths/zz_bundled_default_bom_files_configdata.go
@@ -12,5 +12,5 @@ package tkgconfigpaths
 var (
 	TKGDefaultImageRepo               string = "projects-stg.registry.vmware.com/tkg"
 	TKGDefaultCompatibilityImagePath  string = "framework-zshippable/tkg-compatibility"
-	TKGManagementClusterPluginVersion string = "v0.20.0-dev"
+	TKGManagementClusterPluginVersion string = "v0.20.0"
 )
