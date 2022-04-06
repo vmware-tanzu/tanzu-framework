a9b8f3a7fc2ea45863a46a077be784e79ead0110
Navid Shaikh <navids@vmware.com>
2022-04-06 14:26:01 +0530

    [release-0.11] Update version for BOM retrieval to v0.11.4 (#1953)
    for tanzu-framework v0.11.4 release
    
    Signed-off-by: Navid Shaikh <navids@vmware.com>

--- pkg/v1/tkg/tkgconfigpaths/zz_bundled_default_bom_files_configdata.go
+++ pkg/v1/tkg/tkgconfigpaths/zz_bundled_default_bom_files_configdata.go
@@ -12,5 +12,5 @@ package tkgconfigpaths
 var (
 	TKGDefaultImageRepo               string = "projects-stg.registry.vmware.com/tkg"
 	TKGDefaultCompatibilityImagePath  string = "framework-zshippable/tkg-compatibility"
-	TKGManagementClusterPluginVersion string = "v0.11.3"
+	TKGManagementClusterPluginVersion string = "v0.11.4"
 )
