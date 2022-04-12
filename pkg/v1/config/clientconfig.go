074b6e6ce4d4ed966cd222c8270fb82bce9dc623
Giri <gelumalai@vmware.com>
2022-04-12 15:54:57 -0700

    CLI: Add context types in client config
    Context replaces Server in the config and will be used for explicit
    context management via the core CLI using a dedicated context command.
    To start with we would support 2 types of contexts:
    1. Kubernetes (management cluster & workload cluster)
    2. Tanzu Mission Control
    
    Also, add conversion between server and context types for backwards
    compatibility with older core and older plugins.
    
    Signed-off-by: Giri <gelumalai@vmware.com>

--- pkg/v1/config/clientconfig.go
+++ pkg/v1/config/clientconfig.go
@@ -335,6 +335,24 @@ func GetClientConfig() (cfg *configv1alpha1.ClientConfig, err error) {
 		return nil, errors.Wrap(err, "could not decode config file")
 	}
 
+<<<<<<< HEAD
+=======
+	addedDefaultDiscovery := populateDefaultStandaloneDiscovery(&c)
+	addedFeatureFlags := addDefaultFeatureFlagsIfMissing(&c, DefaultCliFeatureFlags)
+	addedEdition := addDefaultEditionIfMissing(&c)
+	addedBomRepo := addBomRepoIfMissing(&c)
+	addedCompatabilityFile := addCompatibilityFileIfMissing(&c)
+	// contexts could be lost when older plugins edit the config, so populate them from servers
+	addedContexts, err := populateContexts(&c)
+	if err != nil {
+		return nil, err
+	}
+
+	if addedFeatureFlags || addedDefaultDiscovery || addedEdition || addedCompatabilityFile || addedBomRepo || addedContexts {
+		_ = StoreClientConfig(&c)
+	}
+
+>>>>>>> 4435e9cb (CLI: Add context types in client config)
 	return &c, nil
 }
 
@@ -423,6 +441,14 @@ func storeConfigToLegacyDir(data []byte) {
 // Make sure to Acquire and Release tanzu lock when reading/writing to the
 // tanzu client configuration
 func StoreClientConfig(cfg *configv1alpha1.ClientConfig) error {
+	// new plugins would be setting only contexts, so populate servers for backwards compatibility
+	populateServers(cfg)
+	// old plugins would be setting only servers, so populate contexts for forwards compatibility
+	_, err := populateContexts(cfg)
+	if err != nil {
+		return err
+	}
+
 	cfgPath, err := ClientConfigPath()
 	if err != nil {
 		return errors.Wrap(err, "could not find config path")
@@ -519,14 +545,23 @@ func AddServer(s *configv1alpha1.Server, setCurrent bool) error {
 	if err != nil {
 		return err
 	}
+
 	for _, server := range cfg.KnownServers {
 		if server.Name == s.Name {
 			return fmt.Errorf("server %q already exists", s.Name)
 		}
 	}
+
 	cfg.KnownServers = append(cfg.KnownServers, s)
+	c := convertServerToContext(s)
+	cfg.KnownContexts = append(cfg.KnownContexts, c)
+
 	if setCurrent {
 		cfg.CurrentServer = s.Name
+		err = cfg.SetCurrentContext(c.Type, c.Name)
+		if err != nil {
+			return err
+		}
 	}
 	return StoreClientConfig(cfg)
 }
@@ -541,6 +576,7 @@ func PutServer(s *configv1alpha1.Server, setCurrent bool) error {
 	if err != nil {
 		return err
 	}
+
 	newServers := []*configv1alpha1.Server{s}
 	for _, server := range cfg.KnownServers {
 		if server.Name == s.Name {
@@ -549,8 +585,23 @@ func PutServer(s *configv1alpha1.Server, setCurrent bool) error {
 		newServers = append(newServers, server)
 	}
 	cfg.KnownServers = newServers
+
+	c := convertServerToContext(s)
+	newContexts := []*configv1alpha1.Context{c}
+	for _, ctx := range cfg.KnownContexts {
+		if ctx.Name == c.Name {
+			continue
+		}
+		newContexts = append(newContexts, ctx)
+	}
+	cfg.KnownContexts = newContexts
+
 	if setCurrent {
 		cfg.CurrentServer = s.Name
+		err = cfg.SetCurrentContext(c.Type, c.Name)
+		if err != nil {
+			return err
+		}
 	}
 	return StoreClientConfig(cfg)
 }
@@ -574,8 +625,23 @@ func RemoveServer(name string) error {
 	}
 	cfg.KnownServers = newServers
 
+	var c *configv1alpha1.Context
+	newContexts := []*configv1alpha1.Context{}
+	for _, ctx := range cfg.KnownContexts {
+		if ctx.Name != name {
+			newContexts = append(newContexts, ctx)
+		} else {
+			c = ctx
+		}
+	}
+	cfg.KnownContexts = newContexts
+
 	if cfg.CurrentServer == name {
 		cfg.CurrentServer = ""
+	}
+
+	if cfg.CurrentContext[c.Type] == name {
+		delete(cfg.CurrentContext, c.Type)
 	}
 
 	err = StoreClientConfig(cfg)
@@ -605,6 +671,16 @@ func SetCurrentServer(name string) error {
 		return fmt.Errorf("could not set current server; %q is not a known server", name)
 	}
 	cfg.CurrentServer = name
+
+	c, err := cfg.GetContext(name)
+	if err != nil {
+		return err
+	}
+	err = cfg.SetCurrentContext(c.Type, c.Name)
+	if err != nil {
+		return err
+	}
+
 	err = StoreClientConfig(cfg)
 	if err != nil {
 		return err
@@ -632,6 +708,18 @@ func EndpointFromServer(s *configv1alpha1.Server) (endpoint string, err error) {
 	case configv1alpha1.ManagementClusterServerType:
 		return s.ManagementClusterOpts.Endpoint, nil
 	case configv1alpha1.GlobalServerType:
+		return s.GlobalOpts.Endpoint, nil
+	default:
+		return endpoint, fmt.Errorf("unknown server type %q", s.Type)
+	}
+}
+
+// EndpointFromContext returns the endpoint from context.
+func EndpointFromContext(s *configv1alpha1.Context) (endpoint string, err error) {
+	switch s.Type {
+	case configv1alpha1.CtxTypeK8s:
+		return s.ClusterOpts.Endpoint, nil
+	case configv1alpha1.CtxTypeTMC:
 		return s.GlobalOpts.Endpoint, nil
 	default:
 		return endpoint, fmt.Errorf("unknown server type %q", s.Type)
