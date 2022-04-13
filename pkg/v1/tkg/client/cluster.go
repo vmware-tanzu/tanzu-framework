72212cff2131371374b37a533e6ceb545ae2c4da
Anuj Chaudhari <anujc@vmware.com>
2022-04-13 11:20:12 -0700

    Gate the legacy configuration file prompt flow with features.global.package-based-lcm feature-flag

--- pkg/v1/tkg/client/cluster.go
+++ pkg/v1/tkg/client/cluster.go
@@ -150,26 +150,30 @@ func (c *TkgClient) CreateCluster(options *CreateClusterOptions, waitForCluster 
 			return false, errors.Wrap(err, "unable to get cluster configuration")
 		}
 
-		clusterConfigDir, err := c.tkgConfigPathsClient.GetClusterConfigurationDirectory()
-		if err != nil {
-			return false, err
-		}
-		configFilePath = filepath.Join(clusterConfigDir, fmt.Sprintf("%s.yaml", options.ClusterName))
-		err = utils.SaveFile(configFilePath, bytes)
-		if err != nil {
-			return false, err
-		}
+		// If `features.global.package-based-lcm` feature-flag is activated than
+		// save the configuration to the file and apply it based on feature-flag
+		if config.IsFeatureActivated(config.FeatureFlagPackageBasedLCM) {
+			clusterConfigDir, err := c.tkgConfigPathsClient.GetClusterConfigurationDirectory()
+			if err != nil {
+				return false, err
+			}
+			configFilePath = filepath.Join(clusterConfigDir, fmt.Sprintf("%s.yaml", options.ClusterName))
+			err = utils.SaveFile(configFilePath, bytes)
+			if err != nil {
+				return false, err
+			}
 
-		log.Warningf("\nLegacy configuration file detected. The inputs from said file have been converted into the new Cluster configuration as '%v'", configFilePath)
+			log.Warningf("\nLegacy configuration file detected. The inputs from said file have been converted into the new Cluster configuration as '%v'", configFilePath)
 
-		// If `features.cluster.auto-apply-generated-clusterclass-based-configuration` feature-flag is not activated
-		// log command to use to create cluster using ClusterClass based config file and return
-		if !config.IsFeatureActivated(config.FeatureFlagAutoApplyGeneratedClusterClassBasedConfiguration) {
-			log.Warningf("\nTo create a cluster with it, use")
-			log.Warningf("    tanzu cluster create --file %v", configFilePath)
-			return false, nil
+			// If `features.cluster.auto-apply-generated-clusterclass-based-configuration` feature-flag is not activated
+			// log command to use to create cluster using ClusterClass based config file and return
+			if !config.IsFeatureActivated(config.FeatureFlagAutoApplyGeneratedClusterClassBasedConfiguration) {
+				log.Warningf("\nTo create a cluster with it, use")
+				log.Warningf("    tanzu cluster create --file %v", configFilePath)
+				return false, nil
+			}
+			log.Warningf("\nUsing this new Cluster configuration '%v' to create the cluster.\n", configFilePath)
 		}
-		log.Warningf("\nUsing this new Cluster configuration '%v' to create the cluster.\n", configFilePath)
 	}
 
 	clusters, err := regionalClusterClient.ListClusters("")
