2880e204736a632561f0b585468791d3a03d4445
Sai Bandi <saib@vmware.com>
2022-05-12 11:39:10 -0700

    Add github action to publish management packages

--- pkg/v1/tkg/test/tkgctl/aws_cc/aws_cc_suite_test.go
+++ pkg/v1/tkg/test/tkgctl/aws_cc/aws_cc_suite_test.go
@@ -89,10 +89,6 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 		exec.WithStdout(GinkgoWriter),
 	)
 
-	os.Setenv("_MANAGEMENT_PACKAGE_REPO_IMAGE", "gcr.io/eminent-nation-87317/tkg/test20/management:v0.21.0")
-	os.Setenv("_MANAGEMENT_PACKAGE_VERSION", "0.21.0")
-	os.Setenv("_ADDITIONAL_MANAGEMENT_COMPONENT_CONFIGURATION_FILE", "https://gist.githubusercontent.com/anujc25/cf8b5ce3e9e241527e3af881d5d748c1/raw/32dda7874da12f98c05d6d5400cdde2f5334a90d/tkr-addons-resources-v1.23.5.yaml")
-
 	fmt.Println("Executing the hack script")
 	out, cmdErr, err := hackCmd.Run(context.Background())
 	fmt.Println(string(out))
