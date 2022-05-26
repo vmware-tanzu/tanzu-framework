66e5ec0c739124504b5329c840b22f1606f5ab20
Chenrui Li <lchenrui@vmware.com>
2022-05-26 11:52:36 +0800

    Update SkipCNIBinaries param to CalicoConfig (#2416)

--- apis/cni/v1alpha1/calicoconfig_types.go
+++ apis/cni/v1alpha1/calicoconfig_types.go
@@ -7,26 +7,35 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// CalicoConfigSpec defines the desired state of CalicoConfig
+// CalicoConfigSpec defines the desired state of CalicoConfig.
 type CalicoConfigSpec struct {
 	Calico Calico `json:"calico,omitempty"`
 }
 
+// Calico stores the configurations for Calico.
 type Calico struct {
 	Config CalicoConfigDataValue `json:"config,omitempty"`
 }
 
+// CalicoConfigDataValue contains the specific configurations for the Calico package.
 type CalicoConfigDataValue struct {
-	// Maximum transmission unit setting. "0" as default means MTU will be auto detected
-	//+ kubebuilder:validation:Optional
+	// VethMTU defines maximum transmission unit. "0" as default means MTU will be auto detected.
+	//+kubebuilder:validation:Optional
 	//+kubebuilder:validation:Minimum=0
 	//+kubebuilder:default:=0
 	VethMTU int64 `json:"vethMTU,omitempty"`
+
+	// SkipCNIBinaries allows to skip the cni plugin binaries installation.
+	// Default to false. Set to true for providers who already installed
+	// cni plugin binaries in their OVAs and do not want Calico to overwrite them.
+	//+kubebuilder:validation:Optional
+	//+kubebuilder:default:=false
+	SkipCNIBinaries bool `json:"skipCNIBinaries,omitempty"`
 }
 
-// CalicoConfigStatus defines the observed state of CalicoConfig
+// CalicoConfigStatus defines the observed state of CalicoConfig.
 type CalicoConfigStatus struct {
-	// Name of the data value secret created by calico controller
+	// SecretRef is the name of the data value secret created by calico controller.
 	//+ kubebuilder:validation:Optional
 	SecretRef string `json:"secretRef,omitempty"`
 }
@@ -34,11 +43,10 @@ type CalicoConfigStatus struct {
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 //+kubebuilder:resource:path=calicoconfigs,shortName=calicoconf,scope=Namespaced
-//+kubebuilder:printcolumn:name="Namespace",type="string",JSONPath=".spec.cni.namespace",description="The namespace in which calico is deployed"
-//+kubebuilder:printcolumn:name="VethMTU",type="string",JSONPath=".spec.cni.calico.config.vethMTU",description="Maximum transmission unit setting. '0' as default means MTU will be auto detected"
+//+kubebuilder:printcolumn:name="VethMTU",type="integer",JSONPath=".spec.calico.config.vethMTU",description="Maximum transmission unit setting. '0' as default means MTU will be auto detected"
 //+kubebuilder:printcolumn:name="SecretRef",type="string",JSONPath=".status.secretRef",description="Name of the Calico data values secret"
 
-// CalicoConfig is the Schema for the calicoconfigs API
+// CalicoConfig is the Schema for the calicoconfigs API.
 type CalicoConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -49,7 +57,7 @@ type CalicoConfig struct {
 
 //+kubebuilder:object:root=true
 
-// CalicoConfigList contains a list of CalicoConfig
+// CalicoConfigList contains a list of CalicoConfig.
 type CalicoConfigList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
