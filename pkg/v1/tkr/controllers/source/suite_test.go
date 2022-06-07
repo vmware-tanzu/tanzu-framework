vmware-tanzu/tanzu-framework

commit 44942217640a9023daef2f853477a388b200806e

Install TKR package contents directly
TKR package contents get automatically installed without the use of
Carvel PackageInstall resource (which doesn't support shared resources
across packages).

This is necessary because a TKR package may contain resources that are
reused by other TKR packages, e.g. different TKRs shipping the same
Kubernetes version may share some OSImages or addon packages
(addon packages may even be reused across K8s versions).

Inject Registry as an interface

This improves testability and modularity of affected components, and
reuse of Registry as a component. Now, both Fetcher and TKR Package
Reconciler re-use the same Registry instance provided externally.

Only fetch compatible TKR BOMs and TKR packages

We're also now using compatibility information to download only
compatible TKR packages and TKR BOMs into the management cluster.

Signed-off-by: Ivan Mikushin <imikushin@vmware.com>

pkg/v1/tkr/pkg/types/bommetadata.go
@@ -5,8 +5,8 @@ package types
 
 // ManagementClusterVersion contains kubernetes versions that are supported by the management cluster with a certain TKG version.
 type ManagementClusterVersion struct {
-	TKGVersion                  string   `yaml:"version"`
-	SupportedKubernetesVersions []string `yaml:"supportedKubernetesVersions"`
+	TKGVersion                  string   `json:"version"`
+	SupportedKubernetesVersions []string `json:"supportedKubernetesVersions"`
 }
 
 // CompatibilityMetadata contains tanzu release support matrix

pkg/v1/tkr/controllers/source/tkr_source_controller.go
@@ -15,7 +15,6 @@ import (
 	"github.com/go-logr/logr"
 	ctlimg "github.com/k14s/imgpkg/pkg/imgpkg/registry"
 	"github.com/pkg/errors"
-	"gopkg.in/yaml.v2"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -33,6 +32,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	"sigs.k8s.io/yaml"
 
 	runv1 "github.com/vmware-tanzu/tanzu-framework/apis/run/v1alpha1"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/constants"

pkg/v2/tkr/controller/tkr-source/compatibility/reconciler.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
-	"gopkg.in/yaml.v3"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/labels"
@@ -22,18 +21,27 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	"sigs.k8s.io/yaml"
 
 	runv1 "github.com/vmware-tanzu/tanzu-framework/apis/run/v1alpha3"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/constants"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/types"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/util/patchset"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/util/sets"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/util/version"
 )
 
 type Reconciler struct {
+	version.Compatibility
+
 	Ctx    context.Context
 	Log    logr.Logger
 	Client client.Client
+	Config Config
+}
 
+type Compatibility struct {
+	Client client.Client
 	Config Config
 }
 
@@ -112,12 +120,12 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ct
 }
 
 func (r *Reconciler) updateTKRCompatibleCondition(ctx context.Context, tkr *runv1.TanzuKubernetesRelease) error {
-	compatibleSet, err := r.getCompatibleSet(ctx)
+	compatibleSet, err := r.CompatibleVersions(ctx)
 	if err != nil {
 		return err
 	}
 
-	if _, isCompatible := compatibleSet[tkr.Spec.Version]; isCompatible {
+	if compatibleSet.Has(tkr.Spec.Version) {
 		conditions.MarkTrue(tkr, runv1.ConditionCompatible)
 		return nil
 	}
@@ -125,38 +133,30 @@ func (r *Reconciler) updateTKRCompatibleCondition(ctx context.Context, tkr *runv
 	return nil
 }
 
-func (r *Reconciler) getCompatibleSet(ctx context.Context) (map[string]struct{}, error) {
-	mgmtClusterVersion, err := r.getManagementClusterVersion(ctx)
+func (c *Compatibility) CompatibleVersions(ctx context.Context) (sets.StringSet, error) {
+	mgmtClusterVersion, err := c.getManagementClusterVersion(ctx)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get the management cluster info")
 	}
 
-	metadata, err := r.compatibilityMetadata(ctx)
+	metadata, err := c.compatibilityMetadata(ctx)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to get BOM compatibility metadata")
 	}
 
 	for _, mgmtVersion := range metadata.ManagementClusterVersions {
 		if mgmtClusterVersion == mgmtVersion.TKGVersion {
-			return stringSet(mgmtVersion.SupportedKubernetesVersions), nil
+			return sets.Strings(mgmtVersion.SupportedKubernetesVersions...), nil
 		}
 	}
 
-	return stringSet(nil), nil
-}
-
-func stringSet(ss []string) map[string]struct{} {
-	result := make(map[string]struct{}, len(ss))
-	for _, s := range ss {
-		result[s] = struct{}{}
-	}
-	return result
+	return sets.Strings(), nil
 }
 
 // getManagementClusterVersion get the version of the management cluster
-func (r *Reconciler) getManagementClusterVersion(ctx context.Context) (string, error) {
+func (c *Compatibility) getManagementClusterVersion(ctx context.Context) (string, error) {
 	clusterList := &clusterv1.ClusterList{}
-	if err := r.Client.List(ctx, clusterList, client.HasLabels{constants.ManagementClusterRoleLabel}); err != nil {
+	if err := c.Client.List(ctx, clusterList, client.HasLabels{constants.ManagementClusterRoleLabel}); err != nil {
 		return "", errors.Wrap(err, "failed to list clusters")
 	}
 
@@ -169,10 +169,10 @@ func (r *Reconciler) getManagementClusterVersion(ctx context.Context) (string, e
 	return "", errors.New("failed to get management cluster info")
 }
 
-func (r *Reconciler) compatibilityMetadata(ctx context.Context) (*types.CompatibilityMetadata, error) {
+func (c *Compatibility) compatibilityMetadata(ctx context.Context) (*types.CompatibilityMetadata, error) {
 	cm := &corev1.ConfigMap{}
-	cmObjectKey := client.ObjectKey{Namespace: r.Config.TKRNamespace, Name: constants.BOMMetadataConfigMapName}
-	if err := r.Client.Get(ctx, cmObjectKey, cm); err != nil {
+	cmObjectKey := client.ObjectKey{Namespace: c.Config.TKRNamespace, Name: constants.BOMMetadataConfigMapName}
+	if err := c.Client.Get(ctx, cmObjectKey, cm); err != nil {
 		return nil, err
 	}
 

pkg/v2/tkr/controller/tkr-source/fetcher/configurer.go
@@ -1,76 +1,0 @@
-// Copyright 2022 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package fetcher
-
-import (
-	"context"
-	"os"
-	"path"
-
-	ctlimg "github.com/k14s/imgpkg/pkg/imgpkg/registry"
-	"github.com/pkg/errors"
-	corev1 "k8s.io/api/core/v1"
-	k8serr "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/registry"
-)
-
-const (
-	configMapName     = "tkr-controller-config"
-	caCertsKey        = "caCerts"
-	registryCertsFile = "registry_certs"
-)
-
-func (f *Fetcher) configure(ctx context.Context) error {
-	configMap := &corev1.ConfigMap{}
-	if err := f.Client.Get(ctx,
-		types.NamespacedName{Namespace: f.Config.TKRNamespace, Name: configMapName},
-		configMap); !k8serr.IsNotFound(err) {
-		return errors.Wrapf(err, "unable to get the ConfigMap %s", configMapName)
-	}
-
-	err := addTrustedCerts(configMap.Data[caCertsKey])
-	if err != nil {
-		return errors.Wrap(err, "failed to add certs")
-	}
-
-	f.registryOps = ctlimg.Opts{
-		VerifyCerts: f.Config.VerifyRegistryCert,
-		Anon:        true,
-	}
-
-	// Add custom CA cert paths only if VerifyCerts is enabled
-	if f.registryOps.VerifyCerts {
-		registryCertPath, err := getRegistryCertFile()
-		if err == nil {
-			if _, err = os.Stat(registryCertPath); err == nil {
-				f.registryOps.CACertPaths = []string{registryCertPath}
-			}
-		}
-	}
-
-	f.registry, err = registry.New(&f.registryOps)
-	return err
-}
-
-func addTrustedCerts(certChain string) (err error) {
-	if certChain == "" {
-		return nil
-	}
-
-	filePath, err := getRegistryCertFile()
-	if err != nil {
-		return err
-	}
-
-	return os.WriteFile(filePath, []byte(certChain), 0644)
-}
-func getRegistryCertFile() (string, error) {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		return "", errors.Wrap(err, "could not locate local tanzu dir")
-	}
-	return path.Join(home, registryCertsFile), nil
-}

pkg/v2/tkr/controller/tkr-source/fetcher/fetcher.go
@@ -14,23 +14,24 @@ import (
 	"time"
 
 	"github.com/go-logr/logr"
-	ctlimg "github.com/k14s/imgpkg/pkg/imgpkg/registry"
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	kerrors "k8s.io/apimachinery/pkg/util/errors"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/yaml"
 
 	kapppkgv1 "github.com/vmware-tanzu/carvel-kapp-controller/pkg/apiserver/apis/datapackaging/v1alpha1"
-
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/constants"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/registry"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/types"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/controller/tkr-source/pkgcr"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/controller/tkr-source/registry"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/util/sets"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/util/version"
 )
 
 type Fetcher struct {
@@ -38,8 +39,9 @@ type Fetcher struct {
 	Client client.Client
 	Config Config
 
-	registryOps ctlimg.Opts
-	registry    registry.Registry
+	Registry registry.Registry
+
+	Compatibility version.Compatibility
 }
 
 // Config contains the controller manager context.
@@ -48,7 +50,6 @@ type Config struct {
 	BOMImagePath         string
 	BOMMetadataImagePath string
 	TKRRepoImagePath     string
-	VerifyRegistryCert   bool
 	TKRDiscoveryOption   TKRDiscoveryIntervals
 }
 
@@ -58,12 +59,11 @@ type TKRDiscoveryIntervals struct {
 	ContinuousDiscoveryFrequency time.Duration
 }
 
-func (f *Fetcher) Start(ctx context.Context) error {
-	f.Log.Info("Performing configuration setup")
-	if err := f.configure(ctx); err != nil {
-		return errors.Wrap(err, "failed to configure the controller")
-	}
+func (f *Fetcher) SetupWithManager(m ctrl.Manager) error {
+	return m.Add(f)
+}
 
+func (f *Fetcher) Start(ctx context.Context) error {
 	f.Log.Info("Performing an initial release discovery")
 	f.initialReconcile(ctx, f.Config.TKRDiscoveryOption.InitialDiscoveryFrequency, InitialDiscoveryRetry)
 
@@ -112,14 +112,18 @@ func (f *Fetcher) tkrDiscovery(ctx context.Context, frequency time.Duration) {
 }
 
 func (f *Fetcher) fetchAll(ctx context.Context) error {
+	fetchTKRCompatibilityDone := make(chan struct{})
 	return kerrors.AggregateGoroutines(
 		func() error {
+			defer close(fetchTKRCompatibilityDone)
 			return f.fetchTKRCompatibilityCM(ctx)
 		},
 		func() error {
+			<-fetchTKRCompatibilityDone
 			return f.fetchTKRBOMConfigMaps(ctx)
 		},
 		func() error {
+			<-fetchTKRCompatibilityDone
 			return f.fetchTKRPackages(ctx)
 		})
 }
@@ -160,7 +164,7 @@ func (f *Fetcher) fetchTKRCompatibilityCM(ctx context.Context) error {
 
 func (f *Fetcher) fetchCompatibilityMetadata() (*types.CompatibilityMetadata, error) {
 	f.Log.Info("Listing BOM metadata image tags", "image", f.Config.BOMMetadataImagePath)
-	tags, err := f.registry.ListImageTags(f.Config.BOMMetadataImagePath)
+	tags, err := f.Registry.ListImageTags(f.Config.BOMMetadataImagePath)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to list compatibility metadata image tags")
 	}
@@ -184,7 +188,7 @@ func (f *Fetcher) fetchCompatibilityMetadata() (*types.CompatibilityMetadata, er
 	for i := len(tagNum) - 1; i >= 0; i-- {
 		tagName := fmt.Sprintf("v%d", tagNum[i])
 		f.Log.Info("Fetching BOM metadata image", "image", f.Config.BOMMetadataImagePath, "tag", tagName)
-		metadataContent, err = f.registry.GetFile(fmt.Sprintf("%s:%s", f.Config.BOMMetadataImagePath, tagName), "")
+		metadataContent, err = f.Registry.GetFile(fmt.Sprintf("%s:%s", f.Config.BOMMetadataImagePath, tagName), "")
 		if err == nil {
 			if err = yaml.Unmarshal(metadataContent, &metadata); err == nil {
 				break
@@ -209,15 +213,18 @@ func (f *Fetcher) fetchTKRBOMConfigMaps(ctx context.Context) error {
 	default:
 	}
 
+	compatibleImageTags, err := f.compatibleImageTags(ctx)
+	if err != nil {
+		return err
+	}
+
 	f.Log.Info("Listing BOM image tags", "image", f.Config.BOMImagePath)
-	imageTags, err := f.registry.ListImageTags(f.Config.BOMImagePath)
+	imageTags, err := f.Registry.ListImageTags(f.Config.BOMImagePath)
 	if err != nil {
 		return errors.Wrap(err, "failed to list current available BOM image tags")
 	}
-	tagMap := make(map[string]bool)
-	for _, tag := range imageTags {
-		tagMap[tag] = false
-	}
+
+	tagsToDownload := compatibleImageTags.Intersect(sets.Strings(imageTags...))
 
 	cmList := &corev1.ConfigMapList{}
 	if err := f.Client.List(ctx, cmList, &client.ListOptions{Namespace: f.Config.TKRNamespace}); err != nil {
@@ -226,25 +233,18 @@ func (f *Fetcher) fetchTKRBOMConfigMaps(ctx context.Context) error {
 
 	for i := range cmList.Items {
 		if imageTag, ok := cmList.Items[i].ObjectMeta.Annotations[constants.BomConfigMapImageTagAnnotation]; ok {
-			if _, ok := tagMap[imageTag]; ok {
-				tagMap[imageTag] = true
-			}
+			tagsToDownload.Remove(imageTag)
 		}
 	}
-	var errs errorSlice
-	for tag, exist := range tagMap {
-		if !exist {
-			if err := f.createBOMConfigMap(ctx, tag); err != nil {
-				errs = append(errs, errors.Wrapf(err, "failed to create BOM ConfigMap for image %s", fmt.Sprintf("%s:%s", f.Config.BOMImagePath, tag)))
-			}
+	var errs []error
+	for tag := range tagsToDownload {
+		if err := f.createBOMConfigMap(ctx, tag); err != nil {
+			errs = append(errs, errors.Wrapf(err, "failed to create BOM ConfigMap for image %s", fmt.Sprintf("%s:%s", f.Config.BOMImagePath, tag)))
 		}
-	}
-	if len(errs) != 0 {
-		return errs
 	}
 
 	f.Log.Info("Done reconciling BOM images", "image", f.Config.BOMImagePath)
-	return nil
+	return kerrors.NewAggregate(errs)
 }
 
 func (f *Fetcher) createBOMConfigMap(ctx context.Context, tag string) error {
@@ -255,7 +255,7 @@ func (f *Fetcher) createBOMConfigMap(ctx context.Context, tag string) error {
 	}
 
 	f.Log.Info("Fetching BOM", "image", f.Config.BOMImagePath, "tag", tag)
-	bomContent, err := f.registry.GetFile(fmt.Sprintf("%s:%s", f.Config.BOMImagePath, tag), "")
+	bomContent, err := f.Registry.GetFile(fmt.Sprintf("%s:%s", f.Config.BOMImagePath, tag), "")
 	if err != nil {
 		return errors.Wrapf(err, "failed to get the BOM file from image %s:%s", f.Config.BOMImagePath, tag)
 	}
@@ -305,24 +305,38 @@ func (f *Fetcher) fetchTKRPackages(ctx context.Context) error {
 	default:
 	}
 
+	compatibleImageTags, err := f.compatibleImageTags(ctx)
+	if err != nil {
+		return err
+	}
+
 	f.Log.Info("Listing TKR Package Repository tags", "image", f.Config.TKRRepoImagePath)
-	imageTags, err := f.registry.ListImageTags(f.Config.TKRRepoImagePath)
+	imageTags, err := f.Registry.ListImageTags(f.Config.TKRRepoImagePath)
 	if err != nil {
 		return errors.Wrap(err, "failed to list current available TKR Package Repository image tags")
 	}
 
-	var errs errorSlice
-	for _, tag := range imageTags {
+	imageTagsToPull := compatibleImageTags.Intersect(sets.Strings(imageTags...))
+
+	var errs []error
+	for tag := range imageTagsToPull {
 		if err := f.createTKRPackages(ctx, tag); err != nil {
 			errs = append(errs, errors.Wrapf(err, "failed to create TKR Package for image %s", fmt.Sprintf("%s:%s", f.Config.TKRRepoImagePath, tag)))
 		}
 	}
-	if len(errs) != 0 {
-		return errs
-	}
 
 	f.Log.Info("Done fetching TKR Packages", "image", f.Config.TKRRepoImagePath)
-	return nil
+	return kerrors.NewAggregate(errs)
+}
+
+func (f *Fetcher) compatibleImageTags(ctx context.Context) (sets.StringSet, error) {
+	compatibleTKRVersions, err := f.Compatibility.CompatibleVersions(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return compatibleTKRVersions.Map(func(v string) string {
+		return strings.ReplaceAll(v, "+", "_")
+	}), nil
 }
 
 func (f *Fetcher) createTKRPackages(ctx context.Context, tag string) error {
@@ -334,7 +348,7 @@ func (f *Fetcher) createTKRPackages(ctx context.Context, tag string) error {
 
 	imageName := fmt.Sprintf("%s:%s", f.Config.TKRRepoImagePath, tag)
 	f.Log.Info("Fetching TKR Package Repository imgpkg bundle", "image", imageName)
-	bundleContent, err := f.registry.GetFiles(imageName)
+	bundleContent, err := f.Registry.GetFiles(imageName)
 	if err != nil {
 		return errors.Wrapf(err, "failed to fetch the BOM file from image '%s'", imageName)
 	}

pkg/v2/tkr/controller/tkr-source/fetcher/helper.go
@@ -3,27 +3,7 @@
 
 package fetcher
 
-import (
-	"strings"
-)
-
 const (
 	// InitialDiscoveryRetry is the number of retries for the initial TKR sync-up
 	InitialDiscoveryRetry = 10
 )
-
-type errorSlice []error
-
-func (e errorSlice) Error() string {
-	if len(e) == 0 {
-		return ""
-	}
-	sb := &strings.Builder{}
-	for i, err := range e {
-		if i != 0 {
-			sb.WriteString(", ")
-		}
-		sb.WriteString(err.Error())
-	}
-	return sb.String()
-}

pkg/v2/tkr/controller/tkr-source/main.go
@@ -4,8 +4,11 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"os"
+	"reflect"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
@@ -20,12 +23,12 @@ import (
 
 	kapppkgiv1 "github.com/vmware-tanzu/carvel-kapp-controller/pkg/apis/packaging/v1alpha1"
 	kapppkgv1 "github.com/vmware-tanzu/carvel-kapp-controller/pkg/apiserver/apis/datapackaging/v1alpha1"
-
 	runv1 "github.com/vmware-tanzu/tanzu-framework/apis/run/v1alpha3"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/buildinfo"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/controller/tkr-source/compatibility"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/controller/tkr-source/fetcher"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/controller/tkr-source/pkgcr"
+	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/controller/tkr-source/registry"
 )
 
 var (
@@ -41,17 +44,19 @@ func init() {
 	utilruntime.Must(corev1.AddToScheme(scheme))
 }
 
-func main() {
-	var metricsAddr string
-	var tkrNamespace string
-	var tkrPkgServiceAccountName string
-	var bomImagePath string
-	var bomMetadataImagePath string
-	var tkrRepoImagePath string
-	var initTKRDiscoveryFreq int
-	var continuousTKRDiscoverFreq int
-	var skipVerifyRegistryCerts bool
+var (
+	metricsAddr               string
+	tkrNamespace              string
+	tkrPkgServiceAccountName  string
+	bomImagePath              string
+	bomMetadataImagePath      string
+	tkrRepoImagePath          string
+	initTKRDiscoveryFreq      int
+	continuousTKRDiscoverFreq int
+	skipVerifyRegistryCerts   bool
+)
 
+func init() {
 	flag.StringVar(&metricsAddr, "metrics-bind-addr", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&tkrNamespace, "namespace", "tkg-system", "Namespace for TKR related resources")
 	flag.StringVar(&tkrPkgServiceAccountName, "sa-name", "tkr-source-controller-manager-sa", "ServiceAccount name used by TKR PackageInstalls")
@@ -63,16 +68,84 @@ func main() {
 	flag.IntVar(&continuousTKRDiscoverFreq, "continuous-discover-frequency", 600, "Continuous TKR discovery frequency in seconds")
 	flag.Parse()
 
+	setupLog.Info("Version", "version", buildinfo.Version, "buildDate", buildinfo.Date, "sha", buildinfo.SHA)
+
+	registryConfig = registry.Config{
+		TKRNamespace:       tkrNamespace,
+		VerifyRegistryCert: !skipVerifyRegistryCerts,
+	}
+	fetcherConfig = fetcher.Config{
+		TKRNamespace:         tkrNamespace,
+		BOMImagePath:         bomImagePath,
+		BOMMetadataImagePath: bomMetadataImagePath,
+		TKRRepoImagePath:     tkrRepoImagePath,
+		TKRDiscoveryOption: fetcher.TKRDiscoveryIntervals{
+			InitialDiscoveryFrequency:    time.Duration(initTKRDiscoveryFreq) * time.Second,
+			ContinuousDiscoveryFrequency: time.Duration(continuousTKRDiscoverFreq) * time.Second,
+		},
+	}
+	pkgcrConfig = pkgcr.Config{
+		ServiceAccountName: tkrPkgServiceAccountName,
+	}
+	compatibilityConfig = compatibility.Config{
+		TKRNamespace: tkrNamespace,
+	}
+}
+
+var (
+	registryConfig      registry.Config
+	fetcherConfig       fetcher.Config
+	pkgcrConfig         pkgcr.Config
+	compatibilityConfig compatibility.Config
+)
+
+func main() {
 	opts := zap.Options{
 		Development: true,
 	}
 	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
-
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	setupLog.Info("Version", "version", buildinfo.Version, "buildDate", buildinfo.Date, "sha", buildinfo.SHA)
+	mgr := createManager()
+	ctx := signals.SetupSignalHandler()
 
+	tkrCompatibility := &compatibility.Compatibility{
+		Client: mgr.GetClient(),
+		Config: compatibilityConfig,
+	}
+	registryInstance := registry.New(mgr.GetClient(), registryConfig)
+	fetcherInstance := &fetcher.Fetcher{
+		Log:           mgr.GetLogger().WithName("tkr-fetcher"),
+		Client:        mgr.GetClient(),
+		Config:        fetcherConfig,
+		Registry:      registryInstance,
+		Compatibility: tkrCompatibility,
+	}
+	pkgcrReconciler := &pkgcr.Reconciler{
+		Log:      mgr.GetLogger().WithName("tkr-source"),
+		Client:   mgr.GetClient(),
+		Config:   pkgcrConfig,
+		Registry: registryInstance,
+	}
+	compatibilityReconciler := &compatibility.Reconciler{
+		Ctx:           ctx,
+		Log:           mgr.GetLogger().WithName("tkr-compatibility"),
+		Client:        mgr.GetClient(),
+		Config:        compatibilityConfig,
+		Compatibility: tkrCompatibility,
+	}
+
+	setupWithManager(mgr, []managedComponent{
+		registryInstance,
+		fetcherInstance,
+		pkgcrReconciler,
+		compatibilityReconciler,
+	})
+
+	startManager(ctx, mgr)
+}
+
+func createManager() manager.Manager {
 	// Setup Manager
 	setupLog.Info("setting up manager")
 	mgr, err := manager.New(config.GetConfigOrDie(), manager.Options{
@@ -83,51 +156,32 @@ func main() {
 		setupLog.Error(err, "unable to set up controller manager")
 		os.Exit(1)
 	}
+	return mgr
+}
 
-	ctx := signals.SetupSignalHandler()
-
-	if err := mgr.Add(&fetcher.Fetcher{
-		Log:    mgr.GetLogger().WithName("tkr-fetcher"),
-		Client: mgr.GetClient(),
-		Config: fetcher.Config{
-			TKRNamespace:         tkrNamespace,
-			BOMImagePath:         bomImagePath,
-			BOMMetadataImagePath: bomMetadataImagePath,
-			TKRRepoImagePath:     tkrRepoImagePath,
-			VerifyRegistryCert:   !skipVerifyRegistryCerts,
-			TKRDiscoveryOption: fetcher.TKRDiscoveryIntervals{
-				InitialDiscoveryFrequency:    time.Duration(initTKRDiscoveryFreq) * time.Second,
-				ContinuousDiscoveryFrequency: time.Duration(continuousTKRDiscoverFreq) * time.Second,
-			},
-		},
-	}); err != nil {
-		setupLog.Error(err, "unable to add fetcher to controller manager")
-		os.Exit(1)
+func setupWithManager(mgr manager.Manager, managedComponents []managedComponent) {
+	for _, c := range managedComponents {
+		setupLog.Info("setting up component", "type", fullTypeName(c))
+		if err := c.SetupWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to setup component", "type", fullTypeName(c))
+			os.Exit(1)
+		}
 	}
+}
 
-	if err := (&pkgcr.Reconciler{
-		Log:    mgr.GetLogger().WithName("tkr-source"),
-		Client: mgr.GetClient(),
-		Config: pkgcr.Config{
-			ServiceAccountName: tkrPkgServiceAccountName,
-		},
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "TKR Package")
-		os.Exit(1)
+func fullTypeName(c managedComponent) string {
+	cType := reflect.TypeOf(c)
+	for cType.Kind() == reflect.Ptr {
+		cType = cType.Elem()
 	}
+	return fmt.Sprintf("%s.%s", cType.PkgPath(), cType.Name())
+}
 
-	if err := (&compatibility.Reconciler{
-		Ctx:    ctx,
-		Log:    mgr.GetLogger().WithName("tkr-compatibility"),
-		Client: mgr.GetClient(),
-		Config: compatibility.Config{
-			TKRNamespace: tkrNamespace,
-		},
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "TKR Compatibility")
-		os.Exit(1)
-	}
+type managedComponent interface {
+	SetupWithManager(ctrl.Manager) error
+}
 
+func startManager(ctx context.Context, mgr manager.Manager) {
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctx); err != nil {
 		setupLog.Error(err, "unable to run manager")

commit ff9e21119b267d43f147d3f0060161fb0005c41a

Fix YAML marshaling error introduced by #2524
Without this fix, `bom-metadata` ConfigMap in the `tkr-system` namespace
would differ in `binaryData.compatibility` value from its contents
prior to #2524.

Adding a unit test that would fail illustrating the difference:
```
  Expected
      <string>: "Manage..."
  to equal       |
      <string>: "manage..."
```

Signed-off-by: Ivan Mikushin <imikushin@vmware.com>

pkg/v1/tkr/pkg/types/bommetadata.go
@@ -11,5 +11,5 @@ type ManagementClusterVersion struct {
 
 // CompatibilityMetadata contains tanzu release support matrix
 type CompatibilityMetadata struct {
-	ManagementClusterVersions []ManagementClusterVersion `yaml:"managementClusterVersions"`
+	ManagementClusterVersions []ManagementClusterVersion `json:"managementClusterVersions"`
 }

pkg/v1/tkr/controllers/source/suite_test.go
@@ -689,6 +689,51 @@ var _ = Describe("r.Reconcile()", func() {
 	})
 })
 
+var _ = Describe("r.compatibilityMetadata()", func() {
+	var (
+		fakeRegistry *fakes.Registry
+		fakeClient   client.Client
+		scheme       *runtime.Scheme
+		objects      []runtime.Object
+		r            reconciler
+	)
+
+	JustBeforeEach(func() {
+		scheme = runtime.NewScheme()
+		addToScheme(scheme)
+		fakeClient = fake.NewClientBuilder().WithScheme(scheme).WithRuntimeObjects(objects...).Build()
+		r = reconciler{
+			registry:                   fakeRegistry,
+			ctx:                        context.Background(),
+			client:                     fakeClient,
+			log:                        ctrllog.Log,
+			scheme:                     scheme,
+			compatibilityMetadataImage: "",
+		}
+	})
+
+	BeforeEach(func() {
+		fakeRegistry = &fakes.Registry{}
+		fakeRegistry.ListImageTagsReturnsOnCall(0, []string{"v1"}, nil)
+		fakeRegistry.GetFileReturnsOnCall(0, metadataContent, nil)
+	})
+
+	It("should correctly parse bom-metadata", func() {
+		err := r.reconcileBOMMetadataCM(r.ctx)
+		Expect(err).ToNot(HaveOccurred())
+
+		cm := &corev1.ConfigMap{}
+		Expect(r.client.Get(r.ctx,
+			client.ObjectKey{Namespace: constants.TKRNamespace, Name: constants.BOMMetadataConfigMapName},
+			cm)).To(Succeed())
+		Expect(normalizeYAML(cm.BinaryData[constants.BOMMetadataCompatibilityKey])).To(Equal(normalizeYAML(metadataContent)))
+
+		compatibilityMetadata, err := r.compatibilityMetadata(r.ctx)
+		Expect(err).ToNot(HaveOccurred())
+		Expect(compatibilityMetadata.ManagementClusterVersions).ToNot(BeEmpty())
+	})
+})
+
 type clientErrOnGetCluster struct {
 	client.Client
 	err error

pkg/v1/tkr/controllers/source/test_helper.go
@@ -1,0 +1,19 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package source
+
+import "sigs.k8s.io/yaml"
+
+func normalizeYAML(content []byte) string {
+	var parsed map[string]interface{}
+	err := yaml.Unmarshal(content, &parsed)
+	if err != nil {
+		panic(err)
+	}
+	bytes, err := yaml.Marshal(parsed)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
+}