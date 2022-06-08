8c47d372da3a59b5f34e13351a22ae62563a921f
prkalle <38508878+prkalle@users.noreply.github.com>
2022-06-08 14:05:18 -0700

    Temporary hack to set tkr-source-controller configuration during tkg package installation (#2566)
    Signed-off-by: PremKumar Kalle <pkalle@vmware.com>

--- pkg/v1/tkg/managementcomponents/helper.go
+++ pkg/v1/tkg/managementcomponents/helper.go
@@ -22,6 +22,19 @@ const (
 
 // GetTKGPackageConfigValuesFileFromUserConfig returns values file from user configuration
 func GetTKGPackageConfigValuesFileFromUserConfig(managementPackageVersion string, userProviderConfigValues map[string]string) (string, error) {
+	// TODO: Temporary hack(hard coded values) to configure TKR source controller package values. This should be replaced with the logic
+	// that fetches these values from tkg-bom(for bom related urls) and set the TKR source controller package values
+	var tkrRepoImagePath string
+	providerType := userProviderConfigValues[constants.ConfigVariableProviderType]
+	switch providerType {
+	case constants.InfrastructureProviderVSphere:
+		tkrRepoImagePath = "projects-stg.registry.vmware.com/tkg/tkr-repository-vsphere-nonparavirt"
+	case constants.InfrastructureProviderAWS:
+		tkrRepoImagePath = "projects-stg.registry.vmware.com/tkg/tkr-repository-aws"
+	case constants.InfrastructureProviderAzure:
+		tkrRepoImagePath = "projects-stg.registry.vmware.com/tkg/tkr-repository-azure"
+	}
+
 	tkgPackageConfig := TKGPackageConfig{
 		Metadata: Metadata{
 			InfraProvider: userProviderConfigValues[constants.ConfigVariableProviderType],
@@ -54,6 +67,15 @@ func GetTKGPackageConfigValuesFileFromUserConfig(managementPackageVersion string
 			VersionConstraints: managementPackageVersion,
 			ClusterClassInfraPackageValues: ClusterClassInfraPackageValues{
 				VersionConstraints: managementPackageVersion,
+			},
+		},
+		TKRSourceControllerPackage: TKRSourceControllerPackage{
+			VersionConstraints: managementPackageVersion,
+			TKRSourceControllerPackageValues: TKRSourceControllerPackageValues{
+				VersionConstraints:   managementPackageVersion,
+				BomImagePath:         "projects-stg.registry.vmware.com/tkg/tkr-bom",
+				BomMetadataImagePath: "projects-stg.registry.vmware.com/tkg/v16-v1.6.0-zshippable/tkr-compatibility",
+				TKRRepoImagePath:     tkrRepoImagePath,
 			},
 		},
 	}
