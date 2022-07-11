1633e01755ddaa912a432c5e2c519904efc9d620
ridavid2002 <98621755+ridavid2002@users.noreply.github.com>
2022-07-11 11:59:19 -0400

    sh update (#2861)

--- hack/kind/deploy_kind_with_capi_and_kapp.sh
+++ hack/kind/deploy_kind_with_capi_and_kapp.sh
@@ -16,4 +16,4 @@ export EXP_CLUSTER_RESOURCE_SET=true
 clusterctl init --core cluster-api:v1.1.0-beta.2 --bootstrap kubeadm:v1.1.0-rc.0 --control-plane kubeadm:v1.1.0-beta.2 -i docker:v1.1.0-beta.2 -i aws:v1.2.0 -i vsphere:v1.0.2
 
 # install kapp-controller
-kubectl apply -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/download/v0.31.0/release.yml
+kubectl apply -f https://github.com/vmware-tanzu/carvel-kapp-controller/releases/download/v0.38.3/release.yml
