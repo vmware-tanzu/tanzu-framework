2991de36f84d50e2ad681532ea28af92c15a2716
Sudarshan <asudarshan@vmware.com>
2022-08-15 14:59:42 -0700

    Successfully created a cluster-class based MC for CAPD

---
+++ pkg/v1/tkg/test/tkgctl/docker_cc/docker_antrea_test.go
@@ -0,0 +1,13 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// nolint:typecheck,nolintlint
+package docker
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Functional tests for docker - antrea", func() {
+	//no-op
+})
