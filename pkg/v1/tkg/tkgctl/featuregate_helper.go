c33b3f65bc657222466574b1d0ae6bacf5f1c4bf
Rajath Agasthya <ragasthya@vmware.com>
2022-08-24 15:06:01 -0500

    Temporarily copy featuregate functions to tkgctl
    Until `tkgctl` package moves into a different Go module. This removes
    circular dependency between featuregates and tanzu-framework.

--- pkg/v1/tkg/tkgctl/featuregate_helper.go
+++ pkg/v1/tkg/tkgctl/featuregate_helper.go
@@ -5,12 +5,16 @@ package tkgctl
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pkg/errors"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/vmware-tanzu/tanzu-framework/apis/config/v1alpha1"
 	configv1alpha1 "github.com/vmware-tanzu/tanzu-framework/apis/config/v1alpha1"
-	"github.com/vmware-tanzu/tanzu-framework/featuregate/client/pkg/util"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/clusterclient"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/constants"
 )
@@ -59,7 +63,80 @@ func (fg *featureGateHelper) FeatureActivatedInNamespace(reqContext context.Cont
 		}
 	}
 
-	return util.FeatureActivatedInNamespace(reqContext, clusterClient.GetClientSet(), namespace, feature)
+	return featureActivatedInNamespace(reqContext, clusterClient.GetClientSet(), namespace, feature)
+}
+
+// TODO(ragasthya): Remove temp copies of featuregate helper functions.
+// featureActivatedInNamespace returns true only if all of the features specified are activated in the namespace.
+func featureActivatedInNamespace(ctx context.Context, c client.Client, namespace, feature string) (bool, error) {
+	selector := metav1.LabelSelector{
+		MatchExpressions: []metav1.LabelSelectorRequirement{
+			{Key: "kubernetes.io/metadata.name", Operator: metav1.LabelSelectorOpIn, Values: []string{namespace}},
+		},
+	}
+	return featuresActivatedInNamespacesMatchingSelector(ctx, c, selector, []string{feature})
+}
+
+// featuresActivatedInNamespacesMatchingSelector returns true only if all the features specified are activated in every namespace matched by the selector.
+func featuresActivatedInNamespacesMatchingSelector(ctx context.Context, c client.Client, namespaceSelector metav1.LabelSelector, features []string) (bool, error) {
+	namespaces, err := namespacesMatchingSelector(ctx, c, &namespaceSelector)
+	if err != nil {
+		return false, err
+	}
+
+	// If no namespaces are matched or no features specified, return false.
+	if len(namespaces) == 0 || len(features) == 0 {
+		return false, nil
+	}
+
+	featureGatesList := &configv1alpha1.FeatureGateList{}
+	if err := c.List(ctx, featureGatesList); err != nil {
+		return false, err
+	}
+
+	// Map of namespace to a set of features activated in that namespace.
+	namespaceToActivatedFeatures := make(map[string]sets.String)
+	for i := range featureGatesList.Items {
+		fg := featureGatesList.Items[i]
+		for _, namespace := range fg.Status.Namespaces {
+			namespaceToActivatedFeatures[namespace] = sets.NewString(fg.Status.ActivatedFeatures...)
+		}
+	}
+
+	for _, ns := range namespaces {
+		activatedFeatures, found := namespaceToActivatedFeatures[ns]
+		if !found {
+			// Namespace has no features gated.
+			return false, nil
+		}
+		// Feature is not activated in this namespace.
+		if !activatedFeatures.HasAll(features...) {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+// namespacesMatchingSelector returns the list of namespaces after applying the NamespaceSelector filter.
+// Note that a nil selector selects nothing, while an empty selector selects everything.
+// Callers using this function in feature gates context should be sending a pointer to an empty selector instead of nil.
+func namespacesMatchingSelector(ctx context.Context, c client.Client, selector *metav1.LabelSelector) ([]string, error) {
+	s, err := metav1.LabelSelectorAsSelector(selector)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get namespaces from NamespaceSelector: %w", err)
+	}
+
+	nsList := &corev1.NamespaceList{}
+	err = c.List(ctx, nsList, client.MatchingLabelsSelector{Selector: s})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get namespaces from NamespaceSelector: %w", err)
+	}
+
+	var namespaces []string
+	for i := range nsList.Items {
+		namespaces = append(namespaces, nsList.Items[i].Name)
+	}
+	return namespaces, nil
 }
 
 // isClusterClassFeatureActivated is decided based on the FeatureGate CRD is present or not
