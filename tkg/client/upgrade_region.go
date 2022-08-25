c5c9074647222671482c7b25e8da5116fd8f795f
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-08-25 11:05:10 -0700

    Create  tkg/providersupgradeclient top-level directory
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- tkg/client/upgrade_region.go
+++ tkg/client/upgrade_region.go
@@ -27,7 +27,7 @@ import (
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/config"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/clusterclient"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/constants"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/providersupgradeclient"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/providersupgradeclient"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/region"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigbom"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/log"
