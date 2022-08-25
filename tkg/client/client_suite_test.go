2963bbadb8f870278357fbcecbf1a75731476265
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-08-25 10:21:21 -0700

    Create  tkg/clientcreator top-level directory
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- tkg/client/client_suite_test.go
+++ tkg/client/client_suite_test.go
@@ -14,7 +14,7 @@ import (
 
 	azure "github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/azure/mocks"
 	. "github.com/vmware-tanzu/tanzu-framework/tkg/client"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/clientcreator"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/clientcreator"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/constants"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/region"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigpaths"
