a353c71e2da5f65b65f315d313bc2751ba2f9d81
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-08-26 10:31:30 -0700

    creating top level dir for tkg/tkgctl
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- tkg/vc/helpers.go
+++ tkg/vc/helpers.go
@@ -17,9 +17,9 @@ import (
 	"github.com/vmware/govmomi/vim25/soap"
 	"github.com/vmware/govmomi/vim25/types"
 
-	"github.com/vmware-tanzu/tanzu-framework/tkg/tkgconfighelper"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigreaderwriter"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/constants"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/tkgconfighelper"
 	tkgtypes "github.com/vmware-tanzu/tanzu-framework/tkg/types"
 )
 
