e2b045a3d327f8e09f3fb40e8659499252dad6e1
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-08-25 23:37:08 -0700

    Build error
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- tkg/client/client_suite_test.go
+++ tkg/client/client_suite_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/constants"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/region"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigpaths"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigproviders"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/tkgconfigproviders"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigreaderwriter"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/types"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/models"
