954fcfed1c57d63bbcd5e56cbfe32bf427ae6ada
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-08-28 07:59:57 -0700

    creating top level dir for tkg/clusterclient
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- pkg/v1/tkg/web/server/serve.go
+++ pkg/v1/tkg/web/server/serve.go
@@ -19,11 +19,11 @@ import (
 	assetfs "github.com/elazarl/go-bindata-assetfs"
 
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/client"
-	servermanifest "github.com/vmware-tanzu/tanzu-framework/tkg/manifest/server"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/handlers"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/restapi"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/restapi/operations"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/ws"
+	servermanifest "github.com/vmware-tanzu/tanzu-framework/tkg/manifest/server"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/tkgconfigreaderwriter"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/types"
 )
