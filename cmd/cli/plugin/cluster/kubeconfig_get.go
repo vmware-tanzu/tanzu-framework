5121c22939dd8d3bfb2e8d9e514eab3595710990
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-08-29 01:34:11 -0700

    tkg/tkgctl
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- cmd/cli/plugin/cluster/kubeconfig_get.go
+++ cmd/cli/plugin/cluster/kubeconfig_get.go
@@ -15,7 +15,7 @@ import (
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/config"
 
 	tkgclient "github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/client"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgctl"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/tkgctl"
 )
 
 type getClusterKubeconfigOptions struct {
