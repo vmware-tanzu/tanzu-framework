f14a3372959b1b7e10e0d6ded8502eab564aa8e2
tkg-serviceaccount <tkg-serviceaccount@users.noreply.github.com>
2022-09-01 11:41:51 +0000

    Update the BOM retrieval version to v0.27.0-dev

--- pkg/v1/tkg/tkgconfigpaths/zz_bundled_default_bom_files_configdata.go
+++ pkg/v1/tkg/tkgconfigpaths/zz_bundled_default_bom_files_configdata.go
@@ -12,5 +12,5 @@ package tkgconfigpaths
 var (
 	TKGDefaultImageRepo               string = "projects-stg.registry.vmware.com/tkg"
 	TKGDefaultCompatibilityImagePath  string = "framework-zshippable/tkg-compatibility"
-	TKGManagementClusterPluginVersion string = "v0.26.0"
+	TKGManagementClusterPluginVersion string = "v0.27.0-dev"
 )
