22fbc2c4705f43adb0729cd10777a65d16aa03f1
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-09-01 23:51:38 -0700

    moving tkg to top level
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- pkg/v1/tkg/client/client_suite_test.go
+++ pkg/v1/tkg/client/client_suite_test.go
@@ -14,7 +14,7 @@ import (
 
 	azure "github.com/vmware-tanzu/tanzu-framework/tkg/azure/mocks"
 	. "github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/client"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/clientcreator"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/clientcreator"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/constants"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/region"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigpaths"
--- pkg/v1/tkg/web/server/handlers/docker.go
+++ pkg/v1/tkg/web/server/handlers/docker.go
@@ -10,7 +10,7 @@ import (
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/models"
 
 	"github.com/vmware-tanzu/tanzu-framework/cli/runtime/config"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/clientcreator"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/clientcreator"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/clusterclient"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/restapi/operations/docker"
 
