cabea4131c9736877fd229a9ff9e4ea5b20b27bf
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-09-02 00:12:00 -0700

    moving tkg to top level
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- pkg/v1/tkg/tkgconfigupdater/tkgconfigupdater_test.go
+++ pkg/v1/tkg/tkgconfigupdater/tkgconfigupdater_test.go
@@ -17,7 +17,7 @@ import (
 
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/constants"
 	fakehelper "github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/fakes/helper"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/providerinterface"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/providerinterface"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigpaths"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigreaderwriter"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/log"
--- pkg/v1/tkg/tkgctl/client.go
+++ pkg/v1/tkg/tkgctl/client.go
@@ -15,7 +15,7 @@ import (
 	"github.com/vmware-tanzu/tanzu-framework/tkg/clientcreator"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/clusterclient"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/constants"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/providerinterface"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/providerinterface"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/region"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigbom"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/tkgconfigpaths"
