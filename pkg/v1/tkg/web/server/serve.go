277d65613bf4e29de911e576031d9cb514990231
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-09-08 23:40:44 -0700

    [Part 3] Extract TKG client lib to top level package
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- pkg/v1/tkg/web/server/serve.go
+++ pkg/v1/tkg/web/server/serve.go
@@ -18,13 +18,13 @@ import (
 
 	assetfs "github.com/elazarl/go-bindata-assetfs"
 
-	servermanifest "github.com/vmware-tanzu/tanzu-framework/tkg/manifest/server"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/types"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/handlers"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/restapi"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/restapi/operations"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkg/web/server/ws"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/client"
+	servermanifest "github.com/vmware-tanzu/tanzu-framework/tkg/manifest/server"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/tkgconfigreaderwriter"
 )
 
