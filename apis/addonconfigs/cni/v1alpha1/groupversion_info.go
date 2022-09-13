b6c0af669285c39b147d9172d01249493400789d
Bin Liu <biliu@vmware.com>
2022-09-13 18:24:23 +0800

    update multicastInterface to multicastInterfaces
    Refer to antrea.yaml, multicastInterfaces is a string slice, other than a string.
    Antrea config uses a tring to store the value, is it not correct. This patch fixes
    the above issue, please refer to:
    https://github.com/antrea-io/antrea/blob/d331eb247e4c0c195cc1523b6082134409c6677f/build/yamls/antrea.yml#L4193
    
    Signed-off-by: Bin Liu <biliu@vmware.com>

--- apis/addonconfigs/cni/v1alpha1/groupversion_info.go
+++ apis/addonconfigs/cni/v1alpha1/groupversion_info.go
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package v1alpha1 contains API Schema definitions for the cni v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=cni.tanzu.vmware.com
+// +kubebuilder:object:generate=true
+// +groupName=cni.tanzu.vmware.com
 package v1alpha1
 
 import (
--- apis/addonconfigs/csi/v1alpha1/groupversion_info.go
+++ apis/addonconfigs/csi/v1alpha1/groupversion_info.go
@@ -2,8 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Package v1alpha1 contains API Schema definitions for the csi v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=csi.tanzu.vmware.com
+// +kubebuilder:object:generate=true
+// +groupName=csi.tanzu.vmware.com
 package v1alpha1
 
 import (
