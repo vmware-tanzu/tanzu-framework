64ababbed81c8241667625c3507727962508047d
Shashank Girish <girishsh@vmware.com>
2022-09-14 16:38:19 +0530

    Remove dependency on pkg/v1/tkr/pkg/registry
    Signed-off-by: Shashank Girish <girishsh@vmware.com>

---
+++ tkg/registry/doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package registry defines a Registry interface and client for working for imkpkg images.
+// Copied over from github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/registry
+package registry
