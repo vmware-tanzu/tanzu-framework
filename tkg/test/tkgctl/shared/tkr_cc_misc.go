40d4f563b1a561945d5a322809b713d0bf5bdda2
Shashank Girish <girishsh@vmware.com>
2022-09-14 16:16:20 +0530

    Remove dependency on pkg/v1/tkr/pkg/constants
    Signed-off-by: Shashank Girish <girishsh@vmware.com>

--- tkg/test/tkgctl/shared/tkr_cc_misc.go
+++ tkg/test/tkgctl/shared/tkr_cc_misc.go
@@ -25,9 +25,10 @@ import (
 	"github.com/vmware-tanzu/tanzu-framework/apis/run/util/sets"
 	"github.com/vmware-tanzu/tanzu-framework/apis/run/util/version"
 	runv1 "github.com/vmware-tanzu/tanzu-framework/apis/run/v1alpha3"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/constants"
+
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/registry"
 	"github.com/vmware-tanzu/tanzu-framework/pkg/v2/tkr/controller/tkr-source/compatibility"
+	"github.com/vmware-tanzu/tanzu-framework/tkg/constants"
 	"github.com/vmware-tanzu/tanzu-framework/tkg/test/framework"
 )
 
