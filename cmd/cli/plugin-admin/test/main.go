c919d3fc591296a887cd0c6c9a8d730270036f7b
Marc Khouzam <kmarc@vmware.com>
2022-09-15 12:12:37 -0400

    Add go.mod and go.sum to test admin plugin (#3328)
    Signed-off-by: Marc Khouzam <kmarc@vmware.com>

--- cmd/cli/plugin-admin/test/main.go
+++ cmd/cli/plugin-admin/test/main.go
@@ -11,9 +11,9 @@ import (
 
 	cliv1alpha1 "github.com/vmware-tanzu/tanzu-framework/apis/cli/v1alpha1"
 	"github.com/vmware-tanzu/tanzu-framework/cli/core/pkg/cli"
+	"github.com/vmware-tanzu/tanzu-framework/cli/runtime/buildinfo"
 	"github.com/vmware-tanzu/tanzu-framework/cli/runtime/config"
 	"github.com/vmware-tanzu/tanzu-framework/cli/runtime/plugin"
-	"github.com/vmware-tanzu/tanzu-framework/pkg/v1/buildinfo"
 )
 
 var descriptor = cliv1alpha1.PluginDescriptor{
