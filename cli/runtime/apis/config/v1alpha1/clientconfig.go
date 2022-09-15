986a7e9e0357ccc1b5dc482f04985ffb63f924ff
Anuj Chaudhari <anujc@vmware.com>
2022-09-15 09:54:26 -0700

    Move `apis/config/ClientConfig`, `apis/cli/Catalog`

---
+++ cli/runtime/apis/config/v1alpha1/clientconfig.go
@@ -0,0 +1,208 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	// AllUnstableVersions allows all plugin versions
+	AllUnstableVersions VersionSelectorLevel = "all"
+	// AlphaUnstableVersions allows all alpha tagged versions
+	AlphaUnstableVersions VersionSelectorLevel = "alpha"
+	// ExperimentalUnstableVersions includes all pre-releases, minus +build tags
+	ExperimentalUnstableVersions VersionSelectorLevel = "experimental"
+	// NoUnstableVersions allows no unstable plugin versions, format major.minor.patch only
+	NoUnstableVersions VersionSelectorLevel = "none"
+)
+
+const (
+	// FeatureCli allows a feature to be set at the CLI level (globally) rather than for a single plugin
+	FeatureCli string = "cli"
+	// Edition value (in config) affects branding and cluster creation
+	EditionStandard  = "tkg"
+	EditionCommunity = "tce"
+)
+
+// EditionSelector allows selecting edition versions based on config file
+type EditionSelector string
+
+// VersionSelectorLevel allows selecting plugin versions based on semver properties
+type VersionSelectorLevel string
+
+// IsGlobal tells if the server is global.
+// Note: Shall be deprecated in a future version. Use Context.Type instead.
+func (s *Server) IsGlobal() bool {
+	return s.Type == GlobalServerType
+}
+
+// IsManagementCluster tells if the server is a management cluster.
+// Note: Shall be deprecated in a future version. Use Context.Type instead.
+func (s *Server) IsManagementCluster() bool {
+	return s.Type == ManagementClusterServerType
+}
+
+// GetCurrentServer returns the current server.
+// Note: Shall be deprecated in a future version. Use GetCurrentContext() instead.
+func (c *ClientConfig) GetCurrentServer() (*Server, error) {
+	for _, server := range c.KnownServers {
+		if server.Name == c.CurrentServer {
+			return server, nil
+		}
+	}
+	return nil, fmt.Errorf("current server %q not found", c.CurrentServer)
+}
+
+// HasServer tells whether the Server by the given name exists.
+func (c *ClientConfig) HasServer(name string) bool {
+	for _, s := range c.KnownServers {
+		if s.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+// GetContext by name.
+func (c *ClientConfig) GetContext(name string) (*Context, error) {
+	for _, ctx := range c.KnownContexts {
+		if ctx.Name == name {
+			return ctx, nil
+		}
+	}
+	return nil, fmt.Errorf("could not find context %q", name)
+}
+
+// HasContext tells whether the Context by the given name exists.
+func (c *ClientConfig) HasContext(name string) bool {
+	_, err := c.GetContext(name)
+	return err == nil
+}
+
+// GetCurrentContext returns the current context for the given type.
+func (c *ClientConfig) GetCurrentContext(ctxType ContextType) (*Context, error) {
+	ctxName := c.CurrentContext[ctxType]
+	if ctxName == "" {
+		return nil, fmt.Errorf("no current context set for type %q", ctxType)
+	}
+	ctx, err := c.GetContext(ctxName)
+	if err != nil {
+		return nil, fmt.Errorf("unable to get current context: %s", err.Error())
+	}
+	return ctx, nil
+}
+
+// SetCurrentContext sets the current context for the given type.
+func (c *ClientConfig) SetCurrentContext(ctxType ContextType, ctxName string) error {
+	if c.CurrentContext == nil {
+		c.CurrentContext = make(map[ContextType]string)
+	}
+	c.CurrentContext[ctxType] = ctxName
+	ctx, err := c.GetContext(ctxName)
+	if err != nil {
+		return err
+	}
+	if ctx.IsManagementCluster() || ctx.Type == CtxTypeTMC {
+		c.CurrentServer = ctxName
+	}
+	return nil
+}
+
+// IsManagementCluster tells if the context is for a management cluster.
+func (c *Context) IsManagementCluster() bool {
+	return c != nil && c.Type == CtxTypeK8s && c.ClusterOpts != nil && c.ClusterOpts.IsManagementCluster
+}
+
+// SetUnstableVersionSelector will help determine the unstable versions supported
+// In order of restrictiveness:
+// "all" -> "alpha" -> "experimental" -> "none"
+// none: return stable versions only. the default for both the config and the old flag.
+// alpha: only versions tagged with -alpha
+// experimental: all pre-release versions without +build semver data
+// all: return all unstable versions.
+func (c *ClientConfig) SetUnstableVersionSelector(f VersionSelectorLevel) {
+	if c.ClientOptions == nil {
+		c.ClientOptions = &ClientOptions{}
+	}
+	if c.ClientOptions.CLI == nil {
+		c.ClientOptions.CLI = &CLIOptions{}
+	}
+	switch f {
+	case AllUnstableVersions, AlphaUnstableVersions, ExperimentalUnstableVersions, NoUnstableVersions:
+		c.ClientOptions.CLI.UnstableVersionSelector = f
+		return
+	}
+	c.ClientOptions.CLI.UnstableVersionSelector = AllUnstableVersions
+}
+
+// IsConfigFeatureActivated return true if the feature is activated, false if not. An error if the featurePath is malformed
+func (c *ClientConfig) IsConfigFeatureActivated(featurePath string) (bool, error) {
+	plugin, flag, err := c.SplitFeaturePath(featurePath)
+	if err != nil {
+		return false, err
+	}
+
+	if c.ClientOptions == nil || c.ClientOptions.Features == nil ||
+		c.ClientOptions.Features[plugin] == nil || c.ClientOptions.Features[plugin][flag] == "" {
+		return false, nil
+	}
+
+	booleanValue, err := strconv.ParseBool(c.ClientOptions.Features[plugin][flag])
+	if err != nil {
+		errMsg := "error converting " + featurePath + " entry '" + c.ClientOptions.Features[plugin][flag] + "' to boolean value: " + err.Error()
+		return false, errors.New(errMsg)
+	}
+	return booleanValue, nil
+}
+
+// GetEnvConfigurations returns a map of environment variables to values
+// it returns nil if configuration is not yet defined
+func (c *ClientConfig) GetEnvConfigurations() map[string]string {
+	if c.ClientOptions == nil || c.ClientOptions.Env == nil {
+		return nil
+	}
+	return c.ClientOptions.Env
+}
+
+// SplitFeaturePath splits a features path into the pluginName and the featureName
+// For example "features.management-cluster.dual-stack" returns "management-cluster", "dual-stack"
+// An error results from a malformed path, including any path that does not start with "features."
+func (c *ClientConfig) SplitFeaturePath(featurePath string) (string, string, error) {
+	// parse the param
+	paramArray := strings.Split(featurePath, ".")
+	if len(paramArray) != 3 {
+		return "", "", errors.New("unable to parse feature name config parameter into three parts [" + featurePath + "]  (was expecting features.<plugin>.<feature>)")
+	}
+
+	featuresLiteral := paramArray[0]
+	plugin := paramArray[1]
+	flag := paramArray[2]
+
+	if featuresLiteral != "features" {
+		return "", "", errors.New("unsupported feature config path parameter [" + featuresLiteral + "] (was expecting 'features.<plugin>.<feature>')")
+	}
+	return plugin, flag, nil
+}
+
+// SetEditionSelector indicates the edition of tanzu to be run
+// EditionStandard is the default, EditionCommunity is also available.
+// These values affect branding and cluster creation
+func (c *ClientConfig) SetEditionSelector(edition EditionSelector) {
+	if c.ClientOptions == nil {
+		c.ClientOptions = &ClientOptions{}
+	}
+	if c.ClientOptions.CLI == nil {
+		c.ClientOptions.CLI = &CLIOptions{}
+	}
+	switch edition {
+	case EditionCommunity, EditionStandard:
+		c.ClientOptions.CLI.Edition = edition
+		return
+	}
+	c.ClientOptions.CLI.UnstableVersionSelector = EditionStandard
+}
---
+++ cli/runtime/apis/config/v1alpha1/clientconfig_types.go
@@ -0,0 +1,309 @@
+// Copyright 2021 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ServerType is the type of server.
+// Note: Shall be deprecated in a future version. Superseded by ContextType.
+type ServerType string
+
+// ContextType is the type of the context (control plane).
+type ContextType string
+
+const (
+	// ManagementClusterServerType is a management cluster server.
+	// Note: Shall be deprecated in a future version. Superseded by CtxTypeK8s.
+	ManagementClusterServerType ServerType = "managementcluster"
+
+	// GlobalServerType is a global control plane server.
+	// Note: Shall be deprecated in a future version. Superseded by CtxTypeTMC.
+	GlobalServerType ServerType = "global"
+
+	// CtxTypeK8s is a kubernetes cluster API server.
+	CtxTypeK8s ContextType = "k8s"
+
+	// CtxTypeTMC is a Tanzu Mission Control server.
+	CtxTypeTMC ContextType = "tmc"
+)
+
+// Server connection.
+// Note: Shall be deprecated in a future version. Superseded by Context.
+type Server struct {
+	// Name of the server.
+	Name string `json:"name,omitempty" yaml:"name"`
+
+	// Type of the endpoint.
+	Type ServerType `json:"type,omitempty" yaml:"type"`
+
+	// GlobalOpts if the server is global.
+	GlobalOpts *GlobalServer `json:"globalOpts,omitempty" yaml:"globalOpts"`
+
+	// ManagementClusterOpts if the server is a management cluster.
+	ManagementClusterOpts *ManagementClusterServer `json:"managementClusterOpts,omitempty" yaml:"managementClusterOpts"`
+
+	// DiscoverySources determines from where to discover plugins
+	// associated with this server
+	DiscoverySources []PluginDiscovery `json:"discoverySources,omitempty" yaml:"discoverySources"`
+}
+
+// Context configuration for a control plane. This can one of the following,
+// 1. Kubernetes Cluster
+// 2. Tanzu Mission Control endpoint
+type Context struct {
+	// Name of the context.
+	Name string `json:"name,omitempty" yaml:"name"`
+
+	// Type of the context.
+	Type ContextType `json:"type,omitempty" yaml:"type"`
+
+	// GlobalOpts if the context is a global control plane (e.g., TMC).
+	GlobalOpts *GlobalServer `json:"globalOpts,omitempty" yaml:"globalOpts"`
+
+	// ClusterOpts if the context is a kubernetes cluster.
+	ClusterOpts *ClusterServer `json:"clusterOpts,omitempty" yaml:"clusterOpts"`
+
+	// DiscoverySources determines from where to discover plugins
+	// associated with this context.
+	DiscoverySources []PluginDiscovery `json:"discoverySources,omitempty" yaml:"discoverySources"`
+}
+
+// ManagementClusterServer is the configuration for a management cluster kubeconfig.
+// Note: Shall be deprecated in a future version. Superseded by ClusterServer.
+type ManagementClusterServer struct {
+	// Endpoint for the login.
+	Endpoint string `json:"endpoint,omitempty" yaml:"endpoint"`
+
+	// Path to the kubeconfig.
+	Path string `json:"path,omitempty" yaml:"path"`
+
+	// The context to use (if required), defaults to current.
+	Context string `json:"context,omitempty" yaml:"context"`
+}
+
+// ClusterServer contains the configuration for a kubernetes cluster (kubeconfig).
+type ClusterServer struct {
+	// Endpoint for the login.
+	Endpoint string `json:"endpoint,omitempty" yaml:"endpoint"`
+
+	// Path to the kubeconfig.
+	Path string `json:"path,omitempty" yaml:"path"`
+
+	// The kubernetes context to use (if required), defaults to current.
+	Context string `json:"context,omitempty" yaml:"context"`
+
+	// Denotes whether this server is a management cluster or not (workload cluster).
+	IsManagementCluster bool `json:"isManagementCluster,omitempty" yaml:"isManagementCluster"`
+}
+
+// GlobalServer is the configuration for a global server.
+type GlobalServer struct {
+	// Endpoint for the server.
+	Endpoint string `json:"endpoint,omitempty" yaml:"endpoint"`
+
+	// Auth for the global server.
+	Auth GlobalServerAuth `json:"auth,omitempty" yaml:"auth"`
+}
+
+// GlobalServerAuth is authentication for a global server.
+type GlobalServerAuth struct {
+	// Issuer url for IDP, compliant with OIDC Metadata Discovery.
+	Issuer string `json:"issuer" yaml:"issuer"`
+
+	// UserName is the authorized user the token is assigned to.
+	UserName string `json:"userName" yaml:"userName"`
+
+	// Permissions are roles assigned to the user.
+	Permissions []string `json:"permissions" yaml:"permissions"`
+
+	// AccessToken is the current access token based on the context.
+	AccessToken string `json:"accessToken" yaml:"accessToken"`
+
+	// IDToken is the current id token based on the context scoped to the CLI.
+	IDToken string `json:"IDToken" yaml:"IDToken"`
+
+	// RefreshToken will be stored only in case of api-token login flow.
+	RefreshToken string `json:"refresh_token" yaml:"refresh_token"`
+
+	// Expiration times of the token.
+	Expiration metav1.Time `json:"expiration" yaml:"expiration"`
+
+	// Type of the token (user or client).
+	Type string `json:"type" yaml:"type"`
+}
+
+// ClientOptions are the client specific options.
+type ClientOptions struct {
+	// CLI options specific to the CLI.
+	CLI      *CLIOptions           `json:"cli,omitempty" yaml:"cli"`
+	Features map[string]FeatureMap `json:"features,omitempty" yaml:"features"`
+	Env      map[string]string     `json:"env,omitempty" yaml:"env"`
+}
+
+// FeatureMap is simply a hash table, but needs an explicit type to be an object in another hash map (cf ClientOptions.Features)
+type FeatureMap map[string]string
+
+// EnvMap is simply a hash table, but needs an explicit type to be an object in another hash map (cf ClientOptions.Env)
+type EnvMap map[string]string
+
+// CLIOptions are options for the CLI.
+type CLIOptions struct {
+	// Repositories are the plugin repositories.
+	Repositories []PluginRepository `json:"repositories,omitempty" yaml:"repositories"`
+	// DiscoverySources determines from where to discover stand-alone plugins
+	DiscoverySources []PluginDiscovery `json:"discoverySources,omitempty" yaml:"discoverySources"`
+	// UnstableVersionSelector determined which version tags are allowed
+	UnstableVersionSelector VersionSelectorLevel `json:"unstableVersionSelector,omitempty" yaml:"unstableVersionSelector"`
+	// Edition
+	Edition EditionSelector `json:"edition,omitempty" yaml:"edition"`
+	// Deprecated: BOMRepo has been deprecated and will be removed from future version
+	// BOMRepo is the root repository URL used to resolve the compatibiilty file
+	// and bill of materials. An example URL is projects.registry.vmware.com/tkg.
+	BOMRepo string `json:"bomRepo,omitempty" yaml:"bomRepo"`
+	// Deprecated: CompatibilityFilePath has been deprecated and will be removed from future version
+	// CompatibilityFilePath is the path, from the BOM repo, to download and access the compatibility file.
+	// the compatibility file is used for resolving the bill of materials for creating clusters.
+	CompatibilityFilePath string `json:"compatibilityFilePath,omitempty" yaml:"compatibilityFilePath"`
+}
+
+// PluginDiscovery contains a specific distribution mechanism. Only one of the
+// configs must be set.
+type PluginDiscovery struct {
+	// GCPStorage is set if the plugins are to be discovered via Google Cloud Storage.
+	GCP *GCPDiscovery `json:"gcp,omitempty"`
+	// OCIDiscovery is set if the plugins are to be discovered via an OCI Image Registry.
+	OCI *OCIDiscovery `json:"oci,omitempty"`
+	// GenericRESTDiscovery is set if the plugins are to be discovered via a REST API endpoint.
+	REST *GenericRESTDiscovery `json:"rest,omitempty"`
+	// KubernetesDiscovery is set if the plugins are to be discovered via the Kubernetes API server.
+	Kubernetes *KubernetesDiscovery `json:"k8s,omitempty"`
+	// LocalDiscovery is set if the plugins are to be discovered via Local Manifest fast.
+	Local *LocalDiscovery `json:"local,omitempty"`
+	// ContextType the discovery source is associated with (applicable only for stand-alone plugins).
+	ContextType ContextType `json:"contextType,omitempty"`
+}
+
+// GCPDiscovery provides a plugin discovery mechanism via a Google Cloud Storage
+// bucket with a manifest.yaml file.
+type GCPDiscovery struct {
+	// Name is a name of the discovery
+	Name string `json:"name"`
+	// Bucket is a Google Cloud Storage bucket.
+	// E.g., tanzu-cli
+	Bucket string `json:"bucket"`
+	// BasePath is a URI path that is prefixed to the object name/path.
+	// E.g., plugins/cluster
+	ManifestPath string `json:"manifestPath"`
+}
+
+// OCIDiscovery provides a plugin discovery mechanism via a OCI Image Registry
+type OCIDiscovery struct {
+	// Name is a name of the discovery
+	Name string `json:"name"`
+	// Image is an OCI compliant image. Which include DNS-compatible registry name,
+	// a valid URI path(MAY contain zero or more ‘/’) and a valid tag.
+	// E.g., harbor.my-domain.local/tanzu-cli/plugins-manifest:latest
+	// Contains a directory containing YAML files, each of which contains single
+	// CLIPlugin API resource.
+	Image string `json:"image"`
+}
+
+// GenericRESTDiscovery provides a plugin discovery mechanism via any REST API
+// endpoint. The fully qualified list URL is constructed as
+// `https://{Endpoint}/{BasePath}` and the get plugin URL is constructed as .
+// `https://{Endpoint}/{BasePath}/{Plugin}`.
+type GenericRESTDiscovery struct {
+	// Name is a name of the discovery
+	Name string `json:"name"`
+	// Endpoint is the REST API server endpoint.
+	// E.g., api.my-domain.local
+	Endpoint string `json:"endpoint"`
+	// BasePath is the base URL path of the plugin discovery API.
+	// E.g., /v1alpha1/cli/plugins
+	BasePath string `json:"basePath"`
+}
+
+// KubernetesDiscovery provides a plugin discovery mechanism via the Kubernetes API server.
+type KubernetesDiscovery struct {
+	// Name is a name of the discovery
+	Name string `json:"name"`
+	// Path to the kubeconfig.
+	Path string `json:"path"`
+	// The context to use (if required), defaults to current.
+	Context string `json:"context"`
+	// Version of the CLIPlugins API to query.
+	// E.g., v1alpha1
+	Version string `json:"version"`
+}
+
+// LocalDiscovery is a artifact discovery endpoint utilizing a local host OS.
+type LocalDiscovery struct {
+	// Name is a name of the discovery
+	Name string `json:"name"`
+	// Path is a local path pointing to directory
+	// containing YAML files, each of which contains single
+	// CLIPlugin API resource.
+	Path string `json:"path"`
+}
+
+// PluginRepository is a CLI plugin repository
+type PluginRepository struct {
+	// GCPPluginRepository is a plugin repository that utilizes GCP cloud storage.
+	GCPPluginRepository *GCPPluginRepository `json:"gcpPluginRepository,omitempty" yaml:"gcpPluginRepository"`
+}
+
+// GCPPluginRepository is a plugin repository that utilizes GCP cloud storage.
+type GCPPluginRepository struct {
+	// Name of the repository.
+	Name string `json:"name,omitempty" yaml:"name"`
+
+	// BucketName is the name of the bucket.
+	BucketName string `json:"bucketName,omitempty" yaml:"bucketName"`
+
+	// RootPath within the bucket.
+	RootPath string `json:"rootPath,omitempty" yaml:"rootPath"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClientConfig is the Schema for the configs API
+type ClientConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// KnownServers available.
+	// Note: Shall be deprecated in a future version. Superseded by KnownContexts.
+	KnownServers []*Server `json:"servers,omitempty" yaml:"servers"`
+
+	// CurrentServer in use.
+	// Note: Shall be deprecated in a future version. Superseded by CurrentContext.
+	CurrentServer string `json:"current,omitempty" yaml:"current"`
+
+	// KnownContexts available.
+	KnownContexts []*Context `json:"contexts,omitempty" yaml:"contexts"`
+
+	// CurrentContext for every type.
+	CurrentContext map[ContextType]string `json:"currentContext,omitempty" yaml:"currentContext"`
+
+	// ClientOptions are client specific options.
+	ClientOptions *ClientOptions `json:"clientOptions,omitempty" yaml:"clientOptions"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClientConfigList contains a list of ClientConfig
+type ClientConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClientConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClientConfig{}, &ClientConfigList{})
+}
