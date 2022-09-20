9bbb2fc23cc5e89dcd33c6db0e266c3f9420df91
Sathyanarayanan Saravanamuthu <sathyanarays@.vmware.com>
2022-09-20 13:25:42 +0530

    Removing tkrtypes directory by moving in required code to tkg/registry directory
    Signed-off-by: Sathyanarayanan Saravanamuthu <sathyanarays@.vmware.com>

--- tkg/tkrtypes/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2021 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package tkrtypes defines some common types used in TKR.
-// Copied over from github.com/vmware-tanzu/tanzu-framework/pkg/v1/tkr/pkg/types
-package tkrtypes
