de490b613b521e86b13ad6118c466586a235c6d1
lubronzhan <lzhan@vmware.com>
2022-09-26 15:09:28 -0700

    Update import

--- addons/controllers/csi/vspherecsiconfig_controller.go
+++ addons/controllers/csi/vspherecsiconfig_controller.go
@@ -15,6 +15,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	capvvmwarev1beta1 "sigs.k8s.io/cluster-api-provider-vsphere/apis/vmware/v1beta1"
 	clusterapiv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	clusterapiutil "sigs.k8s.io/cluster-api/util"
 	clusterapipatchutil "sigs.k8s.io/cluster-api/util/patch"
