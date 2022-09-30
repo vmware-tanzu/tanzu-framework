793be6bd911c3d92748567b0a482489876b99db8
Anuj Chaudhari <anujc@vmware.com>
2022-09-29 18:18:11 -0700

    Add ConfigDefaultFeatureFlagsIfMissing API

--- cli/runtime/config/featureflags.go
+++ cli/runtime/config/featureflags.go
@@ -4,7 +4,10 @@
 package config
 
 import (
+	"strconv"
 	"strings"
+
+	configapi "github.com/vmware-tanzu/tanzu-framework/cli/runtime/apis/config/v1alpha1"
 )
 
 var (
@@ -93,4 +96,44 @@ var (
 // is set to true during build time
 func ContextAwareDiscoveryEnabled() bool {
 	return strings.EqualFold(IsContextAwareDiscoveryEnabled, "true")
+}
+
+func ConfigureDefaultFeatureFlagsIfMissing(defaultFeatureFlags map[string]bool) error {
+	c, err := GetClientConfig()
+	if err != nil {
+		return err
+	}
+
+	for featurePath, activated := range defaultFeatureFlags {
+		plugin, feature, err := c.SplitFeaturePath(featurePath)
+		if err == nil && !containsFeatureFlag(c, plugin, feature) {
+			addFeatureFlag(c, plugin, feature, activated)
+		}
+	}
+
+	err = StoreClientConfig(c)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// containsFeatureFlag returns true if the features section in the configuration object contains any value for the plugin.feature combination
+func containsFeatureFlag(c *configapi.ClientConfig, plugin, feature string) bool {
+	return c.ClientOptions != nil && c.ClientOptions.Features != nil && c.ClientOptions.Features[plugin] != nil &&
+		c.ClientOptions.Features[plugin][feature] != ""
+}
+
+func addFeatureFlag(c *configapi.ClientConfig, plugin, flag string, flagValue bool) {
+	if c.ClientOptions == nil {
+		c.ClientOptions = &configapi.ClientOptions{}
+	}
+	if c.ClientOptions.Features == nil {
+		c.ClientOptions.Features = make(map[string]configapi.FeatureMap)
+	}
+	if c.ClientOptions.Features[plugin] == nil {
+		c.ClientOptions.Features[plugin] = make(map[string]string)
+	}
+	c.ClientOptions.Features[plugin][flag] = strconv.FormatBool(flagValue)
 }
