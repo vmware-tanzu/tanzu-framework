0823ed627246632c76894ad2556939b3b7bb991d
Prem Kumar Kalle <pkalle@vmware.com>
2022-10-04 17:43:05 -0700

    Adding 'pluginRuntimeVersion' to PluginDescriptor
    Signed-off-by: Prem Kumar Kalle <pkalle@vmware.com>

--- cli/runtime/plugin/plugin.go
+++ cli/runtime/plugin/plugin.go
@@ -14,6 +14,7 @@ import (
 	"golang.org/x/mod/semver"
 
 	cliapi "github.com/vmware-tanzu/tanzu-framework/cli/runtime/apis/cli/v1alpha1"
+	rtversion "github.com/vmware-tanzu/tanzu-framework/cli/runtime/version"
 )
 
 // Plugin is a Tanzu CLI plugin.
@@ -76,6 +77,7 @@ func parsePluginDescriptor(path string) (desc cliapi.PluginDescriptor, err error
 
 // ApplyDefaultConfig applies default configurations to plugin descriptor.
 func ApplyDefaultConfig(p *cliapi.PluginDescriptor) {
+	p.PluginRuntimeVersion = rtversion.Version
 	if p.PostInstallHook == nil {
 		p.PostInstallHook = func() error {
 			return nil
