82df25b0736cfc2d9105e91185ef6a16672729b1
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-10-11 02:44:59 -0700

    testing
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

--- hack/clustergen/ci-validate-clustergen.sh
+++ hack/clustergen/ci-validate-clustergen.sh
@@ -29,11 +29,12 @@ git checkout .
 echo git checkout -B old origin/${GIT_BRANCH_PROVIDERS_BASE}
 git checkout -B old origin/${GIT_BRANCH_PROVIDERS_BASE}
 git log --pretty=oneline -5
+pushd ../pkg/v1/providers
 make generate-bindata
 make CLUSTERGEN_CC_OUTPUT_DIR=oldcc CLUSTERGEN_OUTPUT_DIR=old GOOS=${GOOS} GOARCH=${GOARCH} CLI_REPO=${CLI_REPO} cluster-generation-tests
 git checkout .
 git checkout -
-
+popd
 pushd tests/clustergen/testdata
 
 diff -r -U15 old new > clustergen.diff.txt
