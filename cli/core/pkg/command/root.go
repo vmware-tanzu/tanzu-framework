080143b30e62035d70c128dedd36145f3974a8a9
Vui Lam <vui@vmware.com>
2022-10-13 10:57:20 -0700

    Turn on context-target functionality
    Fixes: #3653
    
    This change eliminates the cli feature flag
    globals.features.context-target and enables the functionality
    unconditionally.
    
    Note that the `tanzu login` and `tanzu config server ...` commands
    (which the context commands will eventually replace) will still be
    supported, and will be deprecated at some point in the future.
    
    Signed-off-by: Vui Lam <vui@vmware.com>

--- cli/core/pkg/command/root.go
+++ cli/core/pkg/command/root.go
@@ -66,19 +66,18 @@ func NewRootCmd() (*cobra.Command, error) {
 		genAllDocsCmd,
 	)
 
-	// If the context and target feature is enabled, add the corresponding commands under root.
-	if config.IsFeatureActivated(config.FeatureContextCommand) {
-		RootCmd.AddCommand(
-			contextCmd,
-			k8sCmd,
-			tmcCmd,
-		)
-		if err := addCtxPlugins(k8sCmd, configapi.CtxTypeK8s); err != nil {
-			return nil, err
-		}
-		if err := addCtxPlugins(tmcCmd, configapi.CtxTypeTMC); err != nil {
-			return nil, err
-		}
+	// Now that the context and target features are activated, add the
+	// corresponding commands under root unconditionally.
+	RootCmd.AddCommand(
+		contextCmd,
+		k8sCmd,
+		tmcCmd,
+	)
+	if err := addCtxPlugins(k8sCmd, configapi.CtxTypeK8s); err != nil {
+		return nil, err
+	}
+	if err := addCtxPlugins(tmcCmd, configapi.CtxTypeTMC); err != nil {
+		return nil, err
 	}
 
 	plugins, err := getAvailablePlugins()
