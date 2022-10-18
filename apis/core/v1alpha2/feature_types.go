34ce8e9af898791ade46c9b5745e1fb4784da3c0
Harish Yayi <yharish991@gmail.com>
2022-10-18 09:18:33 -0400

    add Feature and FeatureGate API in core API group
    Signed-off-by: Harish Yayi <yharish991@gmail.com>

---
+++ apis/core/v1alpha2/feature_types.go
@@ -0,0 +1,58 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// FeatureSpec defines the desired state of Feature
+type FeatureSpec struct {
+	// Description of the feature.
+	Description string `json:"description,omitempty"`
+	// Immutable indicates this feature cannot be toggled once set
+	// If set at creation time, this state will persist for the life of the cluster
+	Immutable bool `json:"immutable"`
+	// Stability indicates stability level of this feature.
+	// Stability levels are Work In Progress, Experimental, Technical Preview, Stable and Deprecated.
+	// +kubebuilder:validation:Enum=Work In Progress;Experimental;Technical Preview;Stable;Deprecated
+	// - Work In Progress: the default for new resources, represents local dev. intended to be hidden and deactivated
+	// - Experimental: the first milestone meant for limited wider consumption, discoverable and defaults to deactivated
+	// - Technical Preview: greater visibility for proven designs, discoverable and defaults to activated
+	// - Stable: intended to be part of the mainline codebase, non-optional
+	// - Deprecated: destined for future removal
+	Stability string `json:"stability"`
+}
+
+// FeatureStatus defines the observed state of Feature
+type FeatureStatus struct {
+	// Activated is a boolean which indicates whether a feature is activated or not.
+	Activated bool `json:"activated"`
+}
+
+// Feature is the Schema for the features API
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:printcolumn:name="Description",type=string,JSONPath=.spec.description
+// +kubebuilder:printcolumn:name="Immutable",type=boolean,JSONPath=.spec.immutable
+// +kubebuilder:printcolumn:name="Stability",type=string,JSONPath=.spec.stability
+type Feature struct {
+	Status            FeatureStatus `json:"status,omitempty"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              FeatureSpec `json:"spec,omitempty"`
+	metav1.TypeMeta   `json:",inline"`
+}
+
+// +kubebuilder:object:root=true
+
+// FeatureList contains a list of Feature
+type FeatureList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Feature `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Feature{}, &FeatureList{})
+}
---
+++ apis/core/v1alpha2/featuregate_types.go
@@ -0,0 +1,85 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha2
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// FeatureReference refers to a Feature resource and specifies its intended activation state.
+type FeatureReference struct {
+	// Name is the name of the Feature resource, which represents a feature the system offers.
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+	// Activate indicates the activation intent for the feature.
+	Activate bool `json:"activate,omitempty"`
+	// SkipStabilityValidation lets you skip the validation performed when activating an unstable feature.
+	SkipStabilityValidation bool `json:"skipStabilityValidation,omitempty"`
+}
+
+// FeatureGateSpec defines the desired state of FeatureGate
+type FeatureGateSpec struct {
+	// Features is a slice of FeatureReference to gate features.
+	// The Feature resource specified may or may not be present in the system. If the Feature is present, the
+	// FeatureGate controller and webhook sets the specified activation state only if the Feature is discoverable and
+	// its immutability constraint is satisfied. If the Feature is not present, the activation intent is applied when
+	// the Feature resource appears in the system. The actual activation state of the Feature is reported in the status.
+	// +listType=map
+	// +listMapKey=name
+	Features []FeatureReference `json:"features,omitempty"`
+}
+
+// FeatureGateStatus defines the observed state of FeatureGate
+type FeatureGateStatus struct {
+	// Results represents the results of all the features specified in the FeatureGate spec.
+	// +listType=map
+	// +listMapKey=name
+	Results []Result `json:"results"`
+}
+
+// Result represents the result of Feature.
+type Result struct {
+	// Name is the name of the feature.
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:MinLength:=1
+	Name string `json:"name"`
+	// Status represents the outcome of the feature operation specified in the spec
+	// +kubebuilder:validation:Required
+	// +kubebuilder:validation:Enum=applied;no-op;invalid
+	// - applied: represents feature toggle has been successfully applied.
+	// - no-op: represent no operation has been done, feature is already in the intended state.
+	// - invalid: represents that the intended state of the feature is invalid.
+	Status string `json:"status"`
+	// Message represents the reason for invalid status
+	// +optional
+	Message string `json:"message,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+//+kubebuilder:resource:scope=Cluster
+
+// FeatureGate is the Schema for the featuregates API
+type FeatureGate struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// Spec is the specification for gating features.
+	Spec FeatureGateSpec `json:"spec,omitempty"`
+	// Status reports the observed state of the FeatureGate
+	Status FeatureGateStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// FeatureGateList contains a list of FeatureGate
+type FeatureGateList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []FeatureGate `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&FeatureGate{}, &FeatureGateList{})
+}
