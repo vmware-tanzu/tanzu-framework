8b49a5f0f49f60be5ed8623742d0492b1fbe9d57
Leo Li <lhaoran@vmware.com>
2022-10-18 22:27:13 -0700

    Avoid adding last-applied annotation on kapp-controller on workload cluster (#3663)
    - Let the logic only works on management cluster when the feature flag is off.

--- tkg/client/upgrade_addon.go
+++ tkg/client/upgrade_addon.go
@@ -248,8 +248,8 @@ func (c *TkgClient) DoUpgradeAddon(regionalClusterClient clusterclient.Client, /
 			return errors.Wrap(err, "unable to get cluster configuration")
 		}
 
-		// ensure current kapp-controller deployment has last-applied annotation, which is required for future apply operations
-		if addonName == "addons-management/kapp-controller" {
+		// ensure current kapp-controller deployment on the management cluster has last-applied annotation, which is required for future apply operations
+		if addonName == "addons-management/kapp-controller" && options.IsRegionalCluster {
 			if err := clusterClient.PatchKappControllerLastAppliedAnnotation(options.Namespace); err != nil {
 				return errors.Wrap(err, "unable to add last-applied annotation on kapp-controller")
 			}
