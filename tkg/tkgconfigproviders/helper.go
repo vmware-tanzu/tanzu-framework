d1ea0c0abed841ba2e2de89a1468251da6588e5e
Justin Miclette <miclettej@vmware.com>
2022-10-24 13:48:28 -0700

    Kickstart UI: Remove trailing comma when converting a string map to string (#3615)
    * Kickstart UI: Remove trailing comma when converting a string map to single string
    
    Fixes issue with conversion of string map leaving trailing comma on converted single string
    
    Co-authored-by: Justin Miclette <miclettej@miclettej-a02.vmware.com>

--- tkg/tkgconfigproviders/helper.go
+++ tkg/tkgconfigproviders/helper.go
@@ -6,21 +6,25 @@ package tkgconfigproviders
 import "strings"
 
 // mapToConfigString returns a string with key:value pairs separated by ,
-// For example: "key1:value1,key2:value2,"
+// For example: "key1:value1,key2:value2"
 // An empty map returns an empty string
-// Note that a non-empty return value always has a trailing comma
+// An empty key is ignored
 func mapToConfigString(data map[string]string) string {
 	result := ""
 	for key, value := range data {
-		result += key + ":" + value + ","
+		if len(key) > 0 {
+			result += key + ":" + value + ","
+		}
 	}
+	result = strings.TrimSuffix(result, ",")
 	return result
 }
 
 // configStringToMap parses a data string into a map
-// The expected format is: "key1:value1,key2:value2,"
+// The expected format is: "key1:value1,key2:value2"
 // Blank values are ignored; any key-value with no colon (or multiple colons) is ignored;
 // An empty data string is allowed; an empty map will be returned
+// A string ending with a comma is allowed ("key1:value1,key2:value2,")
 func configStringToMap(data string) map[string]string {
 	result := make(map[string]string)
 
