2b1ec3fac1c8896496446e53be8d434dac515e42
Christian Schlotter <christi.schlotter@gmail.com>
2022-11-07 14:05:53 +0100

    raise the bar to v1.24

--- tkg/client/admission_upgrade_helper.go
+++ tkg/client/admission_upgrade_helper.go
@@ -22,9 +22,9 @@ plugins:
       enforce: ""
       enforce-version: ""
       audit: "baseline"
-      audit-version: "v1.23"
+      audit-version: "v1.24"
       warn: "baseline"
-      warn-version: "v1.23"
+      warn-version: "v1.24"
     exemptions:
       usernames: []
       runtimeClasses: []
