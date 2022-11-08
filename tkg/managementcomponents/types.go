chenlin07/tanzu-framework-1

Add ako-operator as management composite packages
Signed-off-by: Chen Lin <linch@vmware.com>

tkg/managementcomponents/types.go
@@ -11,6 +11,7 @@ type TKGPackageConfig struct {
 	ClusterClassPackage          ClusterClassPackage          `yaml:"clusterclassPackage"`
 	TKRSourceControllerPackage   TKRSourceControllerPackage   `yaml:"tkrSourceControllerPackage"`
 	CoreManagementPluginsPackage CoreManagementPluginsPackage `yaml:"coreManagementPluginsPackage"`
+	AkoOperatorPackage           AkoOperatorPackage           `yaml:"akoOperator"`
 }
 
 // Metadata specifies metadata as part of TKG package config
@@ -47,6 +48,10 @@ type ClusterClassPackage struct {
 	NamespaceForPackageInstallation string                         `yaml:"namespaceForPackageInstallation,omitempty"`
 	VersionConstraints              string                         `yaml:"versionConstraints,omitempty"`
 	ClusterClassInfraPackageValues  ClusterClassInfraPackageValues `yaml:"clusterclassInfraPackageValues,omitempty"`
+}
+
+type AkoOperatorPackage struct {
+	AkoOperatorPackageValues AkoOperatorPackageValues `yaml:"akoOperator,omitempty"`
 }
 
 type AddonsFeatureGates struct {
@@ -100,4 +105,28 @@ type TanzuAuthPackageValues struct {
 	Namespace          string `yaml:"namespace,omitempty"`
 	CreateNamespace    string `yaml:"createNamespace,omitempty"`
 	VersionConstraints string `yaml:"versionConstraints,omitempty"`
+}
+
+type AkoOperatorPackageValues struct {
+	AviEnable         string            `yaml:"avi_enable,omitempty"`
+	AviOperatorConfig AviOperatorConfig `yaml:"config,omitempty"`
+}
+
+type AviOperatorConfig struct {
+	AviControllerAddress                           string `yaml:"avi_controller,omitempty"`
+	AviControllerVersion                           string `yaml:"avi_controller_version,omitempty"`
+	AviControllerUsername                          string `yaml:"avi_username,omitempty"`
+	AviControllerPassword                          string `yaml:"avi_password,omitempty"`
+	AviControllerCA                                string `yaml:"avi_ca_data_b64,omitempty"`
+	AviCloudName                                   string `yaml:"avi_cloud_name,omitempty"`
+	AviServiceEngineGroup                          string `yaml:"avi_service_engine_group,omitempty"`
+	AviManagementClusterServiceEngineGroup         string `yaml:"avi_management_cluster_service_engine_group,omitempty"`
+	AviDataPlaneNetworkName                        string `yaml:"avi_data_network,omitempty"`
+	AviDataPlaneNetworkCIDR                        string `yaml:"avi_data_network_cidr,omitempty"`
+	AviControlPlaneNetworkName                     string `yaml:"avi_control_plane_network,omitempty"`
+	AviControlPlaneNetworkCIDR                     string `yaml:"avi_control_plane_network_cidr,omitempty"`
+	AviManagementClusterDataPlaneNetworkName       string `yaml:"avi_management_cluster_vip_network_name,omitempty"`
+	AviManagementClusterDataPlaneNetworkCIDR       string `yaml:"avi_management_cluster_vip_network_cidr,omitempty"`
+	AviManagementClusterControlPlaneVipNetworkName string `yaml:"avi_management_cluster_control_plane_vip_network_name,omitempty"`
+	AviManagementClusterControlPlaneVipNetworkCIDR string `yaml:"avi_management_cluster_control_plane_vip_network_cidr,omitempty"`
 }

vmware-tanzu/tanzu-framework

tkg/managementcomponents/types.go
@@ -11,6 +11,7 @@ type TKGPackageConfig struct {
 	ClusterClassPackage          ClusterClassPackage          `yaml:"clusterclassPackage"`
 	TKRSourceControllerPackage   TKRSourceControllerPackage   `yaml:"tkrSourceControllerPackage"`
 	CoreManagementPluginsPackage CoreManagementPluginsPackage `yaml:"coreManagementPluginsPackage"`
+	AkoOperatorPackage           AkoOperatorPackage           `yaml:"akoOperatorPackage"`
 }
 
 // Metadata specifies metadata as part of TKG package config
@@ -55,6 +56,10 @@ type ClusterClassPackage struct {
 	NamespaceForPackageInstallation string                         `yaml:"namespaceForPackageInstallation,omitempty"`
 	VersionConstraints              string                         `yaml:"versionConstraints,omitempty"`
 	ClusterClassInfraPackageValues  ClusterClassInfraPackageValues `yaml:"clusterclassInfraPackageValues,omitempty"`
+}
+
+type AkoOperatorPackage struct {
+	AkoOperatorPackageValues AkoOperatorPackageValues `yaml:"akoOperator,omitempty"`
 }
 
 type AddonsFeatureGates struct {
@@ -108,4 +113,28 @@ type TanzuAuthPackageValues struct {
 	Namespace          string `yaml:"namespace,omitempty"`
 	CreateNamespace    string `yaml:"createNamespace,omitempty"`
 	VersionConstraints string `yaml:"versionConstraints,omitempty"`
+}
+
+type AkoOperatorPackageValues struct {
+	AviEnable         string            `yaml:"avi_enable,omitempty"`
+	AviOperatorConfig AviOperatorConfig `yaml:"config,omitempty"`
+}
+
+type AviOperatorConfig struct {
+	AviControllerAddress                           string `yaml:"avi_controller,omitempty"`
+	AviControllerVersion                           string `yaml:"avi_controller_version,omitempty"`
+	AviControllerUsername                          string `yaml:"avi_username,omitempty"`
+	AviControllerPassword                          string `yaml:"avi_password,omitempty"`
+	AviControllerCA                                string `yaml:"avi_ca_data_b64,omitempty"`
+	AviCloudName                                   string `yaml:"avi_cloud_name,omitempty"`
+	AviServiceEngineGroup                          string `yaml:"avi_service_engine_group,omitempty"`
+	AviManagementClusterServiceEngineGroup         string `yaml:"avi_management_cluster_service_engine_group,omitempty"`
+	AviDataPlaneNetworkName                        string `yaml:"avi_data_network,omitempty"`
+	AviDataPlaneNetworkCIDR                        string `yaml:"avi_data_network_cidr,omitempty"`
+	AviControlPlaneNetworkName                     string `yaml:"avi_control_plane_network,omitempty"`
+	AviControlPlaneNetworkCIDR                     string `yaml:"avi_control_plane_network_cidr,omitempty"`
+	AviManagementClusterDataPlaneNetworkName       string `yaml:"avi_management_cluster_vip_network_name,omitempty"`
+	AviManagementClusterDataPlaneNetworkCIDR       string `yaml:"avi_management_cluster_vip_network_cidr,omitempty"`
+	AviManagementClusterControlPlaneVipNetworkName string `yaml:"avi_management_cluster_control_plane_vip_network_name,omitempty"`
+	AviManagementClusterControlPlaneVipNetworkCIDR string `yaml:"avi_management_cluster_control_plane_vip_network_cidr,omitempty"`
 }