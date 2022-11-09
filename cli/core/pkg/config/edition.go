69f2e35286d74272bb4faba32bd0e622a2d106ab
Vui Lam <vui@vmware.com>
2022-11-08 23:15:33 -0800

    Address linter issues for cli/core
    Signed-off-by: Vui Lam <vui@vmware.com>

--- cli/core/pkg/config/edition.go
+++ cli/core/pkg/config/edition.go
@@ -14,12 +14,12 @@ func addEdition(c *configapi.ClientConfig, edition configapi.EditionSelector) {
 	if c.ClientOptions.CLI == nil {
 		c.ClientOptions.CLI = &configapi.CLIOptions{}
 	}
-	c.ClientOptions.CLI.Edition = edition
+	c.ClientOptions.CLI.Edition = edition //nolint:staticcheck
 }
 
 // addDefaultEditionIfMissing returns true if the default edition was added to the configuration (because there was no edition)
 func addDefaultEditionIfMissing(config *configapi.ClientConfig) bool {
-	if config.ClientOptions == nil || config.ClientOptions.CLI == nil || config.ClientOptions.CLI.Edition == "" {
+	if config.ClientOptions == nil || config.ClientOptions.CLI == nil || config.ClientOptions.CLI.Edition == "" { //nolint:staticcheck
 		addEdition(config, DefaultEdition)
 		return true
 	}
