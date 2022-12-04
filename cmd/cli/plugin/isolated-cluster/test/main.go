7d44c7c3cf2a3908c29bbbc16a343d3f9b36d1d8
Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>
2022-12-04 07:50:26 -0800

    Add new airgapped plugin as standalone plugin
    Signed-off-by: Vandana Pathak <pathakv@sjc-ubu-eng-127.vmware.com>

---
+++ cmd/cli/plugin/isolated-cluster/test/main.go
@@ -0,0 +1,41 @@
+// Copyright 2022 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// nolint: deadcode,unused,stylecheck
+package test
+
+import (
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/vmware-tanzu/tanzu-framework/cli/runtime/plugin"
+	clitest "github.com/vmware-tanzu/tanzu-framework/cli/runtime/test"
+)
+
+func main() {
+	pluginName := "isolated-cluster"
+	descriptor := clitest.NewTestFor(pluginName)
+
+	p, err := plugin.NewPlugin(descriptor)
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.Cmd.RunE = test
+	if err := p.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func test(c *cobra.Command, _ []string) error {
+	if err := Cleanup(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Cleanup the test.
+func Cleanup() error {
+	return nil
+}
