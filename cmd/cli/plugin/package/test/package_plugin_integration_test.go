09d491ff2d87f937bf7cf18bc83aaa198e37c661
Praveen Rewar <8457124+praveenrewar@users.noreply.github.com>
2022-12-13 13:58:24 +0530

    Rename kctrl-command-tree feature flag
    Use `features.package.kctrl-command-tree` instead of `features.package.kctrl-package-command-tree`. This is done to ensure that the default value is toggled to true even while upgrading from an older version.

--- cmd/cli/plugin/package/test/package_plugin_integration_test.go
+++ cmd/cli/plugin/package/test/package_plugin_integration_test.go
@@ -661,7 +661,7 @@ func disableKctrlCommandTree() {
 	By("disable kctrl command tree")
 	command := exec.NewCommand(
 		exec.WithCommand("tanzu"),
-		exec.WithArgs("config", "set", "features.package.kctrl-package-command-tree", "false"),
+		exec.WithArgs("config", "set", "features.package.kctrl-command-tree", "false"),
 		exec.WithStdout(GinkgoWriter),
 	)
 	err = command.RunAndRedirectOutput(context.Background())
