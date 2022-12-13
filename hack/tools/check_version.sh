ccf4ba0cde44c00794d8acf50b92e01ef453d097
jay vyas <jvyas@vmware.com>
2022-12-13 11:36:09 -0500

    lint fix

--- hack/tools/check_version.sh
+++ hack/tools/check_version.sh
@@ -4,7 +4,7 @@
 # exit 0 if the program, when called from path, has the version in its output.
 VERSION=`$1/$2 version` 
 
-# We dont check v bc, we dont care about "v0.40.0" vs "0.40.0" and differnt
+# We dont check v bc, we dont care about "v0.40.0" vs "0.40.0" and some
 # programs print version strings out differently.
 if echo $VERSION | `grep -q $3 | sed 's/v//g'` ; then
     echo "correct version *** $VERSION ***"
