bbc0acdcd57d4d74067c782930b26dd704d88a90
jay vyas <jvyas@vmware.com>
2022-12-13 18:47:31 -0500

    version fix

--- hack/tools/check_version.sh
+++ hack/tools/check_version.sh
@@ -6,10 +6,13 @@
 # Takes path , program, version as input
 # exit 0 if the program, when called from path, has the version in its output.
 VERSION=`$1/$2 version` 
+# remove the "v" from the version string...
+VERSION_NUM=`echo $VERSION | sed 's/v//g'`
+EXPECTED_NUM=`echo $3 | sed 's/v//g'`
 
 # We dont check v bc, we dont care about "v0.40.0" vs "0.40.0" and some
 # programs print version strings out differently.
-if echo $VERSION | `grep -q $3 | sed 's/v//g'` ; then
+if echo $VERSION_NUM | grep -q $EXPECTED_NUM ; then
     echo "correct version *** $VERSION ***"
     exit 0
 else
