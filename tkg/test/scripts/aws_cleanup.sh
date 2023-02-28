ca37900632efb7c8704024bb4589713fe5e47826
Ivan Mikushin <imikushin@vmware.com>
2023-02-27 16:22:24 -0800

    Mask temporary AWS credentials.
    Signed-off-by: Ivan Mikushin <imikushin@vmware.com>

--- tkg/test/scripts/aws_cleanup.sh
+++ tkg/test/scripts/aws_cleanup.sh
@@ -3,8 +3,7 @@
 # Copyright 2021 VMware, Inc. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# Turn off debugging (to prevent printing out AWS credentials)
-set +x
+set -x
 
 AWS_ACCESS_KEY_ID=$1
 AWS_SECRET_ACCESS_KEY=$2
@@ -22,7 +21,6 @@ if [ -z "$FILTER" ]; then
   exit 1
 fi
 
-set -x
 wget -nv https://github.com/genevieve/leftovers/releases/download/v0.62.0/leftovers-v0.62.0-linux-amd64
 mv leftovers-v0.62.0-linux-amd64 /usr/local/bin/leftovers
 chmod +x /usr/local/bin/leftovers
@@ -30,25 +28,18 @@ chmod +x /usr/local/bin/leftovers
 wget -nv https://github.com/kubernetes-sigs/kind/releases/download/v0.11.0/kind-linux-amd64
 mv kind-linux-amd64 /usr/local/bin/kind
 chmod +x /usr/local/bin/kind
-set +x
 
 # Delete any kind cluster that are left behind
 echo "Deleting any kind clusters that are left behind"
-set -x
 kind get clusters | xargs -n 1 kind delete cluster --name
-set +x
 
 # Run dry-run to see all resources
-echo leftovers -d -i aws --aws-access-key-id="***" --aws-secret-access-key="***" --aws-session-token="***" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n
 leftovers -d -i aws --aws-access-key-id="${AWS_ACCESS_KEY_ID}" --aws-secret-access-key="${AWS_SECRET_ACCESS_KEY}" --aws-session-token="${AWS_SESSION_TOKEN}" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n
 
-echo leftovers -i aws --aws-access-key-id="***" --aws-secret-access-key="***" --aws-session-token="***" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n || true
 leftovers -i aws --aws-access-key-id="${AWS_ACCESS_KEY_ID}" --aws-secret-access-key="${AWS_SECRET_ACCESS_KEY}" --aws-session-token="${AWS_SESSION_TOKEN}" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n || true
 
 # Run dry-run to see any leftover resources
-echo leftovers -d -i aws --aws-access-key-id="***" --aws-secret-access-key="***" --aws-session-token="***" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n
 leftovers -d -i aws --aws-access-key-id="${AWS_ACCESS_KEY_ID}" --aws-secret-access-key="${AWS_SECRET_ACCESS_KEY}" --aws-session-token="${AWS_SESSION_TOKEN}" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n
 
 # Retry the deletion incase the previous attempt failed
-echo leftovers -i aws --aws-access-key-id="***" --aws-secret-access-key="***" --aws-session-token="***" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n
 leftovers -i aws --aws-access-key-id="${AWS_ACCESS_KEY_ID}" --aws-secret-access-key="${AWS_SECRET_ACCESS_KEY}" --aws-session-token="${AWS_SESSION_TOKEN}" --filter="${FILTER}" --aws-region="${AWS_REGION}" -n
---
+++ tkg/test/scripts/aws_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright 2021 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Turn off debugging (to prevent printing out AWS credentials)
+set +x
+
+CG_TOKEN=$(curl -s --location --request POST 'https://api.console.cloudgate.vmware.com/authn/token' \
+  --user '${{ secrets.CG_ID }}:${{ secrets.CG_SECRET }}' \
+  --header 'Content-Type: application/json' \
+  --data-raw '{"grant_type": "client_credentials"}' | jq -r '.access_token')
+
+aws_creds=$(curl -s --location --request POST 'https://api.console.cloudgate.vmware.com/access/access' \
+  --header "Authorization: Bearer ${CG_TOKEN}" \
+  --header 'Content-Type: application/json' \
+  --data-raw '{"ouId":"ou-kw69-lqh1erao","orgAccountId":"942999320260","masterAccountId":"116462199383","role":"PowerUser"}')
+
+echo "AWS_ACCESS_KEY_ID="$(echo $aws_creds | jq -r '.credentials.accessKeyId') >> $GITHUB_ENV
+echo "AWS_SECRET_ACCESS_KEY="$(echo $aws_creds | jq -r '.credentials.secretAccessKey') >> $GITHUB_ENV
+echo "AWS_SESSION_TOKEN="$(echo $aws_creds | jq -r '.credentials.sessionToken') >> $GITHUB_ENV
+echo "AWS_REGION=us-west-2" >> $GITHUB_ENV
