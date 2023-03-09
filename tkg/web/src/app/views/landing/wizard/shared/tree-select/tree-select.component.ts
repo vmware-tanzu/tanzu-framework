426f04321c1dc41118fe14a152fb009b16b216aa
raymond zhang <zraymond@vmware.com>
2023-03-08 21:59:07 -0800

    Kickstart UI: fixed a11y issues

--- tkg/web/src/app/views/landing/wizard/shared/tree-select/tree-select.component.ts
+++ tkg/web/src/app/views/landing/wizard/shared/tree-select/tree-select.component.ts
@@ -1,18 +1,18 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Key } from 'src/app/shared/constants/app.constants';
 
 @Component({
     selector: 'app-tree-select',
     templateUrl: './tree-select.component.html',
-    styleUrls: ['./tree-select.component.scss']
+    styleUrls: ['./tree-select.component.scss'],
 })
 export class TreeSelectComponent implements OnInit {
     @Input() data: any;
     @Input() selectedHandler: any;
 
-    constructor() { }
+    constructor() {}
 
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
 
     handleClick(selected): void {
         this.selectedHandler(selected);
@@ -22,4 +22,17 @@ export class TreeSelectComponent implements OnInit {
         selected.isExpanded = !selected.isExpanded;
     }
 
+    onKeyup(event, node): boolean {
+        if (event.keyCode === Key.Enter) {
+            node.checked = !node.checked;
+            this.selectedHandler(node);
+        } else if (event.keyCode === Key.LeftArrow) {
+            node.isExpanded = true;
+        } else if (event.keyCode === Key.RightArrow) {
+            node.isExpanded = false;
+        } else {
+            return true;
+        }
+        return false;
+    }
 }
