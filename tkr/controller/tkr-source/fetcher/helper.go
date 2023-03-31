c2aad7d7a676f89a55240666db421abf27b125b8
Avi Sharma <avi.08.sh@gmail.com>
2023-03-31 17:20:38 +0530

    Removed tkr directory
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- tkr/controller/tkr-source/fetcher/helper.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2022 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package fetcher
-
-const (
-	// InitialDiscoveryRetry is the number of retries for the initial TKR sync-up
-	InitialDiscoveryRetry = 10
-)
