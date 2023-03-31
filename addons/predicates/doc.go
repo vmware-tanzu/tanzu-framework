18a59d85f23ec4ebd81c069fab93974570ce1708
Avi Sharma <avi.08.sh@gmail.com>
2023-03-31 15:50:46 +0530

    Remove directories
    The following directories are removed:
    
    * addons
    * cliplugins
    * cmd (except feature and codegen plugins)
    * config
    * object-propogation
    * packageclients
    * pinniped-components
    * providers
    * tkg
    * tkr
    
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- addons/predicates/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2021 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package predicates implements add predicate functionality.
-package predicates
--- tkg/test/scripts/legacy_hack.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copyright 2023 VMware, Inc. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-# Use clusterclass based management cluster until we need legacy mc again
-#tanzu config set features.management-cluster.package-based-cc false
-tanzu config set features.cluster.allow-legacy-cluster true
-tanzu config get
