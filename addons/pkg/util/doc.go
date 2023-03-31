d7661b9a686ce5b6af39d7fc7dbf4d53ad93100c
Avi Sharma <avi.08.sh@gmail.com>
2023-03-31 15:50:46 +0530

    Removed addons directory
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- addons/pkg/util/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2021 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package util implements various utility functionality for working with clusters
-// TKR, BOMs, and others.
-package util
