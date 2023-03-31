f09fc60924b23fe3daecccf189d07b889bdce48e
Avi Sharma <avi.08.sh@gmail.com>
2023-03-31 17:20:20 +0530

    Removed tkg directory
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- tkg/tkgctl/helper_constants.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package tkgctl placeholder package comment
-package tkgctl
-
-// True string constant for the value "true"
-var True = "true"
-var False = "false"
