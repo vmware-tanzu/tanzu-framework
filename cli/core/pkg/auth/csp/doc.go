00b98010f2e8fea38e824586bc2cc298804a52ad
Avi Sharma <avi.08.sh@gmail.com>
2023-04-04 12:28:15 +0530

    Removed cli directory
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- cli/core/pkg/auth/csp/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2021 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package csp provides VMware Cloud Services Platform token utilities.
-package csp
--- cli/core/pkg/registry/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2021 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package registry defines a Registry interface and client for working for imkpkg images.
-package registry
