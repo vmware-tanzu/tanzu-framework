82bed89369c1664d609646cba168eddcb07bcf8e
Avi Sharma <avi.08.sh@gmail.com>
2023-04-06 17:13:59 +0530

    Cleanup apis
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- featuregates/client/pkg/featuregateclient/client.go
+++ featuregates/client/pkg/featuregateclient/client.go
@@ -15,7 +15,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	corev1alpha2 "github.com/vmware-tanzu/tanzu-framework/apis/core/v1alpha2"
-	"github.com/vmware-tanzu/tanzu-framework/cli/runtime/config"
+	"github.com/vmware-tanzu/tanzu-plugin-runtime/config"
 )
 
 // FeatureGateClient defines methods to interact with FeatureGate resources
