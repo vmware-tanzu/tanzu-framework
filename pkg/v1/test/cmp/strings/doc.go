422bdce7644171e9c7c8741a9dafc39780822602
Avi Sharma <avi.08.sh@gmail.com>
2023-04-07 13:45:15 +0530

    Cleanup pkg directory
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- pkg/v1/test/cmp/strings/doc.go
+++
@@ -1,5 +0,0 @@
-// Copyright 2021 VMware, Inc. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package strings provides string related utilities for testing purposes.
-package strings
