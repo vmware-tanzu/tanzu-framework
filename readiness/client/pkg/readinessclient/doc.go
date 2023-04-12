9fb270492c9ab6f62a661e97962e59b295faec0c
Avi Sharma <avi.08.sh@gmail.com>
2023-04-12 10:43:10 +0530

    Add project structure for readiness APIs (#4564)
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

---
+++ readiness/client/pkg/readinessclient/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2023 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package readinessclient provides methods to interact with Readiness, Check and Provider resources
+package readinessclient
