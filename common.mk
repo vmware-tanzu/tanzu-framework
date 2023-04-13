a41270dc1b416dd9b07e1968207cdba0cbacf8f4
Avi Sharma <avi.08.sh@gmail.com>
2023-04-13 13:48:45 +0530

    Cleanup Makefiles
    Signed-off-by: Avi Sharma <avi.08.sh@gmail.com>

--- common.mk
+++ common.mk
@@ -37,12 +37,6 @@ ifeq ($(strip $(BUILD_VERSION)),)
 BUILD_VERSION = dev
 endif
 
-# BUILD_EDITION is the Tanzu Edition, the plugin should be built for.
-# Valid values for BUILD_EDITION are 'tce' and 'tkg'. Default value of BUILD_EDITION is 'tkg'.
-ifneq ($(BUILD_EDITION), tce)
-BUILD_EDITION = tkg
-endif
-
 ifdef DEBUG
 LD_FLAGS = -s
 GC_FLAGS = all=-N -l
@@ -51,32 +45,13 @@ LD_FLAGS = -s -w
 GC_FLAGS =
 endif
 
-# Remove old package path for buildinfo vars when it's no longer in use by plugin binaries.
-LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/buildinfo.Date=$(BUILD_DATE)'
-LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/buildinfo.SHA=$(BUILD_SHA)'
-LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/pkg/v1/buildinfo.Version=$(BUILD_VERSION)'
-
-# Set buildinfo for plugins
-LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/cli/runtime/buildinfo.Date=$(BUILD_DATE)'
-LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/cli/runtime/buildinfo.SHA=$(BUILD_SHA)'
-LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/cli/runtime/buildinfo.Version=$(BUILD_VERSION)'
-
-# Set buildinfo for tkr, object-propagation and (potentially) other modules
+# Set buildinfo for modules
 LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/util/buildinfo.Date=$(BUILD_DATE)'
 LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/util/buildinfo.SHA=$(BUILD_SHA)'
 LD_FLAGS += -X 'github.com/vmware-tanzu/tanzu-framework/util/buildinfo.Version=$(BUILD_VERSION)'
 
 # Add supported OS-ARCHITECTURE combinations here
 ENVS ?= linux-amd64 windows-amd64 darwin-amd64
-
-STANDALONE_PLUGINS ?= login management-cluster:k8s package:k8s pinniped-auth secret:k8s telemetry:k8s isolated-cluster
-CONTEXTAWARE_PLUGINS ?= cluster:k8s kubernetes-release:k8s feature:k8s
-ADMIN_PLUGINS ?= builder codegen test
-PLUGINS ?= $(STANDALONE_PLUGINS) $(CONTEXTAWARE_PLUGINS)
-
-STANDALONE_PLUGINS_WITHOUT_TARGET ?= $(shell echo $(STANDALONE_PLUGINS) | sed 's/:k8s//g' | sed 's/:tmc//g')
-CONTEXTAWARE_PLUGINS_WITHOUT_TARGET ?= $(shell echo $(CONTEXTAWARE_PLUGINS) | sed 's/:k8s//g' | sed 's/:tmc//g')
-PLUGINS_WITHOUT_TARGET ?= $(STANDALONE_PLUGINS_WITHOUT_TARGET) $(CONTEXTAWARE_PLUGINS_WITHOUT_TARGET)
 
 # Hosts running SELinux need :z added to volume mounts
 SELINUX_ENABLED := $(shell cat /sys/fs/selinux/enforce 2> /dev/null || echo 0)
@@ -115,28 +90,23 @@ COUNTERFEITER      := $(TOOLS_BIN_DIR)/counterfeiter
 TOOLING_BINARIES   := $(CONTROLLER_GEN) $(GOLANGCI_LINT) $(YTT) $(KBLD) $(VENDIR) $(IMGPKG) $(KAPP) $(KUSTOMIZE) $(GOIMPORTS) $(GOBINDATA) $(GINKGO) $(VALE) $(YQ) $(CONVERSION_GEN) $(COUNTERFEITER)
 
 ## --------------------------------------
-##@ API/controller building and generation
-## --------------------------------------
-
-help: ## Display this help (default)
-	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[0-9a-zA-Z_-]+:.*?##/ { printf "  \033[36m%-28s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m\033[32m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
-
-
-## --------------------------------------
 ##@ Tooling Binaries
 ## --------------------------------------
 
+.PHONY: tools
 tools: $(TOOLING_BINARIES) ## Build tooling binaries
 .PHONY: $(TOOLING_BINARIES)
 $(TOOLING_BINARIES):
 	make -C $(TOOLS_DIR) $(@F)
 
+## --------------------------------------
+##@ API/controller building and generation
+## --------------------------------------
+
+.PHONY: generate-controller-code
 generate-controller-code: $(CONTROLLER_GEN) $(GOIMPORTS) ## Generate code via controller-gen
 	$(CONTROLLER_GEN) $(GENERATOR) object:headerFile="$(ROOT_DIR)/hack/boilerplate.go.txt",year=$(shell date +%Y) paths="$(CONTROLLER_GEN_SRC)" $(OPTIONS)
-	$(MAKE) fmt
 
+.PHONY: generate-manifests
 generate-manifests:
 	$(MAKE) generate-controller-code GENERATOR=crd OPTIONS="output:crd:artifacts:config=$(RELATIVE_ROOT)/config/crd/bases" CONTROLLER_GEN_SRC=$(CONTROLLER_GEN_SRC)
-
-fmt: $(GOIMPORTS) ## Run goimports
-	$(GOIMPORTS) -w -local github.com/vmware-tanzu ./
