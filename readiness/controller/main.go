8036d481dccb841dddd2751c7aeea5de18d853e8
Sathyanarayanan Saravanamuthu <sathyanarays@vmware.com>
2023-05-02 11:41:10 +0530

    Readiness Framework changes

---
+++ readiness/controller/main.go
@@ -0,0 +1,131 @@
+// Copyright 2023 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"flag"
+	"os"
+
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	corev1alpha2 "github.com/vmware-tanzu/tanzu-framework/apis/core/v1alpha2"
+	"github.com/vmware-tanzu/tanzu-framework/readiness/controller/pkg/conditions"
+	readinesscontroller "github.com/vmware-tanzu/tanzu-framework/readiness/controller/pkg/readiness"
+	readinessprovidercontroller "github.com/vmware-tanzu/tanzu-framework/readiness/controller/pkg/readinessprovider"
+	//+kubebuilder:scaffold:imports
+)
+
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(corev1alpha2.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
+}
+
+func main() {
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+
+	restConfig := ctrl.GetConfigOrDie()
+
+	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "906562b5.tanzu.vmware.com",
+		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
+		// when the Manager ends. This requires the binary to immediately end when the
+		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
+		// speeds up voluntary leader transitions as the new leader don't have to wait
+		// LeaseDuration time first.
+		//
+		// In the default scaffold provided, the program ends immediately after
+		// the manager stops, so would be fine to enable this option. However,
+		// if you are doing or is intended to do any operation such as perform cleanups
+		// after the manager stops then its usage might be unsafe.
+		// LeaderElectionReleaseOnCancel: true,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(restConfig)
+	if err != nil {
+		setupLog.Error(err, "unable to create dynamic client")
+		os.Exit(1)
+	}
+
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(restConfig)
+	if err != nil {
+		setupLog.Error(err, "unable to create discovery client")
+		os.Exit(1)
+	}
+
+	if err = (&readinesscontroller.ReadinessReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Readiness").WithValues("apigroup", "core"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Readiness")
+		os.Exit(1)
+	}
+
+	if err = (&readinessprovidercontroller.ReadinessProviderReconciler{
+		Client:                     mgr.GetClient(),
+		Log:                        ctrl.Log.WithName("controllers").WithName("ReadinessProvider").WithValues("apigroup", "core"),
+		Scheme:                     mgr.GetScheme(),
+		ResourceExistenceCondition: conditions.NewResourceExistencConditionFunc(dynamicClient, discoveryClient),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ReadinessProvider")
+		os.Exit(1)
+	}
+	//+kubebuilder:scaffold:builder
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
+}
---
+++ readiness/controller/pkg/readiness/readiness_controller.go
@@ -0,0 +1,256 @@
+// Copyright 2023 VMware, Inc. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package readiness
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-logr/logr"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	corev1alpha2 "github.com/vmware-tanzu/tanzu-framework/apis/core/v1alpha2"
+)
+
+const contextTimeout = 30 * time.Second
+
+// ReadinessReconciler reconciles a Readiness object
+type ReadinessReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=core.tanzu.vmware.com,resources=readinesses,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core.tanzu.vmware.com,resources=readinesses/status,verbs=get;update;patch
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+func (r *ReadinessReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	ctxCancel, cancel := context.WithTimeout(ctx, contextTimeout)
+	defer cancel()
+
+	readiness := &corev1alpha2.Readiness{}
+	err := r.Client.Get(ctxCancel, req.NamespacedName, readiness)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	updateRequired := false
+	currentStatus := readiness.Status
+
+	if len(readiness.Spec.Checks) == 0 {
+		if !currentStatus.Ready {
+			readiness.Status.Ready = true
+			readiness.Status.CheckStatus = []corev1alpha2.CheckStatus{}
+			updateRequired = true
+		}
+	} else {
+		readiness.Status.CheckStatus = []corev1alpha2.CheckStatus{}
+		providersList := &corev1alpha2.ReadinessProviderList{}
+
+		// TODO: Find a better way to index and fetch the proviers in a single list call
+		for _, check := range readiness.Spec.Checks {
+			providers := &corev1alpha2.ReadinessProviderList{}
+			err = r.Client.List(ctxCancel, providers, &client.ListOptions{
+				FieldSelector: fields.OneTermEqualSelector("spec.checkRef", check.Name),
+			})
+			if err != nil {
+				return ctrl.Result{}, err
+			}
+
+			providersList.Items = append(providersList.Items, providers.Items...)
+		}
+
+		// uniqueProviders contain all the providers that satisfy at least one of the checks
+		uniqueProviders := make([]corev1alpha2.ReadinessProvider, 0)
+		uniqueProviderNames := make(map[string]bool)
+
+		for _, provider := range providersList.Items {
+			if _, ok := uniqueProviderNames[provider.Name]; !ok {
+				uniqueProviders = append(uniqueProviders, provider)
+				uniqueProviderNames[provider.Name] = true
+			}
+		}
+
+		// allChecks contain the check names and the list of associated providers
+		allChecks := make(map[string][]int)
+		for i, provider := range uniqueProviders {
+			for _, checkRef := range provider.Spec.CheckRefs {
+				if _, ok := allChecks[checkRef]; !ok {
+					allChecks[checkRef] = make([]int, 0)
+				}
+
+				allChecks[checkRef] = append(allChecks[checkRef], i)
+			}
+		}
+
+		for _, check := range readiness.Spec.Checks {
+			checkStatus := corev1alpha2.CheckStatus{
+				Name:      check.Name,
+				Providers: make([]corev1alpha2.Provider, 0),
+				Ready:     false,
+			}
+
+			if indices, ok := allChecks[check.Name]; ok {
+				for _, index := range indices {
+					provider := uniqueProviders[index]
+
+					if provider.Status.State == corev1alpha2.ProviderSuccessState {
+						checkStatus.Ready = true
+					}
+					checkStatus.Providers = append(checkStatus.Providers, corev1alpha2.Provider{
+						Name:     provider.Name,
+						IsActive: provider.Status.State == corev1alpha2.ProviderSuccessState,
+					})
+				}
+
+			}
+
+			readiness.Status.CheckStatus = append(readiness.Status.CheckStatus, checkStatus)
+		}
+
+		readiness.Status.Ready = true
+
+		for _, checkStatus := range readiness.Status.CheckStatus {
+			readiness.Status.Ready = readiness.Status.Ready && checkStatus.Ready
+		}
+
+		updateRequired = isUpdateRequired(currentStatus, readiness.Status)
+
+	}
+
+	if updateRequired {
+		time := metav1.Now()
+		readiness.Status.LastUpdatedTime = &time
+
+		err = r.Client.Status().Update(ctxCancel, readiness)
+		if err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func isUpdateRequired(oldStatus, newStatus corev1alpha2.ReadinessStatus) bool {
+	oldCheckStatusByName := make(map[string]corev1alpha2.CheckStatus)
+	for _, checkStatus := range oldStatus.CheckStatus {
+		oldCheckStatusByName[checkStatus.Name] = checkStatus
+	}
+
+	newCheckStatusByName := make(map[string]corev1alpha2.CheckStatus)
+	for _, checkStatus := range newStatus.CheckStatus {
+		newCheckStatusByName[checkStatus.Name] = checkStatus
+	}
+
+	if len(oldCheckStatusByName) != len(newCheckStatusByName) {
+		return true
+	}
+
+	for name, oldCheckStatus := range oldCheckStatusByName {
+		newCheckStatus, ok := newCheckStatusByName[name]
+		if !ok {
+			return true
+		}
+
+		if oldCheckStatus.Ready != newCheckStatus.Ready {
+			return true
+		}
+
+		if len(oldCheckStatus.Providers) != len(newCheckStatus.Providers) {
+			return true
+		}
+
+		for _, oldProvider := range oldCheckStatus.Providers {
+			found := false
+			for _, newProvider := range newCheckStatus.Providers {
+				if oldProvider.Name == newProvider.Name {
+					found = true
+
+					if oldProvider.IsActive != newProvider.IsActive {
+						return true
+					}
+
+					break
+				}
+			}
+
+			if !found {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ReadinessReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	mgr.GetFieldIndexer().IndexField(context.Background(), &corev1alpha2.ReadinessProvider{}, "spec.checkRef", func(rawObj client.Object) []string {
+		provider := rawObj.(*corev1alpha2.ReadinessProvider)
+		var indices []string
+		indices = append(indices, provider.Spec.CheckRefs...)
+		return indices
+	})
+
+	mgr.GetFieldIndexer().IndexField(context.Background(), &corev1alpha2.Readiness{}, "spec.checks.name", func(rawObj client.Object) []string {
+		provider := rawObj.(*corev1alpha2.Readiness)
+
+		keys := []string{}
+		for _, check := range provider.Spec.Checks {
+			keys = append(keys, check.Name)
+		}
+
+		return keys
+	})
+
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&corev1alpha2.Readiness{}).
+		Watches(
+			&source.Kind{Type: &corev1alpha2.ReadinessProvider{}},
+			handler.EnqueueRequestsFromMapFunc(r.findObjectsForReadinessProvider),
+		).
+		Complete(r)
+}
+
+func (r *ReadinessReconciler) findObjectsForReadinessProvider(readinessProviderObject client.Object) []reconcile.Request {
+	ctxCancel, cancel := context.WithTimeout(context.Background(), contextTimeout)
+	defer cancel()
+
+	provider, _ := readinessProviderObject.(*corev1alpha2.ReadinessProvider)
+
+	totalReadinessList := &corev1alpha2.ReadinessList{}
+	for _, checkRef := range provider.Spec.CheckRefs {
+		readinessList := &corev1alpha2.ReadinessList{}
+		err := r.Client.List(ctxCancel, readinessList, &client.ListOptions{
+			FieldSelector: fields.OneTermEqualSelector("spec.checks.name", checkRef),
+		})
+		if err != nil {
+			r.Log.Error(err, "error while updating readiness status")
+			return []reconcile.Request{}
+		}
+		totalReadinessList.Items = append(totalReadinessList.Items, readinessList.Items...)
+	}
+
+	requests := []reconcile.Request{}
+
+	for _, readiness := range totalReadinessList.Items {
+		requests = append(requests, reconcile.Request{
+			NamespacedName: types.NamespacedName{
+				Name: readiness.Name,
+			},
+		})
+	}
+
+	return requests
+}
